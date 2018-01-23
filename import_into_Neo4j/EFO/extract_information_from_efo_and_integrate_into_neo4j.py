708009484bd7bb7f5f85db4c862a58d702c72234
ck_linux <dragon493@web.de>
2018-01-23 16:46:10 +0100

    fix all errors

--- import_into_Neo4j/EFO/extract_information_from_efo_and_integrate_into_neo4j.py
+++ import_into_Neo4j/EFO/extract_information_from_efo_and_integrate_into_neo4j.py
@@ -86,50 +86,87 @@ complete the leaves of the  with recursion
 
 
 def get_all_leaves_and_internal_nodes(node_id):
+    # type: (node id) -> set of node ids
     all_nodes = dict_hierarchical_set[node_id]
+    set_of_leaves_and_inner_nodes= dict_hierarchical_set[node_id]
     for node in all_nodes:
         if node in dict_hierarchical_set:
-            dict_hierarchical_set[node_id] = dict_hierarchical_set[node_id].union(
+            set_of_leaves_and_inner_nodes = set_of_leaves_and_inner_nodes.union(
                 get_all_leaves_and_internal_nodes(node))
-    return dict_hierarchical_set[node_id]
+    return set_of_leaves_and_inner_nodes
 
     #                    print(key_term)
     #                    print(value)
 
 
-#                # only for useful hpo ids
-#                if use_full_hpo_id:
-#                    if not is_a_frequency_information:
-#                        create_text = 'Create (:HPOsymptom{'
-#                        # add all properties to node which hpo has
-#                        for key, list_information in dict_all_info.items():
-#                            modify_list_informationan = []
-#                            for info in list_information:
-#                                info = info.replace('"', '').replace("'", "").replace("\\", "")
-#                                modify_list_informationan.append(info)
-#                            create_text = create_text + key + ": '" + "|".join(modify_list_informationan) + "' ,"
-#                        create_text = create_text[0:-1] + '});\n'
-#                        counter_create += 1
-#                        cypher_file.write(create_text)
-#                        # test if a new commit block or new file need to be generated
-#                        if counter_create % constrain_number == 0:
-#                            cypher_file.write('commit \n')
-#                            if counter_create % creation_max_in_file == 0:
-#                                cypher_file.close()
-#                                cypher_file = open('integrate_hpo_into_neo4j_' + str(file_counter) + '.cypher', 'w')
-#                                cypher_file.write('begin \n')
-#                                file_counter += 1
-#                            else:
-#                                cypher_file.write('begin \n')
-#                    else:
-#                        identifier = dict_all_info['id'][0]
-#                        name = dict_all_info['name'][0]
-#                        definition = dict_all_info['def'][0]
-#                        dict_hpo_frequency[identifier] = [name, definition]
-#    print(counter_create)
-#    cypher_file.write('commit \n begin \n')
-#    cypher_file.write('Create Constraint On (node:HPOsymptom) Assert node.id Is Unique; \n')
-#    cypher_file.write('commit \n begin \n')
+# file counter
+file_counter = 1
+# maximal number of queries for a commit block
+constrain_number = 20000
+# maximal number of queries in a file
+creation_max_in_file = 1000000
+
+'''
+generate cypher file
+'''
+
+
+def generate_cypher_file(all_child_of_disease):
+    global file_counter
+    counter_create = 0
+    # list_of_all_diseases = list(dict_hierarchical_set['EFO:0000408'])
+    list_of_all_diseases = list(all_child_of_disease)
+    cypher_file = open('integrate_efo_disease_into_neo4j_' + str(file_counter) + '.cypher', 'w')
+    file_counter += 1
+    cypher_file.write('begin \n')
+
+    for disease_id in list_of_all_diseases:
+        create_text = 'Create (:EFOdisease{'
+        # add all properties to node which hpo has
+        for key, list_information in dict_all_efo_entries[disease_id].items():
+            modify_list_information = []
+            for info in list_information:
+                info = info.replace('"', '').replace("'", "").replace("\\", "")
+                modify_list_information.append(info)
+            create_text = create_text + key + ": '" + "|".join(modify_list_information) + "' ,"
+        create_text = create_text[0:-1] + '});\n'
+        counter_create += 1
+        cypher_file.write(create_text)
+        # test if a new commit block or new file need to be generated
+        if counter_create % constrain_number == 0:
+            cypher_file.write('commit \n')
+            if counter_create % creation_max_in_file == 0:
+                cypher_file.close()
+                cypher_file = open('integrate_efo_disease_into_neo4j_' + str(file_counter) + '.cypher', 'w')
+                cypher_file.write('begin \n')
+                file_counter += 1
+            else:
+                cypher_file.write('begin \n')
+    print(counter_create)
+    cypher_file.write('commit \n begin \n')
+    cypher_file.write('Create Constraint On (node:EFOdisease) Assert node.id Is Unique; \n')
+    cypher_file.write('commit \n begin \n')
+
+    for disease_id in list_of_all_diseases:
+        if disease_id in dict_hierarchical_set:
+            for child_disease  in list(dict_hierarchical_set[disease_id]):
+                query=''' Match (a:EFOdisease{id:"%s"}), (b:EFOdisease{id:"%s"}) 
+                 Create (a)-[:is_a_efo]->(b); \n'''
+                query =query %(child_disease, disease_id)
+                counter_create += 1
+                cypher_file.write(query)
+                # test if a new commit block or new file need to be generated
+                if counter_create % constrain_number == 0:
+                    cypher_file.write('commit \n')
+                    if counter_create % creation_max_in_file == 0:
+                        cypher_file.close()
+                        cypher_file = open('integrate_efo_disease_into_neo4j_' + str(file_counter) + '.cypher', 'w')
+                        cypher_file.write('begin \n')
+                        file_counter += 1
+                    else:
+                        cypher_file.write('begin \n')
+    cypher_file.write('commit')
+    print(counter_create)
 
 
 def main():
@@ -147,7 +184,7 @@ def main():
 
     print('##########################################################################')
 
-    get_all_leaves_and_internal_nodes('EFO:0000408')
+    all_child_nodes_of_disease=get_all_leaves_and_internal_nodes('EFO:0000408')
 
     print(dict_hierarchical_set['EFO:0000408'])
     print(len(list(dict_hierarchical_set['EFO:0000408'])))
@@ -156,7 +193,7 @@ def main():
     print(datetime.datetime.utcnow())
     print('put all relationship information into a cypher file')
 
-    #    add_connection_to_cypher_file()
+    generate_cypher_file(all_child_nodes_of_disease)
 
     print('##########################################################################')
 
