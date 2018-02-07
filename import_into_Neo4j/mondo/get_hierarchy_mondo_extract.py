c497ae8f313ee55fbe52c9c6fd4e08822fc0a96a
ck_linux <dragon493@web.de>
2018-02-07 09:22:22 +0100

    updated

--- import_into_Neo4j/mondo/get_hierarchy_mondo_extract.py
+++ import_into_Neo4j/mondo/get_hierarchy_mondo_extract.py
@@ -6,11 +6,11 @@ Created on Fri Feb 2 07:23:43 2018
 """
 
 from py2neo import Graph, authenticate
-import MySQLdb as mdb
-import sys
 import datetime
 import string
-import types
+import sys
+from ete3 import Tree
+
 
 reload(sys)
 sys.setdefaultencoding("utf-8")
@@ -21,6 +21,10 @@ def database_connection():
     authenticate("localhost:7474", "neo4j", "test")
     global g
     g = Graph("http://localhost:7474/db/data/")
+
+# dictionary with all levels and two lists the first contains the class element and the second the leafs and each has a
+# dict with parent id as key with id name and parent
+dict_levels = {}
 
 
 '''
@@ -65,9 +69,17 @@ def generate_files():
             query_level_element = query_start + query_end_match + query_where_level_element + string.ascii_lowercase[
                 letter_of_parent - 1] + '.id, ' + string.ascii_lowercase[letter_of_parent - 1] + '.name '
 
+        dict_levels[count_level] = []
+
+        dict_class_entry = {}
         results_classes = g.run(query_level_class)
         # collect the information for classes
         for id, name, parent_id, parent_name, in results_classes:
+            if parent_id not in dict_class_entry:
+                dict_class_entry[parent_id] = [[id, name, parent_id]]
+            else:
+                dict_class_entry[parent_id].append([id, name, parent_id])
+
             if id not in dict_level:
                 dict_level[id] = name
                 dict_level_class_parent[id] = set([parent_id])
@@ -75,6 +87,8 @@ def generate_files():
                 dict_level_class_parent[id].add(parent_id)
             if parent_id not in dict_parent:
                 dict_parent[parent_id] = parent_name
+
+        dict_levels[count_level].append(dict_class_entry)
 
         # integrate the information into a file
         for id, set_of_parent_ids in dict_level_class_parent.items():
@@ -88,9 +102,15 @@ def generate_files():
         class_file.close()
 
         print(query_level_element)
+        dict_class_entry = {}
         results_elements = g.run(query_level_element)
         # collect the information for entities
         for id, name, parent_id, parent_name, in results_elements:
+            if parent_id not in dict_class_entry:
+                dict_class_entry[parent_id] = [[id, name, parent_id]]
+            else:
+                dict_class_entry[parent_id].append([id, name, parent_id])
+
             if id not in dict_level:
                 dict_level[id] = name
                 dict_level_entries_parent[id] = set([parent_id])
@@ -98,6 +118,8 @@ def generate_files():
                 dict_level_entries_parent[id].add(parent_id)
             if parent_id not in dict_parent:
                 dict_parent[parent_id] = parent_name
+
+        dict_levels[count_level].append(dict_class_entry)
 
         # integrate the information into a file
         for id, set_of_parent_ids in dict_level_entries_parent.items():
@@ -115,6 +137,43 @@ def generate_files():
         count_level += 1
 
 
+'''
+recursive get the tree structure
+'''
+
+
+def recursion_tree_generation(parent_id, count_level):
+    part_tree_string = ''
+    # if count_level in dict_levels:
+    if count_level < 3:
+        # if parent_id in dict_levels[count_level][1]:
+        #     for [id, name, parent] in dict_levels[count_level][1][parent_id]:
+        #         part_tree_string = part_tree_string  + name.replace('(','').replace(')','').replace(';',' ').replace(':','').replace(',','') + "-" + id.replace(':','_') + ","
+
+        if parent_id in dict_levels[count_level][0]:
+            for [id, name, parent] in dict_levels[count_level][0][parent_id]:
+
+                part_tree_string = part_tree_string + recursion_tree_generation(id,
+                                                                                      count_level + 1)  + name.replace('(','').replace(')','').replace(';',' ').replace(':','').replace(',','') + "-" + id.replace(':','_') + ","
+        if len(part_tree_string)>0:
+            part_tree_string="("+part_tree_string[0:-1]+")"
+
+    return part_tree_string
+
+
+'''
+generate tree 
+'''
+
+
+def generate_tree():
+    count_level = 1
+    datatree = recursion_tree_generation("MONDO:0000001", count_level) + "disease-MONDO_0000001;"
+    print(datatree)
+
+    rooted_tree = Tree(datatree, format=8)
+    print rooted_tree.get_ascii(show_internal=True)
+
 def main():
     print(datetime.datetime.utcnow())
 
@@ -130,6 +189,13 @@ def main():
     print('load in MonDO diseases ')
 
     generate_files()
+
+    print('##########################################################################')
+
+    print(datetime.datetime.utcnow())
+    print('load in MonDO diseases ')
+
+    generate_tree()
 
     print('##########################################################################')
 
