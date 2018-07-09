ccc7f673eadcc194bffcc9f0d55a8776c86bb634
ck_linux <dragon493@web.de>
2018-07-09 08:01:32 +0200

    update

--- mapping_and_merging_into_hetionet/ctd/integrate_disease_go.py
+++ mapping_and_merging_into_hetionet/ctd/integrate_disease_go.py
@@ -31,15 +31,15 @@ dict_gene_go = {}
 # csv files for bp. mf, cc
 bp_file = open('disease_go/bp.csv', 'w')
 bp_writer = csv.writer(bp_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-bp_writer.writerow(['DiseaseID', 'GOID', 'inferenceGeneQty', 'inferenceGeneSymbols'])
+bp_writer.writerow(['DiseaseID', 'GOID', 'inferenceGeneQty', 'inferenceGeneSymbols','diseaseID'])
 
 mf_file = open('disease_go/mf.csv', 'w')
 mf_writer = csv.writer(mf_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-mf_writer.writerow(['DiseaseID', 'GOID', 'inferenceGeneQty', 'inferenceGeneSymbols'])
+mf_writer.writerow(['DiseaseID', 'GOID', 'inferenceGeneQty', 'inferenceGeneSymbols','diseaseID'])
 
 cc_file = open('disease_go/cc.csv', 'w')
 cc_writer = csv.writer(cc_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-cc_writer.writerow(['DiseaseID', 'GOID', 'inferenceGeneQty', 'inferenceGeneSymbols'])
+cc_writer.writerow(['DiseaseID', 'GOID', 'inferenceGeneQty', 'inferenceGeneSymbols','diseaseID'])
 
 # dictionary with for biological_process, cellular_component, molecular_function the right file
 dict_processe = {
@@ -62,29 +62,29 @@ also generate a cypher file to integrate this information
 
 
 def take_all_relationships_of_disease_go():
-    query = '''MATCH (disease)-[r:affects_DGO]->(go:CTDGO)  RETURN disease.mondos, r, go.go_id, go.ontology '''
+    query = '''MATCH (disease)-[r:affects_DGO]->(go:CTDGO)  RETURN disease.mondos, disease.disease_id, r, go.go_id, go.ontology '''
     results = g.run(query)
     count_multiple_pathways = 0
     count_possible_relas = 0
-    for disease_mondos, rela, go_id, ontology in results:
+    for disease_mondos, disease_id, rela, go_id, ontology in results:
         rela = dict(rela)
         inferenceGeneQty = rela['inferenceGeneQty'] if 'inferenceGeneQty' in rela else ''
         inferenceGeneSymbols = rela['inferenceGeneSymbols'] if 'inferenceGeneSymbols' in rela else ''
         for mondo in disease_mondos:
             if not (mondo, go_id) in dict_gene_go:
-                dict_gene_go[(mondo, go_id)] = [[inferenceGeneQty], [inferenceGeneSymbols], [ontology]]
+                dict_gene_go[(mondo, go_id)] = [set([inferenceGeneQty]), set([inferenceGeneSymbols]), set([ontology]), disease_id]
                 dict_processe_counter[ontology] += 1
 
                 count_possible_relas += 1
             else:
                 count_multiple_pathways += 1
-                dict_gene_go[(mondo, go_id)][0].append(inferenceGeneQty)
-                dict_gene_go[(mondo, go_id)][1].append(inferenceGeneSymbols)
-                dict_gene_go[(mondo, go_id)][2].append(ontology)
-                list_rela=list(set(dict_gene_go[(mondo, go_id)][2]))
-                if len(list_rela)>1:
-                    print(mondo, go_id)
-                    print(list_rela)
+                dict_gene_go[(mondo, go_id)][0].add(inferenceGeneQty)
+                dict_gene_go[(mondo, go_id)][1].add(inferenceGeneSymbols)
+                dict_gene_go[(mondo, go_id)][2].add(ontology)
+                # list_rela=list(set(dict_gene_go[(mondo, go_id)][2]))
+                # if len(list_rela)>1:
+                #     print(mondo, go_id)
+                #     print(list_rela)
         if count_possible_relas % 1000 == 0:
             print(count_possible_relas)
 
@@ -101,16 +101,23 @@ Generate the csv and cypher file
 def generate_csv_and_cypher_file():
     # generate cypher file
     cypherfile = open('disease_go/cypher.cypher', 'w')
-    query = '''Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:/home/cassandra/Dokumente/Project/master_database_change/mapping_and_merging_into_hetionet/ctd/disease_go/bp.csv" As line Match (n:Disease{identifier:line.DiseaseID}), (b:BiologicalProcess{identifier:line.GOID}) Merge (n)-[r:ASSOCIATES_DaBP]->(b) On Create Set r.hetionet='no', r.inferenceGeneQty=line.inferenceGeneQty,r.inferenceGeneSymbols=line.inferenceGeneSymbols, r.ctd='yes', r.url_ctd="http://ctdbase.org/detail.go?type=disease&acc="+line.DiseaseID , r.source="CTD", r.license="© 2002–2012 MDI Biological Laboratory. © 2012–2018 MDI Biological Laboratory & NC State University. All rights reserved.", r.unbiased='false' On Match SET r.inferenceGeneQty=line.inferenceGeneQty,r.inferenceGeneSymbols=line.inferenceGeneSymbols, r.ctd='yes', r.url_ctd="http://ctdbase.org/detail.go?type=disease&acc="+line.DiseaseID;\n '''
+    query = '''Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:/home/cassandra/Dokumente/Project/master_database_change/mapping_and_merging_into_hetionet/ctd/disease_go/bp.csv" As line Match (n:Disease{identifier:line.DiseaseID}), (b:BiologicalProcess{identifier:line.GOID}) Merge (n)-[r:ASSOCIATES_DaBP]->(b) On Create Set r.hetionet='no', r.inferenceGeneQty=line.inferenceGeneQty,r.inferenceGeneSymbols=line.inferenceGeneSymbols, r.ctd='yes', r.url_ctd="http://ctdbase.org/detail.go?type=disease&acc="+line.diseaseID, r.source="CTD", r.license="© 2002–2012 MDI Biological Laboratory. © 2012–2018 MDI Biological Laboratory & NC State University. All rights reserved.", r.unbiased='false' On Match SET r.inferenceGeneQty=line.inferenceGeneQty,r.inferenceGeneSymbols=line.inferenceGeneSymbols, r.ctd='yes', r.url_ctd="http://ctdbase.org/detail.go?type=disease&acc="+line.DiseaseID;\n '''
     cypherfile.write(query)
-    query = '''Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:/home/cassandra/Dokumente/Project/master_database_change/mapping_and_merging_into_hetionet/ctd/disease_go/mf.csv" As line Match (n:Disease{identifier:line.DiseaseID}), (b:MolecularFunction{identifier:line.GOID}) Merge (n)-[r:ASSOCIATES_DaMF]->(b) On Create Set r.hetionet='no', r.inferenceGeneQty=line.inferenceGeneQty,r.inferenceGeneSymbols=line.inferenceGeneSymbols, r.ctd='yes', r.url_ctd="http://ctdbase.org/detail.go?type=disease&acc="+line.DiseaseID , r.source="CTD", r.license="© 2002–2012 MDI Biological Laboratory. © 2012–2018 MDI Biological Laboratory & NC State University. All rights reserved.", r.unbiased='false' On Match SET r.inferenceGeneQty=line.inferenceGeneQty,r.inferenceGeneSymbols=line.inferenceGeneSymbols, r.ctd='yes', r.url_ctd="http://ctdbase.org/detail.go?type=disease&acc="+line.DiseaseID;\n '''
+    query = '''Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:/home/cassandra/Dokumente/Project/master_database_change/mapping_and_merging_into_hetionet/ctd/disease_go/mf.csv" As line Match (n:Disease{identifier:line.DiseaseID}), (b:MolecularFunction{identifier:line.GOID}) Merge (n)-[r:ASSOCIATES_DaMF]->(b) On Create Set r.hetionet='no', r.inferenceGeneQty=line.inferenceGeneQty,r.inferenceGeneSymbols=line.inferenceGeneSymbols, r.ctd='yes', r.url_ctd="http://ctdbase.org/detail.go?type=disease&acc="+line.diseaseID , r.source="CTD", r.license="© 2002–2012 MDI Biological Laboratory. © 2012–2018 MDI Biological Laboratory & NC State University. All rights reserved.", r.unbiased='false' On Match SET r.inferenceGeneQty=line.inferenceGeneQty,r.inferenceGeneSymbols=line.inferenceGeneSymbols, r.ctd='yes', r.url_ctd="http://ctdbase.org/detail.go?type=disease&acc="+line.DiseaseID;\n '''
     cypherfile.write(query)
-    query = '''Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:/home/cassandra/Dokumente/Project/master_database_change/mapping_and_merging_into_hetionet/ctd/disease_go/cc.csv" As line Match (n:Disease{identifier:line.DiseaseID}), (b:CellularComponent{identifier:line.GOID}) Merge (n)-[r:ASSOCIATES_DaCC]->(b) On Create Set r.hetionet='no', r.inferenceGeneQty=line.inferenceGeneQty,r.inferenceGeneSymbols=line.inferenceGeneSymbols, r.ctd='yes', r.url_ctd="http://ctdbase.org/detail.go?type=disease&acc="+line.DiseaseID , r.source="CTD", r.license="© 2002–2012 MDI Biological Laboratory. © 2012–2018 MDI Biological Laboratory & NC State University. All rights reserved.", r.unbiased='false' On Match SET r.inferenceGeneQty=line.inferenceGeneQty,r.inferenceGeneSymbols=line.inferenceGeneSymbols, r.ctd='yes', r.url_ctd="http://ctdbase.org/detail.go?type=disease&acc="+line.DiseaseID;\n '''
+    query = '''Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:/home/cassandra/Dokumente/Project/master_database_change/mapping_and_merging_into_hetionet/ctd/disease_go/cc.csv" As line Match (n:Disease{identifier:line.DiseaseID}), (b:CellularComponent{identifier:line.GOID}) Merge (n)-[r:ASSOCIATES_DaCC]->(b) On Create Set r.hetionet='no', r.inferenceGeneQty=line.inferenceGeneQty,r.inferenceGeneSymbols=line.inferenceGeneSymbols, r.ctd='yes', r.url_ctd="http://ctdbase.org/detail.go?type=disease&acc="+line.diseaseID , r.source="CTD", r.license="© 2002–2012 MDI Biological Laboratory. © 2012–2018 MDI Biological Laboratory & NC State University. All rights reserved.", r.unbiased='false' On Match SET r.inferenceGeneQty=line.inferenceGeneQty,r.inferenceGeneSymbols=line.inferenceGeneSymbols, r.ctd='yes', r.url_ctd="http://ctdbase.org/detail.go?type=disease&acc="+line.DiseaseID;\n '''
     cypherfile.write(query)
 
-    for (mondo, go_id), [inferenceGeneQty, inferenceGeneSymbols, ontology] in dict_gene_go.items():
-        writer = dict_processe[ontology]
-        writer.writerow([mondo, go_id, inferenceGeneQty, inferenceGeneSymbols])
+    for (mondo, go_id), [inferenceGeneQty, inferenceGeneSymbols, ontologys, disease_id] in dict_gene_go.items():
+        inferenceGeneQty='|'.join(list(inferenceGeneQty))
+        inferenceGeneSymbols = '|'.join(list(inferenceGeneSymbols))
+        ontologys = list(ontologys)
+        if len(ontologys)>1:
+            print(mondo, go_id)
+            print(ontologys)
+        for ontology in ontologys:
+            writer = dict_processe[ontology]
+            writer.writerow([mondo, go_id, inferenceGeneQty, inferenceGeneSymbols, disease_id])
 
 
 def main():
@@ -123,9 +130,17 @@ def main():
         '###########################################################################################################################')
 
     print (datetime.datetime.utcnow())
-    print('Take all disease-go relationships and generate csv files and cypher file')
+    print('Take all disease-go relationships')
 
     take_all_relationships_of_disease_go()
+
+    print(
+        '###########################################################################################################################')
+
+    print (datetime.datetime.utcnow())
+    print('generate csv files and cypher file')
+
+    generate_csv_and_cypher_file()
 
     print(
         '###########################################################################################################################')
