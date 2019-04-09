2c3ec03d4108b2fa803568b09bea955daad1ed69
ck_linux <dragon493@web.de>
2019-04-09 08:18:32 +0200

    updated

--- mapping_and_merging_into_hetionet/drugbank/merging_protein_into_hetionet.py
+++ mapping_and_merging_into_hetionet/drugbank/merging_protein_into_hetionet.py
@@ -14,6 +14,31 @@ def create_connetion_with_neo4j():
     global g
     g = Graph("http://localhost:7474/db/data/")
 
+#dictionary hetionet protein with uniprot identifier as key and value is the whole node as dictionary
+dict_hetionet_protein={}
+
+
+'''
+gather all hetionet proteins in a dictionary
+'''
+def load_all_hetionet_proteins_in_dictionary():
+    query = '''MATCH (n:Protein) RETURN n.identifier, n ;'''
+    results = g.run(query)
+    counter_multiple_identifier=0
+    for identifier, node, in results:
+        alternative_ids=node['alternative_ids'] if 'alternative_ids' in node else []
+        dict_hetionet_protein[identifier]=[dict(node)]
+        for alternative_id in alternative_ids:
+            if not alternative_id in dict_hetionet_protein:
+                dict_hetionet_protein[alternative_id]=[dict(node)]
+            else:
+                counter_multiple_identifier+=1
+                dict_hetionet_protein[alternative_id].append(dict(node))
+                # print(alternative_id)
+                # print(dict_hetionet_protein[alternative_id])
+                # print(node)
+    print('number of identifier which appears multiple times:'+str(counter_multiple_identifier))
+
 
 # dictionary of maybe protein or not (the drugbank proteins which has only a drugbank id and no components)
 # currently 50 are proteins and 63 not, was manual checked
@@ -69,6 +94,65 @@ dict_db_labels_to_csv_label_file = {
     'Carrier_DrugBank': writer_carrier,
     'Target_DrugBank': writer_target}
 
+# protein csv which should be updated
+generate_csv = open('protein/proteins.csv', 'w')
+writer = csv.writer(generate_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+writer.writerow(['id', 'uniport', 'sequences'])
+
+'''
+preparation of the csv for merging for a given node
+'''
+def interagte_infos_into_csv(part_dict,protein_hetionet,list_input_protein):
+    # first check on the as sequences and combine the if they are not the same, because they are isoforms
+    # or  have only small changes like one as is changed
+    as_seqs = part_dict['amino_acid_sequence'] if 'amino_acid_sequence' in part_dict else ''
+
+    as_seq_hetionet = protein_hetionet['as_sequence'] if 'as_sequence' in protein_hetionet else ''
+    if ':' in as_seq_hetionet:
+        as_seq_hetionet_seq = as_seq_hetionet.split(':')[1]
+    else:
+        as_seq_hetionet_seq = as_seq_hetionet
+    list_as_sequnces = []
+    list_as_sequnces.append(as_seq_hetionet_seq)
+    for as_seq in as_seqs:
+
+        as_seq_part = as_seq.split(' ')[1]
+        if as_seq_hetionet_seq != as_seq_part:
+            list_as_sequnces.append(as_seq_part)
+
+    list_as_sequnces = '|'.join(list_as_sequnces)
+    list_input_protein.append(list_as_sequnces)
+
+    # check on the pfams of both
+    # to many were not correct and around 230 have to be checked
+    # pfam_db= part_dict['pfams'] if 'pfams' in part_dict else []
+    # pfam_db= [ x.replace('::',':') for x in pfam_db]
+    # pfam_protein= protein_hetionet['pfam'] if 'pfam' in protein_hetionet else []
+    # pfam_protein= set(pfam_protein)
+    # #this were manual check pfams which were not correct
+    # if identifier not in ['Q6NUM9','P07307','O94760','P63151','O95865']:
+    #     length_before_union=len(pfam_protein)
+    #     pfam_protein=pfam_protein.union(pfam_db)
+    #     if len(pfam_protein)> length_before_union:
+    #         print(identifier)
+    #         print(pfam_db)
+    #         print('not the same pfams')
+    #
+    # pfam_protein='|'.join(pfam_protein)
+    # list_input_protein.append(pfam_protein)
+
+    #check on gene names are the same
+    # hetionet_gene_names=protein_hetionet['gene_name'] if 'gene_name' in protein_hetionet else []
+    # db_gene_name=part_dict['gene_name'] if 'gene_name' in part_dict else ''
+    # if not db_gene_name in hetionet_gene_names:
+    #     print(part_dict['identifier'])
+    #     print(hetionet_gene_names)
+    #     print(db_gene_name)
+    #     print('ohje gene name')
+
+    writer.writerow(list_input_protein)
+
+
 
 
 '''
@@ -77,22 +161,18 @@ load all possible proteins from Drugbank and put only all as protein defined pro
 
 
 def load_proteins_from_drugbank_into_dict():
-    query = '''MATCH (n:Protein_DrugBank) RETURN n, labels(n) ;'''
-    results = g.run(query)
-    counter_not_a_protein = 0
-    generate_csv = open('protein/proteins.csv', 'w')
-    writer = csv.writer(generate_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    writer.writerow(['id','name'])
 
-    # all queries which are used to integrate Protein with the extra labels into Hetionet
+
+    # conncet the
     cypherfile = open('protein/cypher_protein.cypher', 'w')
-    query = '''Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:/home/cassandra/Dokumente/Project/master_database_change/mapping_and_merging_into_hetionet/drugbank/protein/proteins.csv" As line MATCH (n:Protein_DrugBank{identifier:line.id}) Create (g:Protein{identifier:n.identifier, '''
-    for property in list_of_all_properties_of_proteins:
-        query += property + ':n.' + property + ', '
-
-
-    query = query[:-2] + '''});\n'''
+    # this is only for the first time to have only the sequences as property and not with header
+    query='''Match (n:Protein) Where exists(n.as_sequence) Set n.as_sequence=split(n.as_sequence,':')[1];\n'''
     cypherfile.write(query)
+
+    query = '''Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:/home/cassandra/Dokumente/Project/master_database_change/mapping_and_merging_into_hetionet/drugbank/protein/proteins.csv" As line MATCH (n:Protein_DrugBank{identifier:line.id}) ,(p:Protein{identifier:line.uniport}) Create (p)-[:equal_to_DB_protein]->(n) Set p.drugbank='yes', p.resource=p.resource+ 'DrugBank', p.locus=n.locus, p.molecular_weight=n.molecular_weight, p.as_sequence=split(line.sequences,'|'),p.pfams=split(line.pfams,'|') ;\n'''
+
+    cypherfile.write(query)
+    # all queries which are used to integrate Protein with the extra labels into Hetionet
     query = '''Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:/home/cassandra/Dokumente/Project/master_database_change/mapping_and_merging_into_hetionet/drugbank/protein/proteins_carrier.csv" As line MATCH (g:Protein{identifier:line.id}) Set g:Carrier ;\n'''
     cypherfile.write(query)
     query = '''Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:/home/cassandra/Dokumente/Project/master_database_change/mapping_and_merging_into_hetionet/drugbank/protein/proteins_enzyme.csv" As line MATCH (g:Protein{identifier:line.id}) Set g:Enzyme ;\n'''
@@ -105,16 +185,10 @@ def load_proteins_from_drugbank_into_dict():
     cypherfile.write(query)
     cypherfile.close()
 
-    query='''Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:/home/cassandra/Dokumente/Project/master_database_change/mapping_and_merging_into_hetionet/drugbank/protein/uniprot_gene/db_uniprot_to_gene_rela.csv" As line MATCH (n:Protein_DrugBank{identifier:line.uniprot_id}), (g:Gene{identifier:toInteger(line.gene_id)}) Create (g)-[:PRODUCES_GpP]->(n);'''
-    cypher_rela.write(query)
-
     file_uniprots_with_multiple= open('protein/uniprot_gene/db_uniprot_to_multi_genes.csv','w')
     writer_multi=csv.writer(file_uniprots_with_multiple)
     writer_multi.writerow(['uniprot_id','protein_name','genes','alternative_ids'])
 
-    file_uniprots_gene_rela = open('protein/uniprot_gene/db_uniprot_to_gene_rela.csv', 'w')
-    writer_rela = csv.writer(file_uniprots_gene_rela)
-    writer_rela.writerow(['uniprot_id',  'gene_id','alternative_ids'])
 
     file_uniprots_without_rela = open('protein/uniprot_gene/db_uniprot_without_rela.csv', 'w')
     writer_without_rela = csv.writer(file_uniprots_without_rela)
@@ -124,13 +198,24 @@ def load_proteins_from_drugbank_into_dict():
     writer_rela_with_alternative = csv.writer(file_uniprots_gene_with_alternative)
     writer_rela_with_alternative.writerow(['uniprot_id','alternative id','gene(s)'])
 
+    query = '''MATCH (n:Protein_DrugBank) RETURN n, labels(n) ;'''
+    results = g.run(query)
+    counter_not_a_protein = 0
+
     counter_uniprot_with_no_gene_ids=0
     for node, labels, in results:
+
+        # input list for protein
+        list_input_protein = []
+
         identifier = node['identifier']
         alternative_uniprot_ids= node['alternative_uniprot_id'] if 'alternative_uniprot_id' in node else []
 
         if not identifier in dict_be_identifier_sort_to_protein_or_not or dict_be_identifier_sort_to_protein_or_not[
             identifier]:
+
+            list_input_protein.append(identifier)
+
             part_dict = dict(node)
             part_dict['labels'] = labels
             name = part_dict['name'] if 'name' in part_dict else ''
@@ -140,34 +225,59 @@ def load_proteins_from_drugbank_into_dict():
                 dict_db_labels_to_csv_label_file[label].writerow([identifier])
             dict_proteins[identifier] = labels
 
-            writer.writerow([identifier,name])
-            if identifier in dict_uniprot_count_genes:
-                writer_multi.writerow([identifier,name,dict_uniprot_to_gene_id[identifier]])
-            elif identifier in dict_uniprot_to_gene_id:
-                writer_rela.writerow([identifier,dict_uniprot_to_gene_id[identifier][0]])
-            else:
-                found_at_least_one_mapping=False
-                list_alternative_ids_mapping=[]
-                set_list_genese_of_alternative_ids=set([])
-                for alternative_uniprot_id in alternative_uniprot_ids:
-                    if alternative_uniprot_id in dict_uniprot_count_genes:
-                        list_alternative_ids_mapping.append(alternative_uniprot_id)
-                        set_list_genese_of_alternative_ids.union(dict_uniprot_to_gene_id[alternative_uniprot_id])
-                        writer_rela_with_alternative.writerow([identifier,alternative_uniprot_id,dict_uniprot_to_gene_id[alternative_uniprot_id]])
-                        found_at_least_one_mapping=True
-                    elif alternative_uniprot_id in dict_uniprot_to_gene_id:
-                        list_alternative_ids_mapping.append(alternative_uniprot_id)
-                        set_list_genese_of_alternative_ids.add(dict_uniprot_to_gene_id[alternative_uniprot_id][0])
-                        writer_rela_with_alternative.writerow([identifier,alternative_uniprot_id,dict_uniprot_to_gene_id[alternative_uniprot_id][0]])
-                        found_at_least_one_mapping=True
-                if not found_at_least_one_mapping:
-                    writer_without_rela.writerow([identifier,name])
-                    counter_uniprot_with_no_gene_ids+=1
+            if identifier in dict_hetionet_protein:
+                print('ok')
+                if len(dict_hetionet_protein[identifier])==1:
+                    protein_hetionet=dict_hetionet_protein[identifier][0]
+                    list_input_protein.append(protein_hetionet['identifier'])
+
+                    interagte_infos_into_csv(part_dict, protein_hetionet, list_input_protein)
+
                 else:
-                    if len(set_list_genese_of_alternative_ids)==1:
-                        writer_rela.writerow([identifier, list(set_list_genese_of_alternative_ids)[0],list_alternative_ids_mapping])
-                    else:
-                        writer_multi.writerow([identifier, name, list(set_list_genese_of_alternative_ids),list_alternative_ids_mapping])
+                    print(len(dict_hetionet_protein[identifier]))
+                    print(identifier)
+                    for protein_hetionet in dict_hetionet_protein[identifier]:
+                        list_multiple_input=[]
+                        list_multiple_input.append(identifier)
+                        list_multiple_input.append(protein_hetionet['identifier'])
+                        print(protein_hetionet['identifier'])
+                        interagte_infos_into_csv(part_dict, protein_hetionet, list_multiple_input)
+                    print('multiple mapping')
+            elif identifier in dict_be_identifier_sort_to_protein_or_not:
+                print('protein without uniprot id')
+                # print(node)
+
+            else:
+                print('not found')
+                # print(node)
+                # sys.exit('not found')
+            # if identifier in dict_uniprot_count_genes:
+            #     writer_multi.writerow([identifier,name,dict_uniprot_to_gene_id[identifier]])
+            # elif identifier in dict_uniprot_to_gene_id:
+            #     writer_rela.writerow([identifier,dict_uniprot_to_gene_id[identifier][0]])
+            # else:
+            #     found_at_least_one_mapping=False
+            #     list_alternative_ids_mapping=[]
+            #     set_list_genese_of_alternative_ids=set([])
+            #     for alternative_uniprot_id in alternative_uniprot_ids:
+            #         if alternative_uniprot_id in dict_uniprot_count_genes:
+            #             list_alternative_ids_mapping.append(alternative_uniprot_id)
+            #             set_list_genese_of_alternative_ids.union(dict_uniprot_to_gene_id[alternative_uniprot_id])
+            #             writer_rela_with_alternative.writerow([identifier,alternative_uniprot_id,dict_uniprot_to_gene_id[alternative_uniprot_id]])
+            #             found_at_least_one_mapping=True
+            #         elif alternative_uniprot_id in dict_uniprot_to_gene_id:
+            #             list_alternative_ids_mapping.append(alternative_uniprot_id)
+            #             set_list_genese_of_alternative_ids.add(dict_uniprot_to_gene_id[alternative_uniprot_id][0])
+            #             writer_rela_with_alternative.writerow([identifier,alternative_uniprot_id,dict_uniprot_to_gene_id[alternative_uniprot_id][0]])
+            #             found_at_least_one_mapping=True
+            #     if not found_at_least_one_mapping:
+            #         writer_without_rela.writerow([identifier,name])
+            #         counter_uniprot_with_no_gene_ids+=1
+            #     else:
+            #         if len(set_list_genese_of_alternative_ids)==1:
+            #             writer_rela.writerow([identifier, list(set_list_genese_of_alternative_ids)[0],list_alternative_ids_mapping])
+            #         else:
+            #             writer_multi.writerow([identifier, name, list(set_list_genese_of_alternative_ids),list_alternative_ids_mapping])
 
 
         else:
@@ -206,43 +316,6 @@ def generate_csv_componet_rela():
 
 
 
-#dictionary from uniprot to gene id
-dict_uniprot_to_gene_id={}
-
-#dict_uniprot_count_genes
-dict_uniprot_count_genes={}
-
-'''
-Find mapping between genes and proteins
-'''
-def get_all_genes():
-    query='''MATCH (n:Gene) RETURN n.identifier, n.uniProtIDs'''
-    results=g.run(query)
-    counter_uniprot_to_multiple_genes=0
-    for gene_id, list_uniprots, in results:
-        if list_uniprots:
-            for uniprot in list_uniprots:
-                # generate dictionary for uniprot to gene(s)
-                # and if more than two genes fit to one uniprot id they will be count
-                if uniprot in dict_uniprot_to_gene_id:
-                    dict_uniprot_to_gene_id[uniprot].append(gene_id)
-                    dict_uniprot_count_genes[uniprot]=1+dict_uniprot_count_genes[uniprot] if uniprot in dict_uniprot_count_genes else 2
-                    counter_uniprot_to_multiple_genes+=1
-                else:
-                    dict_uniprot_to_gene_id[uniprot]=[gene_id]
-
-    file=open('protein/uniprot_gene/uniprot_to_multi_genes.csv','w')
-    writer=csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    writer.writerow(['uniprot','gene_ids'])
-    for uniprot in dict_uniprot_count_genes.keys():
-        string_gene_list='|'.join(str(x) for x in dict_uniprot_to_gene_id[uniprot])
-        writer.writerow([uniprot, string_gene_list])
-
-    print('number of uniprots which appears in multiple genes:'+str(counter_uniprot_to_multiple_genes))
-    print(len(dict_uniprot_to_gene_id))
-    print(len(dict_uniprot_count_genes))
-    print(dict_uniprot_count_genes)
-
 
 def main():
     print(datetime.datetime.utcnow())
@@ -265,7 +338,7 @@ def main():
 
     print('load all genes and gather the information for uniprot gene')
 
-    get_all_genes()
+    load_all_hetionet_proteins_in_dictionary()
 
     print(
         '#################################################################################################################################################################')
@@ -283,7 +356,7 @@ def main():
 
     print('load all DrugBank has component rela and write them in csv and generate cypher file for integration')
 
-    # generate_csv_componet_rela()
+    generate_csv_componet_rela()
 
     print(
         '#################################################################################################################################################################')
