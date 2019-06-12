8c5458ef98f5e18632407d8dcfa02a0a4e4533b9
ck_linux <dragon493@web.de>
2019-06-12 13:53:25 +0200

    exact path

--- import_into_Neo4j/mondo/generate_cypher_file.py
+++ import_into_Neo4j/mondo/generate_cypher_file.py
@@ -23,13 +23,13 @@ Generate cypher file with APOC
 '''
 def generate_cypher_file_with_apoc(save_path):
     # generate cypher file for the most of the mondo nodes with their relationships
-    query = '''call apoc.export.cypher.query("MATCH (n:disease)-[r]->(b:disease) Where n.`http://www.geneontology.org/formats/oboInOwl#id` contains 'MONDO' and b.`http://www.geneontology.org/formats/oboInOwl#id` contains 'MONDO' RETURN n,r,b","''' + save_path + '''mondo.cypher", {batchSize:10000}); '''
+    query = '''call apoc.export.cypher.query("MATCH (n:disease)-[r]->(b:disease) Where n.`http://www.geneontology.org/formats/oboInOwl#id` contains 'MONDO' and b.`http://www.geneontology.org/formats/oboInOwl#id` contains 'MONDO' RETURN n,r,b","file:/home/cassandra/Dokumente/Project/master_database_change/import_into_Neo4j/mondo/mondo.cypher", {batchSize:10000}); '''
     #this is query for cypher shell, but this do not work realy for neo4j 3.2.9
     # query='''call apoc.export.cypher.query("MATCH (n:disease)-[r]->(b:disease) Where n.`http://www.geneontology.org/formats/oboInOwl#id` contains 'MONDO' and b.`http://www.geneontology.org/formats/oboInOwl#id` contains 'MONDO' RETURN n,r,b","'''+save_path+'''mondo.cypher", {batchSize:10000,format:'cypher-shell'}); '''
     g.run(query)
 
     # the only nod that has no connection to another disease node with a mondo id (MONDO:0013239)
-    query= '''call apoc.export.cypher.query("MATCH (n:disease) Where n.`http://www.geneontology.org/formats/oboInOwl#id`='MONDO:0013239' Return n","'''+save_path+'''single_node_without_connection.cypher", {batchSize:10}); '''
+    query= '''call apoc.export.cypher.query("MATCH (n:disease) Where n.`http://www.geneontology.org/formats/oboInOwl#id`='MONDO:0013239' Return n","file:/home/cassandra/Dokumente/Project/master_database_change/import_into_Neo4j/mondo/single_node_without_connection.cypher", {batchSize:10}); '''
     g.run(query)
 
 
