0bae4832cbed8763614a1db64bdaa43afd48043b
ck_linux <dragon493@web.de>
2019-08-26 14:35:21 +0200

    update

--- import_into_Neo4j/mondo/integrate_mondo_and_add_level.sh
+++ import_into_Neo4j/mondo/integrate_mondo_and_add_level.sh
@@ -4,50 +4,57 @@ mondo="mondo.cypher"
 echo $mondo
 
 
-
-#define path to neo4j bin
-#path_neo4j=/home/cassandra/Dokumente/hetionet/neo4j-community-3.1.6/bin
-path_neo4j=$1
-if [[ $# -eq 0 ]]
+if [[ $# -ne 2 ]]
 then
-    echo "path to neo4j is missing"
+    echo "path to neo4j is missing path to newest neo4j and save path"
     exit 1
 fi
+#define path to neo4j bin
+#path_neo4j=/home/cassandra/Dokumente/hetionet/neo4j-community-3.1.6/bin
+path_neo4j_original=$1
+path_neo4j_newest_version=$2
+
+
 
 now=$(date +"%F %T")
 echo "Current time: $now"
-read -p "Press any key if monarch scigraph in neo4j is started (APOC is needed as a plugin)" -n1 -s
-echo ''
 
-echo -n "Enter path to dictionary to save the cypher files (with / at the end):"
-read  var_path
-echo ''
+$path_neo4j_original/neo4j stop
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+echo start the newerst version
+
+$path_neo4j_newest_version graph restart $path_neo4j_newest_version 
+
+sleep 180
 
 #generate the cypher files from the neo4j database with apoc
-python generate_cypher_file.py $var_path > output_program.txt
-echo $var_path
-
+python generate_cypher_file.py > output_program.txt
 
 now=$(date +"%F %T")
 echo "Current time: $now"
-read -p "Press any key if the original database in neo4j is started " -n1 -s
-echo ''
 
+$path_neo4j_newest_version graph stop
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+echo start the original version
+
+$path_neo4j_original/neo4j start
 
 #integrate the mondo information into the other database
-path_mondo="$var_path/$mondo"
-$path_neo4j/neo4j-shell -file $path_mondo > output_cypher_integration.txt
+$path_neo4j_original/neo4j-shell -file $mondo > output_cypher_integration.txt
 echo $path_mondo
 
 now=$(date +"%F %T")
 echo "Current time: $now"
 
-path_singel="$var_path/single_node_without_connection.cypher"
-$path_neo4j/neo4j-shell -file $path_singel > output_cypher_integration_singel.txt
+$path_neo4j_original/neo4j-shell -file single_node_without_connection.cypher > output_cypher_integration_singel.txt
 
-sleep 180j
+sleep 180
 
-$path_neo4j/neo4j restart
+$path_neo4j_original/neo4j restart
 
 
 sleep 120
@@ -60,17 +67,29 @@ python nodes_without_a_upper_node.py > output_nodes_without_parents.txt
 
 now=$(date +"%F %T")
 echo "Current time: $now"
-read -p "Press any key if monarch scigraph in neo4j is started" -n1 -s
-echo ''
-echo found parents
 
+$path_neo4j_original/neo4j stop
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+echo start the newerst version
+
+$path_neo4j_newest_version graph restart
+
+echo get a parent
 python find_connection_for_nodes_without_upper_nodes.py > output_found_parents.txt
 
 now=$(date +"%F %T")
 echo "Current time: $now"
-read -p "Press any key if in original database in neo4j is started" -n1 -s
-echo ''
-_hi
+
+$path_neo4j_newest_version graph stop
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+echo start the newerst version
+
+path_neo4j_original/neo4j start
+
 now=$(date +"%F %T")
 echo "Current time: $now"
 echo hierarchy
@@ -79,12 +98,11 @@ python get_hierarchy_mondo.py > output_hierarchy.txt
 now=$(date +"%F %T")
 echo "Current time: $now"
 
-path_singel="$var_path/single_node_without_connection.cypher"
-$path_neo4j/neo4j-shell -file integrate_level.cypher > output_cypher_integration_hierarchy.txt
+$path_neo4j_original/neo4j-shell -file integrate_level.cypher > output_cypher_integration_hierarchy.txt
 
 sleep 180
 
-$path_neo4j/neo4j restart
+$path_neo4j_original/neo4j restart
 
 sleep 120
 
