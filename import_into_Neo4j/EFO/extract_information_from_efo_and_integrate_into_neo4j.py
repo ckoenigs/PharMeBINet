441137d62371a82fabbd2520dfde56929ce1ba30
ck_linux <dragon493@web.de>
2019-08-26 14:34:41 +0200

    update

--- import_into_Neo4j/EFO/extract_information_from_efo_and_integrate_into_neo4j.py
+++ import_into_Neo4j/EFO/extract_information_from_efo_and_integrate_into_neo4j.py
@@ -6,7 +6,7 @@ Created on Fri Jan 19 11:46:22 2018
 """
 
 import sys
-import datetime
+import datetime,csv
 from itertools import groupby
 
 
@@ -20,11 +20,17 @@ def is_data(line):
 # file path to HPO data source
 file_name = 'efo.obo'
 
-# dictionary with hierarchical upper identifier and all of the leafs
-dict_hierarchical_set = {}
-
 # dictionary with all efo identifier and a dictionary for all information of this identifier
 dict_all_efo_entries = {}
+
+#all properties
+set_all_properties_in_efo=set([])
+
+#list of all properties which are lists
+set_list_properties=set([])
+
+#set list of all parent-child pairs
+set_parent_child_pair=set([])
 
 '''
 group the epo terms together and get the information from the different terms and gather all term information in a dictionary
@@ -48,6 +54,7 @@ def get_efo_information_and_map_to_umls():
                 #                print(group.__next__())
                 # header = group.__next__().rstrip('\n')
 
+                #take only the information from the terms
                 if header.find('[Term]') == -1:
                     continue
 
@@ -65,46 +72,21 @@ def get_efo_information_and_map_to_umls():
                     #                            is_a_frequency_information = True
                     if key_term == 'is_a':
                         parent_id = value.split('!')[0].strip()
+                        set_parent_child_pair.add((parent_id,dict_all_info['id'][0]))
 
-                        if parent_id in dict_hierarchical_set:
-                            dict_hierarchical_set[parent_id].add(dict_all_info['id'][0])
-                        else:
-                            dict_hierarchical_set[parent_id] = set([dict_all_info['id'][0]])
-
-                    # for som properties more than one value appears
-                    if not key_term in dict_all_info:
-                        dict_all_info[key_term] = [value]
                     else:
-                        dict_all_info[key_term].append(value)
+                        # for som properties more than one value appears
+                        if not key_term in dict_all_info:
+                            dict_all_info[key_term] = value.replace('"', '').replace("'", "").replace("\\", "")
+                        else:
+                            dict_all_info[key_term]+='|'+value.replace('"', '').replace("'", "").replace("\\", "")
+                            set_list_properties.add(key_term)
+                        set_all_properties_in_efo.add(key_term)
+
+
 
                 dict_all_efo_entries[dict_all_info['id'][0]] = dict_all_info
 
-
-'''
-complete the leaves of the  with recursion
-'''
-
-
-def get_all_leaves_and_internal_nodes(node_id):
-    # type: (node id) -> set of node ids
-    all_nodes = dict_hierarchical_set[node_id]
-    set_of_leaves_and_inner_nodes= dict_hierarchical_set[node_id]
-    for node in all_nodes:
-        if node in dict_hierarchical_set:
-            set_of_leaves_and_inner_nodes = set_of_leaves_and_inner_nodes.union(
-                get_all_leaves_and_internal_nodes(node))
-    return set_of_leaves_and_inner_nodes
-
-    #                    print(key_term)
-    #                    print(value)
-
-
-# file counter
-file_counter = 1
-# maximal number of queries for a commit block
-constrain_number = 20000
-# maximal number of queries in a file
-creation_max_in_file = 1000000
 
 '''
 generate cypher file
@@ -112,61 +94,48 @@ generate cypher file
 
 
 def generate_cypher_file(all_child_of_disease):
-    global file_counter
-    counter_create = 0
-    # list_of_all_diseases = list(dict_hierarchical_set['EFO:0000408'])
-    list_of_all_diseases = list(all_child_of_disease)
-    cypher_file = open('integrate_efo_disease_into_neo4j_' + str(file_counter) + '.cypher', 'w')
-    file_counter += 1
-    cypher_file.write('begin \n')
-
-    for disease_id in list_of_all_diseases:
-        create_text = 'Create (:EFOdisease{'
-        # add all properties to node which hpo has
-        for key, list_information in dict_all_efo_entries[disease_id].items():
-            modify_list_information = []
-            for info in list_information:
-                info = info.replace('"', '').replace("'", "").replace("\\", "")
-                modify_list_information.append(info)
-            create_text = create_text + key + ": '" + "|".join(modify_list_information) + "' ,"
-        create_text = create_text[0:-1] + '});\n'
-        counter_create += 1
-        cypher_file.write(create_text)
-        # test if a new commit block or new file need to be generated
-        if counter_create % constrain_number == 0:
-            cypher_file.write('commit \n')
-            if counter_create % creation_max_in_file == 0:
-                cypher_file.close()
-                cypher_file = open('integrate_efo_disease_into_neo4j_' + str(file_counter) + '.cypher', 'w')
-                cypher_file.write('begin \n')
-                file_counter += 1
-            else:
-                cypher_file.write('begin \n')
-    print(counter_create)
-    cypher_file.write('commit \n begin \n')
+    #create cypher file
+    cypher_file=open('cypher.cypher','w')
+    cypher_file.write('begin\n')
+    query=''' Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:/home/cassandra/Dokumente/Project/master_database_change/import_into_Neo4j/aeolus/node.csv" As line Create (:EFOdisease{'''
+    for property in set_all_properties_in_efo:
+        if not property in set_list_properties :
+            query+=property+':line.'+property+', '
+        else:
+            query += property + ':split(line.' + property + ',"|"), '
+    query=query[:-2]+'});\n'
+    cypher_file.write(query)
+    cypher_file.write('commit\nbegin\n')
     cypher_file.write('Create Constraint On (node:EFOdisease) Assert node.id Is Unique; \n')
-    cypher_file.write('commit \n begin \n')
-
-    for disease_id in list_of_all_diseases:
-        if disease_id in dict_hierarchical_set:
-            for child_disease  in list(dict_hierarchical_set[disease_id]):
-                query=''' Match (a:EFOdisease{id:"%s"}), (b:EFOdisease{id:"%s"}) 
-                 Create (a)-[:is_a_efo]->(b); \n'''
-                query =query %(child_disease, disease_id)
-                counter_create += 1
-                cypher_file.write(query)
-                # test if a new commit block or new file need to be generated
-                if counter_create % constrain_number == 0:
-                    cypher_file.write('commit \n')
-                    if counter_create % creation_max_in_file == 0:
-                        cypher_file.close()
-                        cypher_file = open('integrate_efo_disease_into_neo4j_' + str(file_counter) + '.cypher', 'w')
-                        cypher_file.write('begin \n')
-                        file_counter += 1
-                    else:
-                        cypher_file.write('begin \n')
     cypher_file.write('commit')
-    print(counter_create)
+
+    # create node csv
+    file=open('node.csv','w')
+    csv_writer=csv.DictWriter(file,fieldnames=list(set_all_properties_in_efo))
+    csv_writer.fieldnames()
+
+    for disease_id, dict_value in dict_all_efo_entries.items():
+        # add all properties of a node into csv file
+        csv_writer.writerow(dict_value)
+    file.close()
+
+    #add query for relationship integration
+    cypher_file.write('begin')
+    query = ''' Match (a:EFOdisease{id:line.child_id}), (b:EFOdisease{id:line.parent_id}) 
+                     Create (a)-[:is_a_efo]->(b); \n'''
+    cypher_file.write(query)
+    cypher_file.write('commit')
+
+    #create csv for relationships
+    # create node csv
+    file = open('node.csv', 'w')
+    csv_writer = csv.writer(file)
+    csv_writer.writerow(['child_id','parent_id'])
+
+    for (parent_id,child_id) in set_parent_child_pair:
+        csv_writer.writerow([child_id,parent_id])
+    file.close()
+
 
 
 def main():
@@ -179,21 +148,12 @@ def main():
 
     get_efo_information_and_map_to_umls()
 
-    print(dict_hierarchical_set['EFO:0000408'])
-    print(len(list(dict_hierarchical_set['EFO:0000408'])))
-
-    print('##########################################################################')
-
-    all_child_nodes_of_disease=get_all_leaves_and_internal_nodes('EFO:0000408')
-
-    print(dict_hierarchical_set['EFO:0000408'])
-    print(len(list(dict_hierarchical_set['EFO:0000408'])))
     print('##########################################################################')
 
     print(datetime.datetime.utcnow())
-    print('put all relationship information into a cypher file')
+    print('put all relationship and node information into a csv and create a cypher file')
 
-    generate_cypher_file(all_child_nodes_of_disease)
+    generate_cypher_file()
 
     print('##########################################################################')
 
