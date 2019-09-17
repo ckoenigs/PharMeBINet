7366d9cfdefb7ff5000abe0e46c86bdb17df5123
ck_linux <dragon493@web.de>
2019-09-17 10:28:50 +0200

    update

---
+++ mapping_and_merging_into_hetionet/drugbank/script_mapping_drugbank.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#define path to neo4j bin
+path_neo4j=$1
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+echo 'integrate Drugbank drugs with interaction into Hetionet'
+
+python integrate_DrugBank_with_interaction_into_hetionet.py > output_integration_file_generation.txt
+
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+
+$path_neo4j/neo4j-shell -file cypher.cypher > output_cypher.txt
+
+sleep 180
+$path_neo4j/neo4j restart
+sleep 120
+
