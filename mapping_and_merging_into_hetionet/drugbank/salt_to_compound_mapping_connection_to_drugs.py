24e418b0fc4e5a03a580f59e668dd66b3902bf67
ck_linux <dragon493@web.de>
2019-09-18 13:53:24 +0200

    update

--- mapping_and_merging_into_hetionet/drugbank/salt_to_compound_mapping_connection_to_drugs.py
+++ mapping_and_merging_into_hetionet/drugbank/salt_to_compound_mapping_connection_to_drugs.py
@@ -20,7 +20,7 @@ def create_connection_with_neo4j():
     global g
     g = Graph("http://localhost:7474/db/data/")
 
-# dictionary of not mapped compound
+# dictionary of not mapped compound inchikey to node
 dict_not_mapped_compound={}
 
 '''
@@ -30,20 +30,49 @@ def find_not_mapped_compounds_and_add_to_dict():
     query='''MATCH (n:Compound) WHere not exists(n.drugbank) RETURN n'''
     result=g.run(query)
     for node, in result:
-        id=node['identifier']
-        dict_not_mapped_compound[id]=dict(node)
+        inchikey=node['inchikey']
+        dict_not_mapped_compound[inchikey]=dict(node)
+
+#label of salt
+label_of_salt='Salt_DrugBank'
+
+# name of the csv file
+file_node='salt_compound'
+
+#name rela file
+file_rela='salt_compound_rela'
 
 '''
 Create cypher and csv files for nodes and relationships
 '''
 def create_cypher_and_csv_files():
+    # open cypher file
+    cypher_file=open('cypher_salt.cypher','w')
     # get properties of salt nodes
-    query='''MATCH (p:Salt_DrugBank) WITH DISTINCT keys(p) AS keys UNWIND keys AS keyslisting WITH DISTINCT keyslisting AS allfields RETURN allfields;'''
+    query='''MATCH (p:%s) WITH DISTINCT keys(p) AS keys UNWIND keys AS keyslisting WITH DISTINCT keyslisting AS allfields RETURN allfields;'''
+    query=query %(label_of_salt)
     result=g.run(query)
     header=[]
-    query_start=''''''
+    query_start='''Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:/home/cassandra/Dokumente/Project/master_database_change/mapping_and_merging_into_hetionet/ncbi_gene/drugbank/output/%s.csv" As line Fieldterminator '\\t' Match (a:Salt_DrugBank {identifier:line.identifier}) Create (b:Compound :Salt{'''
+    query_start=query_start %(file_node, label_of_salt)
     for property, in result:
         header.append(property)
+        query_start+= property+':line.'+property+', '
+    query=query_start+' source:"DrugBank", drugbank="yes", resource:["DrugBank"], url:"https://www.drugbank.ca/salts/"+line.identifier}) Create (b)-[:equal_to_drugbank]->(a)'
+
+    cypher_file.write(query)
+    node_file=open('output/'+file_node+'.csv', 'w')
+    rela_file=open('output/'+file_rela+'.csv','w')
+    global csv_node, csv_rela
+    csv_node=csv.DictWriter(node_file,fieldnames=header,delimiter='\t')
+    rela_header=['salt_id','compound_id']
+    csv_rela=csv.writer(rela_file,delimiter='\t')
+    csv_rela.writerow(rela_header)
+
+
+#bash shell for merge doids into the mondo nodes
+bash_shell=open('merge_nodes_salt.sh','w')
+bash_shell.write('#!/bin/bash\n')
 
 '''
 Gather all salt and make a new csv to integrate them as compound into neo4j
@@ -54,6 +83,27 @@ also check on the drugs which did not mapped, because some of them might be now 
 def prepare_node_csv():
     query='''MATCH (n:Salt_DrugBank) RETURN n'''
     result=g.run(query)
+    for node, in result:
+        csv_node.writerow(node)
+        node_id=node['identifier']
+        inchikey=node['inchikey']
+        if inchikey in dict_not_mapped_compound:
+            compound_id=dict_not_mapped_compound[inchikey]['identifier']
+            #if it mapped to a not mapped compound
+            text = 'python ../add_information_from_a_not_existing_node_to_existing_node.py %s %s %s\n' % (
+                compound_id, node_id, 'Compound')
+            bash_shell.write(text)
+            text = '''now=$(date +"%F %T")\n echo "Current time: $now"\n'''
+            bash_shell.write(text)
+
+'''
+Generate fill the rela csv file
+'''
+def fill_rela_csv():
+    query = '''MATCH (n:Salt_DrugBank)-[:has_ChS]-(b:Compound_DrugBank) RETURN n.identifier, b.identifier'''
+    result = g.run(query)
+    for salt_id, compound_id, in result:
+        csv_rela.writerow([salt_id, compound_id])
 
 def main():
     print(datetime.datetime.utcnow())
@@ -73,53 +123,26 @@ def main():
         '#################################################################################################################################################################')
 
     print(datetime.datetime.utcnow())
-    print('load all hetionet compound in dictionary')
+    print('open and create cypher and csv files')
 
-    load_all_hetionet_compound_in_dictionary()
-
-    print(
-        '#################################################################################################################################################################')
-
-    print(datetime.datetime.utcnow())
-
-    print('load all DrugBank compounds in dictionary')
-
-    load_all_DrugBank_compound_in_dictionary()
-
-    print(
-        '#################################################################################################################################################################')
-
-    print(datetime.datetime.utcnow())
-    print('load all connection in dictionary')
-
-    load_in_all_interaction_connection_from_drugbank_in_dict()
+    create_cypher_and_csv_files()
 
     print(
         '#################################################################################################################################################################')
 
     print(datetime.datetime.utcnow())
 
-    print('integrate drugbank into hetionet')
+    print('prepare node csv and make bash for merging not mapped compounds to salt')
 
-    integrate_DB_compound_information_into_hetionet()
-
-    print(
-        '#################################################################################################################################################################')
-
-    print(datetime.datetime.utcnow())
-
-    print('create cypher queries and cypher file')
-
-    create_cypher_file()
+    prepare_node_csv()
 
     print(
         '#################################################################################################################################################################')
 
     print(datetime.datetime.utcnow())
+    print('create rela')
 
-    print('generate cypher file for interaction')
-
-    genration_of_interaction_file()
+    fill_rela_csv()
 
     print(
         '#################################################################################################################################################################')
