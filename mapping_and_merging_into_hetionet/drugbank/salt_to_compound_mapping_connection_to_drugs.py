02e265451d347f739d9595b84731ddbcf9436dc9
ck_linux <dragon493@web.de>
2019-09-18 12:56:26 +0200

    update

--- mapping_and_merging_into_hetionet/drugbank/salt_to_compound_mapping_connection_to_drugs.py
+++ mapping_and_merging_into_hetionet/drugbank/salt_to_compound_mapping_connection_to_drugs.py
@@ -21,17 +21,39 @@ def create_connection_with_neo4j():
     g = Graph("http://localhost:7474/db/data/")
 
 # dictionary of not mapped compound
-
+dict_not_mapped_compound={}
 
 '''
 Load all compounds which did not mapped and upload them into da dictionary
 '''
-
+def find_not_mapped_compounds_and_add_to_dict():
+    query='''MATCH (n:Compound) WHere not exists(n.drugbank) RETURN n'''
+    result=g.run(query)
+    for node, in result:
+        id=node['identifier']
+        dict_not_mapped_compound[id]=dict(node)
 
 '''
+Create cypher and csv files for nodes and relationships
+'''
+def create_cypher_and_csv_files():
+    # get properties of salt nodes
+    query='''MATCH (p:Salt_DrugBank) WITH DISTINCT keys(p) AS keys UNWIND keys AS keyslisting WITH DISTINCT keyslisting AS allfields RETURN allfields;'''
+    result=g.run(query)
+    header=[]
+    query_start=''''''
+    for property, in result:
+        header.append(property)
+
+'''
+Gather all salt and make a new csv to integrate them as compound into neo4j
+also check if a salt is on of the not mapped compounds 
 Prepare the csv for salt integration, because they are not in hetionet they can be directly integrated
 also check on the drugs which did not mapped, because some of them might be now salts
 '''
+def prepare_node_csv():
+    query='''MATCH (n:Salt_DrugBank) RETURN n'''
+    result=g.run(query)
 
 def main():
     print(datetime.datetime.utcnow())
@@ -43,9 +65,9 @@ def main():
         '#################################################################################################################################################################')
 
     print(datetime.datetime.utcnow())
-    print('load all properties of compound and drugbank compound and use the information to genreate csv files')
+    print('get compound which are not ind drugbank drugs')
 
-    get_mondo_properties_and_generate_csv_files()
+    find_not_mapped_compounds_and_add_to_dict()
 
     print(
         '#################################################################################################################################################################')
