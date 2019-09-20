abf06346168952c72531f8806cad4c529a76ea3f
ck_linux <dragon493@web.de>
2019-09-20 07:50:22 +0200

    update

--- mapping_and_merging_into_hetionet/drugbank/script_mapping_drugbank.sh
+++ mapping_and_merging_into_hetionet/drugbank/script_mapping_drugbank.sh
@@ -22,3 +22,34 @@ sleep 120
 chmod 775 merge_nodes.sh
 
 ./merge_nodes.sh > output_merge_compound.txt
+
+
+sleep 180
+$path_neo4j/neo4j restart
+sleep 120
+
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+echo 'integrate Drugbank salts with interaction into Hetionet'
+
+python salt_to_compound_mapping_connection_to_drugs.py > output_integration_file_generation_salt.txt
+
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+
+$path_neo4j/neo4j-shell -file cypher_salt.cypher > output_cypher.txt
+
+sleep 180
+$path_neo4j/neo4j restart
+sleep 120
+
+chmod 775 merge_nodes_salt.sh
+
+./merge_nodes_salt.sh > output_merge_compound.txt
+
+
+sleep 180
+$path_neo4j/neo4j restart
+sleep 120
