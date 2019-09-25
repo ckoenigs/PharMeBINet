9b21f90987a5a7f6fedc0f05d76ab848cc9a7dc9
ck_linux <dragon493@web.de>
2019-09-25 14:42:06 +0200

    update

--- mapping_and_merging_into_hetionet/pathway/switch_identifier_pathway_to_newer_version.py
+++ mapping_and_merging_into_hetionet/pathway/switch_identifier_pathway_to_newer_version.py
@@ -26,8 +26,11 @@ def create_connection_with_neo4j_mysql():
     g = Graph("http://localhost:7474/db/data/")
 
 
-# dictionary with hetionet pathways with identifier as key and value the name
-dict_pathway_hetionet = {}
+# dictionary with hetionet genes with identifier as key and value the name
+dict_genes_hetionet = {}
+
+# label of pathways
+label_pathway='pathway_multi'
 
 '''
 load in all pathways from hetionet in a dictionary
@@ -35,164 +38,148 @@ load in all pathways from hetionet in a dictionary
 
 
 def load_hetionet_pathways_in():
-    query = '''MATCH (n:Pathway) RETURN n.identifier,n.name'''
+    query = '''MATCH (n:Gene) RETURN n.identifier,n.name'''
     results = g.run(query)
 
     for identifier, name, in results:
-        dict_pathway_hetionet[identifier] = name
+        dict_genes_hetionet[identifier] = name
 
-    print('number of pathway nodes in hetionet:' + str(len(dict_pathway_hetionet)))
+    print('number of pathway nodes in hetionet:' + str(len(dict_genes_hetionet)))
 
-# dictionary of pathways which are not in hetionet with they properties: name
-dict_ctd_pathway_not_in_hetionet = {}
+#header of the node csv
+header=[]
 
-# dictionary of ctd pathways which are in hetionet with properties: name
-dict_ctd_pathway_in_hetionet = {}
+'''
+get the properties of pathway and make the list for the header of the csv file
+however ignore the properties with genes, because this information will be in the relationships 
+'''
+def get_pathway_properties():
+    query='''MATCH (p:%s) WITH DISTINCT keys(p) AS keys UNWIND keys AS keyslisting WITH DISTINCT keyslisting AS allfields RETURN allfields;'''
+    query= query % (label_pathway)
+    result=g.run(query)
+    for property in result:
+        if property.find('gene')==-1:
+            header.append(property)
 
-# dictionary with all pathways which are mapped to pc or wp with propertie wp, pc id and propertie pathway identifier, name, source
-dict_ctd_pathway_mapped_to_pc_or_wp = {}
+# version string pc
+version_string='PC11_'
 
-# dictionary with all pathways which are not mapped to pc or wp with pathway identifier and properties name, source
-dict_ctd_pathways_not_mapped = {}
+# cypher file
+cypher_file=open('cypher.cypher','w')
+#query to delete all old pathways
+query='MATCH (n:Pathway) Detach Delete n;\n'
+cypher_file.write(query)
 
 # dictionary pathway name from pathway list an the identifier plus the source
-dict_name_to_pc_or_wp_identifier = defaultdict(list)
+dict_name_to_pc_or_wp_identifier = {}
 
 
 # dictionary with own id from pc to name, pcid and sourc
 dict_own_id_to_pcid_and_other={}
 
-# pc maximal number for pc id
-pc_maximal_number_for_id=0
-
 #dictionary of identifier to nodes information
 dict_id_to_node={}
 
+# csv files for nodes and relationship
+file_node=open('output/node.tsv','w')
+csv_node=csv.writer(file_node,delimiter='\t')
+
+file_rela=open('output(rela.tsv','w')
+csv_rela=csv.writer(file_rela,delimiter='\t')
+rela_header=['gene_id','pathway_id']
+csv_rela.writerow(rela_header)
+
 '''
-load in all pathway with the way himmelstein construct of the identifier but for version 9
-https://github.com/dhimmel/pathways
-properties:
-    0: identifier	
-    1:name	
-    2:url	
-    3:n_genes	
-    4:n_coding_genes	
-    5:source	
-    6:license	
-    7:genes	
-    8:own id
-    9:coding_genes
+load all pathway from neo4j
 '''
 
 
-def load_in_all_pathways_from_himmelsteins_construction():
-    # search for the macimal pc number
-    global pc_maximal_number_for_id
+def load_in_all_pathways():
 
     #
-    query = '''MATCH (n:pathway_multi) RETURN n, n.identifier,n.name, n.source,n.own_id '''
+    query = '''MATCH (n:%s) RETURN n ''' %(label_pathway)
     results = g.run(query)
 
-    for node,identifier, name, source, own_id, in results:
+    for node, in results:
+        identifier=node['identifier']
+        if identifier=='PC11_755':
+            print('huhu')
         dict_id_to_node[identifier]=dict(node)
-        # find the highest number of pc
-        if identifier[0:2]=='PC' and int(identifier.split('_')[1])> pc_maximal_number_for_id  :
-            pc_maximal_number_for_id=int(identifier.split('_')[1])
 
         # fill dictionary with name to rest
-        dict_name_to_pc_or_wp_identifier[name].append(dict(node))
-
-        # this is a little bit weird
-        if own_id in dict_own_id_to_pcid_and_other and own_id!="":
-            dict_own_id_to_pcid_and_other[own_id].append([identifier,source, name])
-        elif own_id in dict_own_id_to_pcid_and_other :
-            dict_own_id_to_pcid_and_other[identifier] = [[identifier, source, name]]
+        name=node['name']
+        if not name in dict_name_to_pc_or_wp_identifier:
+            dict_name_to_pc_or_wp_identifier[name]=[dict(node)]
         else:
-            dict_own_id_to_pcid_and_other[own_id]=[[identifier, source, name]]
+            dict_name_to_pc_or_wp_identifier[name].append(dict(node))
+            # print( dict_name_to_pc_or_wp_identifier[name])
+
+        for gene_id in node['genes']:
+            gene_id=int(gene_id)
+            if gene_id in dict_genes_hetionet:
+                csv_rela.writerow([str(gene_id),identifier])
+
+
+
+
     print('number of different pathway names:' + str(len(dict_name_to_pc_or_wp_identifier)))
-    print('number of different pathway ids:'+str(len(dict_own_id_to_pcid_and_other)))
-
-#list of all new identifier which are mapped
-list_mapped_to=[]
-
-# dictionary with multiple mapped new identifier
-dict_old_id_to_multiple_new_id={}
-
-# version string pc
-version_string='PC11_'
 
 '''
-che old version pathway from hetionet map to new version
+fill the node csv file by going through the name dictionary and maybe they nodes with the same name will be merges to one node
 '''
-def switch_id_for_pathways():
-    global pc_maximal_number_for_id
-    counter_exists=0
-    counter_not_exists=0
-    counter_wp=0
-    counter_not_mapped_wp=0
-    for identifier, name in dict_pathway_hetionet.items():
+def generate_node_csv():
+    csv_node.writerow(header)
 
-
-        if name in dict_name_to_pc_or_wp_identifier:
-            list_of_dict= dict_name_to_pc_or_wp_identifier[name]
-            if len(list_of_dict)>1:
-                print(list_of_dict)
-            for dict_node in list_of_dict:
-                identifiers =dict_node['identifier']
-                if identifiers=='PC9_2504':
-                    print('blub')
-                if identifiers in list_mapped_to:
-                #     if identifiers[0:2]==identifier[0:2] or identifiers[0:2]=='WP':
-                    print('multiple mapped new identifier')
-                    print(identifiers)
-                    print(identifier)
-                    dict_old_id_to_multiple_new_id[identifier]=identifiers
-                    continue
-                else:
-                    # if identifiers[0:2] == identifier[0:2] or identifiers[0:2] == 'WP':
-                    list_mapped_to.append(identifiers)
-                #     else:
-                #         continue
-                # own_ids = [x[2] for x in dict_name_to_pc_or_wp_identifier[name]]
-                # own_source= [x[1] for x in dict_name_to_pc_or_wp_identifier[name]]
-                # own_ids= [x for x in own_ids if x]
-                # own=[i+':'+j for i,j in zip(own_source,own_ids)]
-                # own = ','.join(own)
-                # counter_exists+=1
-                if identifier[0:2]=='WP':
-                    counter_wp+=1
-                #     if identifiers==identifier or identifiers[0:2]=='PC':
-                #         continue
-                # if identifier != identifiers:
-
-                # xrefs=own+'","'+identifier if own != '' else identifier
-                query='''Match (c:Pathway{ identifier:"%s"}) Set c.identifier="%s", c.xrefs=["%s"];'''
-
-
-
+    counter_double_names=0
+    counter_multiple=0
+    for name, list_of_nodes in dict_name_to_pc_or_wp_identifier.items():
+        if len(list_of_nodes)==1:
+            list_info=[]
+            for head in header:
+                list_info.append(list_of_nodes[0][head])
+            csv_node.writerow(list_info)
         else:
-            counter_not_exists += 1
-            # if it has not the PC id the id can be the same
-            if identifier[0:2] == 'WP':
-                continue
-            #else it hast to be updated to the newest version (or should be delete?)
-            pc_maximal_number_for_id += 1
-            new_identifier=version_string + str(pc_maximal_number_for_id)
-            query = '''Match (c:Pathway{ identifier:"%s"}) Set c.identifier="%s" , c.xrefs=[];'''
-    #
-    print('number of mapped:' + str(counter_exists))
-    print('number not exists:' + str(counter_not_exists))
-    print(counter_not_mapped_wp)
-    print(counter_wp)
-    print('xrefs for all')
-    query='''  Match (c:Pathway) Where not exists(c.xrefs) Set c.xrefs=[];'''
-    g.run(query)
+            counter_double_names+=1
+            if len(list_of_nodes)>2:
+                counter_multiple+=1
+                print(list_of_nodes)
+                print(len(list_of_nodes))
+            node1=list_of_nodes[0]
+            node2=list_of_nodes[1]
+            genes_1=node1['genes']
+            genes_2 = node2['genes']
+            source_1=node1['source']
+            source_2=node2['source']
+            intersection=set(genes_1).intersection(genes_2)
+            if len(genes_1)>len(genes_2):
+                if len(intersection)!=len(genes_2):
+                    print(node1['identifier'])
+                    print(node2['identifier'])
+                    print(source_1)
+                    print(source_2)
+                    # print(genes_2)
+                    # print(intersection)
+                    print(set(genes_2).difference(intersection))
+                    print('not the same genes')
+                    if source_1==source_2:
+                        print(node1['idOwn']) if 'idOwn' in node1 else ''
+                        print(node2['idOwn']) if 'idOwn' in node2 else ''
+            else:
+                if len(intersection)!=len(genes_1):
+                    print(node1['identifier'])
+                    print(node2['identifier'])
+                    print(source_1)
+                    print(source_2)
+                    # print(genes_1)
+                    # print(intersection)
+                    print(set(genes_1).difference(intersection))
+                    print('not the same genes')
+                    if source_1==source_2:
+                        print(node1['idOwn']) if 'idOwn' in node1 else ''
+                        print(node2['idOwn']) if 'idOwn' in node2 else ''
 
-    print('merge')
-    for old_id, multiple_new_id in dict_old_id_to_multiple_new_id.items():
-        print(old_id, multiple_new_id)
-        merge_information_from_one_node_to_another(old_id, multiple_new_id, 'Pathway')
-
+    print('number of duplicated once:'+str(counter_double_names))
+    print('number of multies:'+str(counter_multiple))
 
     
     
@@ -216,7 +203,7 @@ def main():
     print (datetime.datetime.utcnow())
     print('Load all pathways from d. himmelstein into a dictionary')
 
-    load_in_all_pathways_from_himmelsteins_construction()
+    load_in_all_pathways()
 
     print(
         '###########################################################################################################################')
@@ -224,8 +211,7 @@ def main():
     print (datetime.datetime.utcnow())
     print('Generate csv for switch identifier and ')
 
-    switch_id_for_pathways()
-
+    generate_node_csv()
 
     print(
         '###########################################################################################################################')
