cdba8f8c221a8084dd4d8b23257731e70bb8abd3
ck_linux <dragon493@web.de>
2019-09-26 12:25:32 +0200

    update

--- import_into_Neo4j/EFO/transform_obo_to_csv_and_cypher_file.py
+++ import_into_Neo4j/EFO/transform_obo_to_csv_and_cypher_file.py
@@ -8,6 +8,7 @@ Created on Fri Jan 19 11:46:22 2018
 import sys
 import datetime, csv
 from itertools import groupby
+from collections import  defaultdict
 
 
 # use to delimit between the blocks
@@ -36,24 +37,43 @@ set_all_properties_in_database = set([])
 # list of all properties which are lists
 set_list_properties = set([])
 
-# set list of all parent-child pairs
-set_parent_child_pair = set([])
-
 # list other relationships
-list_other_rela = []
+list_other_rela = set([])
 
 # dictionary with list of the other relationships
-dict_other_rela_parent_child = {}
+dict_other_rela_parent_child = defaultdict(set)
 
 # type definition dictionary
 type_def = {}
 
-'''
+# set of all properties with !
+set_properties_with_bang=set([])
+
+# counter of all relationships
+counter_rela=0
 
 '''
 
+'''
+def add_information_to_dictionary(dict_all_info,key_term, value):
+    # for some properties more than one value appears
+    if not key_term in dict_all_info:
+        dict_all_info[key_term] = value.replace('"', '').replace("'", "").replace("\\", "")
+    # if more than one value appears for a key the key is add to the list of multi values keys
+    # also the values are connected with a |
+    else:
+        dict_all_info[key_term] += '|' + value.replace('"', '').replace("'", "").replace("\\", "")
+        set_list_properties.add(key_term)
 
-def extract_information_from_block(group):
+'''
+go through a block of information and add the node information into a dictionary
+the relationships are either shown as relationships or contain a !
+return the identifier of the node and all node information as dictionary
+'''
+
+
+def extract_information_from_block(group, is_type):
+    global counter_rela
     # dictionary for all values
     dict_all_info = {}
     # go through all properties of the block
@@ -63,36 +83,37 @@ def extract_information_from_block(group):
 
         # check if the key is a relationship or not and add the information to the right dictionary
         if key_term != 'relationship':
-            if value.find('!') == -1:
-
-                # for some properties more than one value appears
-                if not key_term in dict_all_info:
-                    dict_all_info[key_term] = value.replace('"', '').replace("'", "").replace("\\", "")
-                # if more than one value appears for a key the key is add to the list of multi values keys
-                # also the values are connected with a |
-                else:
-                    dict_all_info[key_term] += '|' + value.replace('"', '').replace("'", "").replace("\\", "")
-                    set_list_properties.add(key_term)
+            if value.find(' ! ') == -1:
+                add_information_to_dictionary(dict_all_info,key_term,value)
                 set_all_properties_in_database.add(key_term)
-            else:
+            elif not is_type:
+                set_properties_with_bang.add(key_term)
                 identifier=dict_all_info['id']
-                parent_id = value.split('!')[0].strip().split(' {')[0]
-                if key_term in list_other_rela:
-                    dict_other_rela_parent_child[key_term].add((parent_id, identifier))
-                else:
-                    list_other_rela.append(key_term)
-                    dict_other_rela_parent_child[key_term] = {(parent_id, identifier)}
+                parent_id = value.split(' ! ')[0].strip().split(' {')[0]
+                counter_rela+=1
+                splitted_parent_id=parent_id.split(' ')
+                if len(splitted_parent_id)>1:
+                    # should be only intersection_of
+                    if len(splitted_parent_id)==2:
+                        rela=splitted_parent_id[0]
+                        parent_id=splitted_parent_id[1]
+                        dict_other_rela_parent_child[key_term].add((parent_id,identifier, rela))
+                        continue
+                    else:
+                        add_information_to_dictionary(dict_all_info,key_term,value)
+                        set_all_properties_in_database.add(key_term)
+                        continue
+                dict_other_rela_parent_child[key_term].add((parent_id, identifier))
+                list_other_rela.add(key_term)
+
         # is a relationship but depending on the type it is add to a different set
         else:
             rela_info = value.split('!')[0].split(' ')
             rela_type = rela_info[0]
             parent_id = rela_info[1]
             identifier=dict_all_info['id']
-            if rela_type in list_other_rela:
-                dict_other_rela_parent_child[rela_type].add((parent_id, identifier))
-            else:
-                list_other_rela.append(rela_type)
-                dict_other_rela_parent_child[rela_type] = {(parent_id, identifier)}
+            dict_other_rela_parent_child[rela_type].add((parent_id, identifier))
+            list_other_rela.add(rela_type)
 
     return dict_all_info['id'], dict_all_info
 
@@ -115,15 +136,18 @@ def gather_information_from_obo():
                 # print(group.__next__())
                 # header = group.__next__().rstrip('\n')
 
-                # take only the information from the terms
+                # take only the information from the terms and add to dictionary and add relationships to dictionaries
                 if header.find('[Term]') >= 0:
-                    identifier, dict_all_info = extract_information_from_block(group, ['is_a', 'relationship'])
+                    identifier, dict_all_info = extract_information_from_block(group, False)
                     dict_all_entries[identifier] = dict_all_info
-
+                # remember the type definition
                 elif header.find('[Typedef]') >= 0:
 
-                    identifier, dict_all_info = extract_information_from_block(group, [])
+                    identifier, dict_all_info = extract_information_from_block(group, True)
                     type_def[identifier] = dict_all_info
+
+    print(set_properties_with_bang)
+    print(counter_rela)
 
 
 '''
@@ -157,37 +181,40 @@ def generate_cypher_file():
         csv_writer.writerow(dict_value)
     file.close()
 
-    # add query for relationship integration
-    query = '''Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:/home/cassandra/Dokumente/Project/master_database_change/import_into_Neo4j/''' + directory + '''/output/rela.csv" As line  Match (a:%s{id:line.child_id}), (b:%s{id:line.parent_id}) Create (a)-[:is_a]->(b); \n''' % (
-    neo4j_label, neo4j_label)
-    cypher_file.write(query)
-
-    # create csv for relationships
-    file = open('output/rela.csv', 'w')
-    csv_writer = csv.writer(file)
-    csv_writer.writerow(['child_id', 'parent_id'])
-
-    for (parent_id, child_id) in set_parent_child_pair:
-        csv_writer.writerow([child_id, parent_id])
-    file.close()
-
     print(list_other_rela)
 
-    # generate cypher query and csv for all other rela types
-    for rela_type, list_parent_child in dict_other_rela_parent_child.items():
+    header_for_two=['child_id', 'parent_id']
+    heeader_for_three=['child_id', 'parent_id','rela']
+
+    # generate cypher query and csv for all rela types
+    for rela_type, list_of_infos in dict_other_rela_parent_child.items():
+        if rela_type in type_def:
+            if not rela_type==type_def[rela_type]['name']:
+                rela_type=type_def[rela_type]['name'].replace(' ','_') if not '/' in type_def[rela_type]['name'] else rela_type
+
+        # create csv for relationships
         file_name = 'rela_' + rela_type + '.csv'
-        query = '''Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:/home/cassandra/Dokumente/Project/master_database_change/import_into_Neo4j/%s/output/%s" As line  Match (a:%s{id:line.child_id}), (b:%s{id:line.parent_id}) Create (a)-[:%s]->(b); \n'''
+        file = open('output/' + file_name, 'w')
+        csv_writer = csv.writer(file)
+        list_of_infos=list(list_of_infos)
+        #depending if the relationships contains also information about a relationships type the qury and the csv file is a bit different
+        if len(list_of_infos[0])==2:
+            query = '''Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:/home/cassandra/Dokumente/Project/master_database_change/import_into_Neo4j/%s/output/%s" As line  Match (a:%s{id:line.child_id}), (b:%s{id:line.parent_id}) Create (a)-[:%s]->(b); \n'''
+            csv_writer.writerow(header_for_two)
+        else:
+            print(rela_type)
+            query = '''Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:/home/cassandra/Dokumente/Project/master_database_change/import_into_Neo4j/%s/output/%s" As line  Match (a:%s{id:line.child_id}), (b:%s{id:line.parent_id}) Create (a)-[:%s{relationship:line.rela}]->(b); \n'''
+            csv_writer.writerow(heeader_for_three)
+        # fill the query ND WRITE INTO FILE
         query = query % (directory, file_name, neo4j_label, neo4j_label, rela_type)
         cypher_file.write(query)
 
-        # create csv for relationships
-        file = open('output/' + file_name, 'w')
-        csv_writer = csv.writer(file)
-        csv_writer.writerow(['child_id', 'parent_id'])
-
-        for (parent_id, child_id) in set_parent_child_pair:
-            csv_writer.writerow([child_id, parent_id])
+        #write information into the csv file
+        for info in list_of_infos:
+            info=list(info)
+            csv_writer.writerow(info)
         file.close()
+
 
 
 def main():
