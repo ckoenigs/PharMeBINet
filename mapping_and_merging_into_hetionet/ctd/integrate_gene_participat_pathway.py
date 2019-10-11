423aec6ba16e8eb430decb135d504c2dca662bac
ck_linux <dragon493@web.de>
2019-10-11 10:32:47 +0200

    update

--- mapping_and_merging_into_hetionet/ctd/integrate_gene_participat_pathway.py
+++ mapping_and_merging_into_hetionet/ctd/integrate_gene_participat_pathway.py
@@ -33,12 +33,9 @@ also generate a cypher file to integrate this information
 def take_all_relationships_of_gene_pathway():
     # generate cypher file
     cypherfile = open('gene_pathway/cypher.cypher', 'w')
-    cypherfile.write('begin\n')
-    cypherfile.write(
-        'Match (n:Gene)-[r:PARTICIPATES_GpPW]->(b:Pathway) Where not exists(r.hetionet) Set r.hetionet="yes";\n')
-    cypherfile.write('commit\n')
     query = '''Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:/home/cassandra/Dokumente/Project/master_database_change/mapping_and_merging_into_hetionet/ctd/gene_pathway/relationships.csv" As line Match (n:Gene{identifier:toInteger(line.GeneID)}), (b:Pathway{identifier:line.PathwayID}) Merge (n)-[r:PARTICIPATES_GpPW]->(b) On Create Set r.hetionet='no', r.ctd='yes', r.url_ctd="http://ctdbase.org/detail.go?type=gene&acc="+line.GeneID , r.source="CTD", r.license="© 2002–2012 MDI Biological Laboratory. © 2012–2018 MDI Biological Laboratory & NC State University. All rights reserved.", r.unbiased='false' On Match SET r.ctd='yes', r.url_ctd="http://ctdbase.org/detail.go?type=gene&acc="+line.GeneID;\n '''
     cypherfile.write(query)
+    # in a seperate file
     cypherfile.write('begin\n')
     cypherfile.write('Match (n:Gene)-[r:PARTICIPATES_GpPW]->(b:Pathway) Where not exists(r.ctd) Set r.ctd="no";\n')
     cypherfile.write('commit')
