f2cf36c8dfc021bfe5b17371dd32ac3a96debd41
ckoenigs <dragon493@web.de>
2019-11-07 15:15:58 +0100

    update

--- import_into_Neo4j/mondo/generate_cypher_file.py
+++ import_into_Neo4j/mondo/generate_cypher_file.py
@@ -5,7 +5,7 @@ Created on Wed Feb 14 09:31:43 2018
 @author: ckoenigs
 """
 
-from py2neo import Graph, authenticate
+from py2neo import Graph#, authenticate
 import sys
 import datetime
 reload(sys)
@@ -14,9 +14,9 @@ sys.setdefaultencoding("utf-8")
 # connect with the neo4j database
 def database_connection():
 
-    authenticate("localhost:7474", "neo4j", "test")
+    # authenticate("localhost:7474", )
     global g
-    g = Graph("http://localhost:7474/db/data/")
+    g = Graph("http://localhost:7474/db/data/", auth=("neo4j", "test"))
 
 '''
 Generate cypher file with APOC
@@ -24,13 +24,13 @@ Generate cypher file with APOC
 def generate_cypher_file_with_apoc():
     # generate cypher file for the most of the mondo nodes with their relationships
     #file:/
-    query = '''call apoc.export.cypher.query("MATCH (n:disease)-[r]->(b:disease) Where n.`http://www.geneontology.org/formats/oboInOwl#id` contains 'MONDO' and b.`http://www.geneontology.org/formats/oboInOwl#id` contains 'MONDO' RETURN n,r,b","/home/cassandra/Dokumente/Project/master_database_change/import_into_Neo4j/mondo/mondo.cypher", {batchSize:10000, format:'cypher-shell'}); '''
+    query = '''call apoc.export.cypher.query("MATCH (n:disease)-[r]->(b:disease) Where n.`http://www.geneontology.org/formats/oboInOwl#id` contains 'MONDO' and b.`http://www.geneontology.org/formats/oboInOwl#id` contains 'MONDO' RETURN n,r,b","/home/cassandra/Documents/Project/master_database_change/import_into_Neo4j/mondo/mondo.cypher", {batchSize:10000, format:'cypher-shell'}); '''
     #this is query for cypher shell, but this do not work realy for neo4j 3.2.9
     # query='''call apoc.export.cypher.query("MATCH (n:disease)-[r]->(b:disease) Where n.`http://www.geneontology.org/formats/oboInOwl#id` contains 'MONDO' and b.`http://www.geneontology.org/formats/oboInOwl#id` contains 'MONDO' RETURN n,r,b","'''+save_path+'''mondo.cypher", {batchSize:10000,format:'cypher-shell'}); '''
     g.run(query)
 
     # the only nod that has no connection to another disease node with a mondo id (MONDO:0013239)
-    query= '''call apoc.export.cypher.query("MATCH (n:disease) Where n.`http://www.geneontology.org/formats/oboInOwl#id`='MONDO:0013239' Return n","/home/cassandra/Dokumente/Project/master_database_change/import_into_Neo4j/mondo/single_node_without_connection.cypher", {batchSize:10, format:'cypher-shell'} ); '''
+    query= '''call apoc.export.cypher.query("MATCH (n:disease) Where n.`http://www.geneontology.org/formats/oboInOwl#id`='MONDO:0013239' Return n","/home/cassandra/Documents/Project/master_database_change/import_into_Neo4j/mondo/single_node_without_connection.cypher", {batchSize:10, format:'cypher-shell'} ); '''
     g.run(query)
 
 
