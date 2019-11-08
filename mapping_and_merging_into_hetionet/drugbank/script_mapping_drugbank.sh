f9d861fc4cfda2023f6ed393f54bf0be1020f493
ckoenigs <dragon493@web.de>
2019-11-08 13:43:41 +0100

    update

--- mapping_and_merging_into_hetionet/drugbank/script_mapping_drugbank.sh
+++ mapping_and_merging_into_hetionet/drugbank/script_mapping_drugbank.sh
@@ -3,13 +3,16 @@
 #define path to neo4j bin
 path_neo4j=$1
 
+#path to project
+path_to_project=$2
+
 license='CC BY-NC 4.0'
 
 now=$(date +"%F %T")
 echo "Current time: $now"
 echo 'integrate Drugbank drugs with interaction into Hetionet'
 
-python integrate_DrugBank_with_interaction_into_hetionet.py $license > output_integration_file_generation.txt
+python integrate_DrugBank_with_interaction_into_hetionet.py $license $path_to_project > output_integration_file_generation.txt
 
 
 now=$(date +"%F %T")
@@ -45,7 +48,7 @@ now=$(date +"%F %T")
 echo "Current time: $now"
 echo 'integrate Drugbank salts with interaction into Hetionet'
 
-python salt_to_compound_mapping_connection_to_drugs.py $license > output_integration_file_generation_salt.txt
+python salt_to_compound_mapping_connection_to_drugs.py $license $path_to_project > output_integration_file_generation_salt.txt
 
 
 now=$(date +"%F %T")
