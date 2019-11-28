f7d7e8d04c1caa6d870f6442085be100836cfbdd
ckoenigs <dragon493@web.de>
2019-11-28 15:37:16 +0100

    update

--- mapping_and_merging_into_hetionet/drugbank/salt_to_compound_mapping_connection_to_drugs.py
+++ mapping_and_merging_into_hetionet/drugbank/salt_to_compound_mapping_connection_to_drugs.py
@@ -5,7 +5,7 @@ Created on Tue Sep 17 16:07:43 2019
 @author: ckoenigs
 """
 
-from py2neo import Graph#, authenticate
+from py2neo import Graph  # , authenticate
 import datetime
 import sys, csv
 
@@ -24,8 +24,12 @@ def create_connection_with_neo4j():
 # dictionary of not mapped compound inchikey to node
 dict_not_mapped_compound = {}
 
+# dictionary not mapped compound names to node
+dict_nomt_mapped_compound_name = {}
+
 '''
-Load all compounds which did not mapped and upload them into da dictionary
+Load all compounds which did not mapped and upload them into a dictionary with inchikey as key
+and also in a dictionary where the name is the key
 '''
 
 
@@ -33,8 +37,14 @@ def find_not_mapped_compounds_and_add_to_dict():
     query = '''MATCH (n:Compound) WHere not exists(n.drugbank) RETURN n'''
     result = g.run(query)
     for node, in result:
-        inchikey = node['inchikey']
+        # print(node)
+        inchikey = node['inchikey'] if 'inchikey' in node else ''
+        # print(inchikey)
+        inchikey = inchikey.split('=')[1] if '=' in inchikey else inchikey
         dict_not_mapped_compound[inchikey] = dict(node)
+        if 'name' in node:
+            dict_nomt_mapped_compound_name[node['name'].lower()] = dict(node)
+    print(len(dict_not_mapped_compound))
 
 
 # label of salt
@@ -59,14 +69,14 @@ def create_cypher_and_csv_files():
     query = query % (label_of_salt)
     result = g.run(query)
     header = []
-    query_start='''Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:'''+path_of_directory+'''master_database_change/mapping_and_merging_into_hetionet/drugbank/output/%s.csv" As line Fieldterminator '\\t' Match '''
-    part=query_start+'''(a:%s {identifier:line.identifier}) Create (b:Compound :Salt{'''
+    query_start = '''Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:''' + path_of_directory + '''master_database_change/mapping_and_merging_into_hetionet/drugbank/output/%s.csv" As line Fieldterminator '\\t' Match '''
+    part = query_start + '''(a:%s {identifier:line.identifier}) Create (b:Compound :Salt{'''
     part = part % (file_node, label_of_salt)
     for property, in result:
         part += property + ':line.' + property + ', '
         header.append(property)
     query = part + ' source:"DrugBank", license:"%s" ,drugbank:"yes", resource:["DrugBank"], url:"https://www.drugbank.ca/salts/"+line.identifier}) Create (b)-[:equal_to_drugbank]->(a);\n'
-    query=query %(license)
+    query = query % (license)
 
     cypher_file.write(query)
     node_file = open('output/' + file_node + '.csv', 'w')
@@ -75,26 +85,51 @@ def create_cypher_and_csv_files():
     csv_node = csv.DictWriter(node_file, fieldnames=header, delimiter='\t')
     csv_node.writeheader()
     rela_header = ['salt_id', 'compound_id']
-    query_rela=query_start+' (b:Compound :Salt{identifier:line.salt_id}), (a:Compound {identifier:line.compound_id}) Create (a)-[r:has_ChS{license:"%s"}]->(b);\n'
-    query_rela=query_rela %(file_rela,license)
+    query_rela = query_start + ' (b:Compound :Salt{identifier:line.salt_id}), (a:Compound {identifier:line.compound_id}) Create (a)-[r:has_ChS{license:"%s"}]->(b);\n'
+    query_rela = query_rela % (file_rela, license)
     cypher_file.write(query_rela)
     csv_rela = csv.writer(rela_file, delimiter='\t')
     csv_rela.writerow(rela_header)
+    cypher_file.close()
+
+    # delete compound nodes which are whether drugbank compound nor salt
+    # this must be the last step of the compound integration, because else the merge nodes are also removed and this would be a problem
+    cypher_delete_file=open('cypher_delete_compound.cypher','w')
+    query = '''Match (c:Compound) Where not exists(c.drugbank) Detach Delete c;'''
+    cypher_delete_file.write(query)
+    cypher_delete_file.close()
 
 
 # bash shell for merge doids into the mondo nodes
-bash_shell = open('merge_nodes_salt.sh', 'a')
+bash_shell = open('merge_nodes_salt.sh', 'w')
 bash_shell.write('#!/bin/bash\n')
 
 # the new table for unii drugbank pairs
 unii_drugbank_table_file = open('data/map_unii_to_drugbank_id.tsv', 'w')
-csv_unii_drugbank_table=csv.writer(unii_drugbank_table_file,delimiter='\t')
+csv_unii_drugbank_table = csv.writer(unii_drugbank_table_file, delimiter='\t')
+
+'''
+Add a merge to the bash file
+'''
+
+
+def add_merge_to_sh_file(dict_not_mapped, mapped_value, node_id):
+    compound_id = dict_not_mapped[mapped_value]['identifier']
+    # if it mapped to a not mapped compound
+    text = 'python ../add_information_from_a_not_existing_node_to_existing_node.py %s %s %s\n' % (
+        compound_id, node_id, 'Compound')
+    bash_shell.write(text)
+    text = '''now=$(date +"%F %T")\n echo "Current time: $now"\n'''
+    bash_shell.write(text)
+
 
 '''
 Gather all salt and make a new csv to integrate them as compound into neo4j
 also check if a salt is on of the not mapped compounds 
 Prepare the csv for salt integration, because they are not in hetionet they can be directly integrated
 also check on the drugs which did not mapped, because some of them might be now salts
+
+{identifier:"DBSALT002847"}
 '''
 
 
@@ -105,18 +140,16 @@ def prepare_node_csv():
         csv_node.writerow(node)
         node_id = node['identifier']
         inchikey = node['inchikey']
+        name = node['name']
         if 'unii' in node:
-            unii=node['unii']
-            csv_unii_drugbank_table.writerow([unii,node_id])
-
+            unii = node['unii']
+            csv_unii_drugbank_table.writerow([unii, node_id])
+        # check if this salt is as a drugbank id  already included
+        # if so merge this nodes together
         if inchikey in dict_not_mapped_compound:
-            compound_id = dict_not_mapped_compound[inchikey]['identifier']
-            # if it mapped to a not mapped compound
-            text = 'python ../add_information_from_a_not_existing_node_to_existing_node.py %s %s %s\n' % (
-                compound_id, node_id, 'Compound')
-            bash_shell.write(text)
-            text = '''now=$(date +"%F %T")\n echo "Current time: $now"\n'''
-            bash_shell.write(text)
+            add_merge_to_sh_file(dict_not_mapped_compound, inchikey, node_id)
+        elif name.lower() in dict_nomt_mapped_compound_name:
+            add_merge_to_sh_file(dict_nomt_mapped_compound_name, name, node_id)
 
 
 '''
@@ -132,12 +165,12 @@ def fill_rela_csv():
 
 
 def main():
-    #path to directory of project
+    # path to directory of project
     global path_of_directory
-    if len(sys.argv)<2:
+    if len(sys.argv) < 2:
         sys.exit('need a license')
     global license
-    license=sys.argv[1]
+    license = sys.argv[1]
     path_of_directory = sys.argv[2]
     print(datetime.datetime.utcnow())
     print('create connection with neo4j')
