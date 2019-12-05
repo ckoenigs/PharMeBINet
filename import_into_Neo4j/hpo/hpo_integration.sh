be37156a1eb9d94b3e11a0eaed9474b2838de705
ckoenigs <dragon493@web.de>
2019-12-05 12:38:17 +0100

    script which download latest version of hpo obo file

---
+++ import_into_Neo4j/hpo/hpo_integration.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#define path to neo4j bin
+path_neo4j=$1
+
+#download obo file
+wget  -O ./hpo.obo "https://raw.githubusercontent.com/obophenotype/human-phenotype-ontology/master/hp.obo"
+
+#python3 integrate_hpo_disease_symptomes_into_neo4j.py  > output_integration_hpo.txt
+python3 ../EFO/transform_obo_to_csv_and_cypher_file.py hpo.obo hpo HPOsymptom $path_to_project > output_generate_integration_file.txt
+
+echo generation of csv from tsv file
+python3 integrate_hpo_disease_symptomes_into_neo4j.py $path_to_project > output_disease.txt
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+
+echo integrate hpo into neo4j
+
+cat cypher.cypher | $path_neo4j/cypher-shell -u neo4j -p test > output_cypher_integration.txt
+
+sleep 180
+
+$path_neo4j/neo4j restart
+
+
+sleep 120
