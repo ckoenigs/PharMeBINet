73e53edee2d4e69b4ba03d2480c1873070fee622
ckoenigs <dragon493@web.de>
2019-12-12 13:09:57 +0100

    more definition of efidence code

--- import_into_Neo4j/hpo/integrate_hpo_disease_symptomes_into_neo4j.py
+++ import_into_Neo4j/hpo/integrate_hpo_disease_symptomes_into_neo4j.py
@@ -72,7 +72,15 @@ file:
     3 	Qualifier: can be NOT, Secondary, MILD, Moderate, Severe, can be multiple  
     4 	HPO ID 	
     5 	DB:Reference: source of the information	
-    6 	Evidence code: 	how the information was gathered
+    6 	Evidence code: 	how the information was gathered This required field indicates the level of evidence supporting the annotation. 
+        Annotations that have been extracted by parsing the Clinical Features sections of the omim.txt file are assigned the evidence code IEA (inferred from electronic annotation). 
+        Please note that you need to contact OMIM in order to reuse these annotations in other software products. Other codes include PCS for published clinical study.
+        This should be used for information extracted from articles in the medical literature. 
+        Generally, annotations of this type will include the PubMed id of the published study in the DB_Reference field. 
+        ICE can be used for annotations based on individual clinical experience. This may be appropriate for disorders with a limited amount of published data. 
+        This must be accompanied by an entry in the DB:Reference field denoting the individual or center performing the annotation together with an identifier. 
+        For instance, GH:007 might be used to refer to the seventh such annotation made by a specialist from Gotham Hospital (assuming the prefix GH has been registered with the HPO). 
+        Finally we have TAS, which stands for “traceable author statement”, usually reviews or disease entries (e.g. OMIM) that only refers to the original public
     7 	Onset modifier: A term-id from the HPO-sub-ontology below the term “Age of onset” (HP:0003674). 	
     8 	Frequency modifier: A term-id from the HPO-sub-ontology below the term “Frequency” (HP:0040279) 	
     9 	With: can have information  about characteristics 
