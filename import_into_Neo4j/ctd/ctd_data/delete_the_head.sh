20ea62c03e5c1d2efdfe8bbb2516f92d457bf389
ckoenigs <dragon493@web.de>
2020-05-05 13:09:50 +0200

    add comments

--- import_into_Neo4j/ctd/ctd_data/delete_the_head.sh
+++ import_into_Neo4j/ctd/ctd_data/delete_the_head.sh
@@ -5,7 +5,9 @@ echo $(($file_line+1))
 #tail -n +28 $1 > text.tsv
 tail -n +$(($file_line+1)) $1 > text.tsv
 
+# delete 2 line
 sed -i '2d' text.tsv
+#replace # wit nothing
 sed -i 's/# //' text.tsv
 
 head -2 text.tsv
