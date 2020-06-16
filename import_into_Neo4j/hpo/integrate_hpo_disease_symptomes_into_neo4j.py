f94faf80699635a09256108f07ca08ff9041d39b
ckoenigs <dragon493@web.de>
2020-06-16 12:22:57 +0200

    made list of disease name and resource

--- import_into_Neo4j/hpo/integrate_hpo_disease_symptomes_into_neo4j.py
+++ import_into_Neo4j/hpo/integrate_hpo_disease_symptomes_into_neo4j.py
@@ -21,7 +21,6 @@ list_disease_ids = []
 # list of all hpo symptoms ids
 list_symptoms_ids = []
 
-
 # path to directory
 path_of_directory = ''
 if len(sys.argv) > 1:
@@ -29,36 +28,35 @@ if len(sys.argv) > 1:
 else:
     sys.exit('need a path')
 
-
-
-#csv file for disease node
-writer=open('disease.csv','w', encoding='utf-8')
-csv_writer=csv.writer(writer)
-list_of_disease_properties=['id','name','source']
+# csv file for disease node
+writer = open('disease.csv', 'w', encoding='utf-8')
+csv_writer = csv.writer(writer)
+list_of_disease_properties = ['id', 'name', 'source']
 csv_writer.writerow(list_of_disease_properties)
 
 # csv file for the relationships
-list_of_rela_properties=['disease_id','phenotype_id','qualifier', 'evidence_code','source', 'frequency_modifier', 'aspect']
-writer_rela=open('rela_disease_phenotyp.csv','w', encoding='utf-8')
-csv_writer_rela=csv.writer(writer_rela)
+list_of_rela_properties = ['disease_id', 'phenotype_id', 'qualifier', 'evidence_code', 'source', 'frequency_modifier',
+                           'aspect']
+writer_rela = open('rela_disease_phenotyp.csv', 'w', encoding='utf-8')
+csv_writer_rela = csv.writer(writer_rela)
 csv_writer_rela.writerow(list_of_rela_properties)
 
 # cypher file
-cypher_file =open('cypher.cypher','a')
-query='''Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:'''+path_of_directory+'''master_database_change/import_into_Neo4j/hpo/disease.csv" As line Create (:HPOdisease{'''
+cypher_file = open('cypher.cypher', 'a')
+query = '''Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:''' + path_of_directory + '''master_database_change/import_into_Neo4j/hpo/disease.csv" As line Create (:HPOdisease{'''
 for property in list_of_disease_properties:
-    query+=property+':line.'+property+ ', '
-query=query[:-2]+'});\n'
+    if property == 'name':
+        query += property + ':split(line.' + property + ',"|"), '
+        continue
+    query += property + ':line.' + property + ', '
+query = query[:-2] + '});\n'
 cypher_file.write(query)
 cypher_file.write(':begin\n')
 cypher_file.write('Create Constraint On (node:HPOdisease) Assert node.id Is Unique; \n')
 cypher_file.write(':commit \n')
-#query for relationships
-query='''Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:'''+path_of_directory+'''master_database_change/import_into_Neo4j/hpo/rela_disease_phenotyp.csv" As line MATCH (n:HPOdisease{id:line.disease_id}),(s:HPOsymptom{id:line.phenotype_id}) Create (n)-[:present{source:split(line.source,'|'),qualifier:split(line.qualifier,'|'), evidence_code:split(line.evidence_code,'|'), reference_id:split(line.reference_id,'|'), frequency_modifier:split(line.frequency_modifier,'|'), aspect:split(line.aspect,'|')}]->(s); \n '''
+# query for relationships
+query = '''Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:''' + path_of_directory + '''master_database_change/import_into_Neo4j/hpo/rela_disease_phenotyp.csv" As line MATCH (n:HPOdisease{id:line.disease_id}),(s:HPOsymptom{id:line.phenotype_id}) Create (n)-[:present{source:split(line.source,'|'),qualifier:split(line.qualifier,'|'), evidence_code:split(line.evidence_code,'|'), reference_id:split(line.reference_id,'|'), frequency_modifier:split(line.frequency_modifier,'|'), aspect:split(line.aspect,'|')}]->(s); \n '''
 cypher_file.write(query)
-
-
-
 
 # dictionary with all hpo identifier for frequency
 dict_hpo_frequency = {}
@@ -105,31 +103,37 @@ def gather_all_disease_symptom_information_from_HPO():
         splitted = line.split('\t')
         db_disease = splitted[0]
         db_disease_id = splitted[1]
-        db_disease_name = splitted[2].lower()
+        db_disease_name = splitted[2].lower().split(';;')
         qualifier = splitted[3]
         hpo_id = splitted[4]
-        reference_id = splitted[5]
+        reference_id = splitted[5].split(';')
         evidence_code = splitted[6]
         frequency_modi = splitted[8]
         aspect = splitted[10]
+
         # # aspect M stands for a not known aspect, so I filter them out
         # if aspect == 'M':
         #     continue
-        if  (db_disease+':'+db_disease_id, hpo_id) in dict_disease_symptom_hpo_pair_to_information:
-            dict_disease_symptom_hpo_pair_to_information[(db_disease + ':' + db_disease_id, hpo_id)]['qualifier'].add(qualifier)
-            dict_disease_symptom_hpo_pair_to_information[(db_disease + ':' + db_disease_id, hpo_id)]['reference_id'].add(
-                reference_id)
-            dict_disease_symptom_hpo_pair_to_information[(db_disease + ':' + db_disease_id, hpo_id)]['evidence_code'].add(
+        if (db_disease + ':' + db_disease_id, hpo_id) in dict_disease_symptom_hpo_pair_to_information:
+            dict_disease_symptom_hpo_pair_to_information[(db_disease + ':' + db_disease_id, hpo_id)]['qualifier'].add(
+                qualifier)
+            dict_disease_symptom_hpo_pair_to_information[(db_disease + ':' + db_disease_id, hpo_id)]['reference_id'] = \
+                dict_disease_symptom_hpo_pair_to_information[(db_disease + ':' + db_disease_id, hpo_id)][
+                    'reference_id'].union(reference_id)
+            dict_disease_symptom_hpo_pair_to_information[(db_disease + ':' + db_disease_id, hpo_id)][
+                'evidence_code'].add(
                 evidence_code)
-            dict_disease_symptom_hpo_pair_to_information[(db_disease + ':' + db_disease_id, hpo_id)]['frequency_modi'].add(
+            dict_disease_symptom_hpo_pair_to_information[(db_disease + ':' + db_disease_id, hpo_id)][
+                'frequency_modi'].add(
                 frequency_modi)
             dict_disease_symptom_hpo_pair_to_information[(db_disease + ':' + db_disease_id, hpo_id)]['aspect'].add(
                 aspect)
         else:
-            dict_disease_symptom_hpo_pair_to_information[(db_disease+':'+db_disease_id, hpo_id)] = {'qualifier':set([qualifier]),
-                                                                                                'reference_id':set([reference_id]),
-                                                                                                'evidence_code':set([evidence_code]),
-                                                                                 'frequency_modi':set([frequency_modi]),'aspect':set([aspect])}
+            dict_disease_symptom_hpo_pair_to_information[(db_disease + ':' + db_disease_id, hpo_id)] = {
+                'qualifier': set([qualifier]),
+                'reference_id': set(reference_id),
+                'evidence_code': set([evidence_code]),
+                'frequency_modi': set([frequency_modi]), 'aspect': set([aspect])}
         # csv_writer_rela.writerow([db_disease+':'+db_disease_id, hpo_id,db_disease, qualifier, evidence_code,
         #                                                                          frequency_modi,aspect])
 
@@ -137,26 +141,28 @@ def gather_all_disease_symptom_information_from_HPO():
             list_symptoms_ids.append((hpo_id))
 
         # many diseases have more than one abnormal phenotype, but the disease need to be added only one time
-        if db_disease+':'+db_disease_id in list_disease_ids:
+        if db_disease + ':' + db_disease_id in list_disease_ids:
             continue
-        #write disease into csv
-        csv_writer.writerow([db_disease + ':' + db_disease_id, db_disease_name, db_disease])
+        # write disease into csv
+        csv_writer.writerow([db_disease + ':' + db_disease_id, '|'.join(db_disease_name), db_disease])
 
         # add disease id to list
-        list_disease_ids.append(db_disease+':'+db_disease_id)
-
+        list_disease_ids.append(db_disease + ':' + db_disease_id)
 
     print('number of disease symptoms relationships:' + str(len(dict_disease_symptom_hpo_pair_to_information)))
     print('number of hpo:' + str(len(list_symptoms_ids)))
     print('number of disease:' + str(len(list_disease_ids)))
 
+
 '''
 Write the combined relationship information into csv
 '''
+
+
 def write_rela_info_into_csv():
     for (db_disease, hpo_id), dict_rela_info in dict_disease_symptom_hpo_pair_to_information.items():
-        info_list=[db_disease,hpo_id]
-        info_list.append('|'.join(filter(None,dict_rela_info['qualifier'])))
+        info_list = [db_disease, hpo_id]
+        info_list.append('|'.join(filter(None, dict_rela_info['qualifier'])))
         info_list.append('|'.join(filter(None, dict_rela_info['evidence_code'])))
         info_list.append('|'.join(filter(None, dict_rela_info['reference_id'])))
         info_list.append('|'.join(filter(None, dict_rela_info['frequency_modi'])))
@@ -164,10 +170,7 @@ def write_rela_info_into_csv():
         csv_writer_rela.writerow(info_list)
 
 
-
 def main():
-
-
     print(datetime.datetime.utcnow())
 
     print('##########################################################################')
@@ -175,8 +178,6 @@ def main():
     print(datetime.datetime.utcnow())
     print('gather disease symptoms pairs and add disease to cypher file')
     gather_all_disease_symptom_information_from_HPO()
-
-
 
     print('##########################################################################')
 
