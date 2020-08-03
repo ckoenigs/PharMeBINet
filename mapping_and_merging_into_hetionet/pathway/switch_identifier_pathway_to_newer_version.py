a0c7195cb47c9657a0cacb2b0501cbc1ec8abb5c
ckoenigs <dragon493@web.de>
2020-08-03 11:19:29 +0200

    add resource to rela

--- mapping_and_merging_into_hetionet/pathway/switch_identifier_pathway_to_newer_version.py
+++ mapping_and_merging_into_hetionet/pathway/switch_identifier_pathway_to_newer_version.py
@@ -323,7 +323,7 @@ def generate_rela_csv_and_cypher_queries():
             query_rela_middle+= '(g:Gene{identifier:line.'+head+'}) ,'
         else:
             query_rela_middle += '(p:Pathway{identifier:line.' + head + '}) ,'
-    query_rela=query_start+query_rela_middle[:-2]+ ' Create (g)-[:PARTICIPATES_GpPW{license:p.license, source:p.source, unbiased:false, url:p.url}]->(p);\n'
+    query_rela=query_start+query_rela_middle[:-2]+ ' Create (g)-[:PARTICIPATES_GpPW{license:p.license, source:p.source, unbiased:false, url:p.url, resource:p.source}]->(p);\n'
     query_rela=query_rela %('rela')
     cypher_file.write(query_rela)
 
