6fbd3588897c8ed479c2cfb03b78d7ef73110400
ckoenigs <dragon493@web.de>
2020-08-18 10:47:26 +0200

    association working

--- mapping_and_merging_into_hetionet/omim/integrate_gene_relationships.py
+++ mapping_and_merging_into_hetionet/omim/integrate_gene_relationships.py
@@ -3,9 +3,6 @@ import sys
 import datetime
 import csv
 
-sys.path.append("..")
-from change_xref_source_name_to_a_specifice_form import go_through_xrefs_and_change_if_needed_source_name
-
 
 def database_connection():
     """
@@ -16,102 +13,134 @@ def database_connection():
     g = Graph("http://localhost:7474/db/data/", auth=("neo4j", "test"))
 
 
-cypher_file = open('output/cypher_gene_phenotype.cypher', 'w', encoding='utf-8')
+cypher_file = open('output/cypher_rela.cypher', 'w', encoding='utf-8')
 
 query_start = '''Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:%smaster_database_change/mapping_and_merging_into_hetionet/omim/%s" As line FIELDTERMINATOR '\\t' 
-    Match '''
-
-# dictionary_omim_gene_id_to_node
-dict_omim_id_to_node = {}
-
-# dictionary of mapping tuples
-dict_of_mapped_tuples = {}
-
-# set of not mapped omim gene ids
-set_not_mapped_ids = set()
-
-# file header
-file_header = ['identifier', 'database_id', 'resource', 'xrefs']
+    Match (g:Gene{identifier:line.%s}),(to:%s{identifier:line.%s}) Merge (g)<-[r:ASSOCIATES_%saG]-(to) On Create Set r.resource=['OMIM'], r.source='OMIM', r.omim='yes', r.license='Copyright® 1966-2020 Johns Hopkins University', %s On Match Set r.resource=r.resource+'OMIM', r.omim="yes", %s ;\n'''
 
 
-def prepare_csv_files(file_name, file_header):
+def prepare_cypher_query(file, header_start, to_label):
     """
-    generate a csv file in a given path with a given header
-    :param file_name: string
-    :param file_header: string
+    generate the different cypher files for Disease and Phenotyp
+    :param file: string
+    :param header_start: list with two entries
+    :param to_label: string
     :return:
     """
+    query = '''MATCH (n:gene_omim)-[p]-(h) Where 'phenotype_omim' in labels(h) or 'predominantly_phenotypes_omim' in labels(h) WITH DISTINCT keys(p) AS keys
+        UNWIND keys AS keyslisting WITH DISTINCT keyslisting AS allfields
+        RETURN allfields; '''
+    result = g.run(query)
+
+    query_merge = ""
+    for key, in result:
+        query_merge += "r." + key + '=split(line.' + key + ",'|'), "
+        header_start.append(key)
+
+    query_merge = query_merge[:-2]
+    combine_cypher = query_start
+    combine_cypher = combine_cypher % (
+        path_of_directory, file, header_start[0], to_label, header_start[1], to_label[0], query_merge, query_merge)
+
+    cypher_file.write(combine_cypher)
+
+
+def prepare_csv_files(label):
+    """
+    generate a csv file in a given path with a given header
+    :param label: string
+    :return:
+    """
+    file_name = 'rela/gene_' + label + '_rela.tsv'
     file = open(file_name, 'w', encoding='utf-8')
     csv_writer = csv.writer(file, delimiter='\t')
+    file_header = ['gene_id', 'to_id']
+    prepare_cypher_query(file_name, file_header, label)
     csv_writer.writerow(file_header)
 
-    return csv_writer
+    return csv_writer, file_header
 
 
 # dictionary gene-disease: rela info
 dict_gene_disease = {}
 
+# dictionary gene-phenotype: rela info
+dict_gene_phenotype = {}
 
-def load_all_omim_gene_and_start_mapping():
+
+def put_pair_in_dictionary(gene_id, rela, to_id, dictionary):
     """
-    load all trait gene in and mapp with xref to the mondo genes
+    add gene-x pair in the dictionary with value the relationship infos
+    :param gene_id: string
+    :param rela: dictionary/object
+    :param to_id: string
+    :param dictionary: dictionary
+    :return:
     """
-    query = "MATCH (g:Gene)--(n:gene_omim)-[r]-(h)--(p) Where 'Disease' in labels(p) or 'Phenotype' in labels(p) RETURN g.identifier, r, p.identifier, labels(p)"
+    if not (gene_id, to_id) in dictionary:
+        new_dictionary_rela = {}
+        for key, value in dict(rela).items():
+            if type(value) == list:
+                new_dictionary_rela[key] = set(value)
+            else:
+                new_dictionary_rela[key] = set([value])
+
+        dictionary[(gene_id, to_id)] = new_dictionary_rela
+    else:
+        print('ohje multiple edges')
+        print(gene_id, to_id)
+        for key, value in dict(rela).items():
+            if key in dictionary[(gene_id, to_id)]:
+                if type(value) == list:
+                    dictionary[(gene_id, to_id)][key].union(value)
+                else:
+                    dictionary[(gene_id, to_id)][key].add(value)
+            else:
+                if type(value) == list:
+                    dictionary[(gene_id, to_id)][key] = set(value)
+                else:
+                    dictionary[(gene_id, to_id)][key] = {value}
+
+
+def load_all_omim_gene_phenotypes():
+    """
+    load all genes, association rela, disease or phenotype id and the labels
+    """
+    # {identifier:'604260'}
+    query = "MATCH (g:Gene)--(n:gene_omim)-[r:associates]-(h)--(p) Where 'Disease' in labels(p) or 'Phenotype' in labels(p) RETURN g.identifier, r, p.identifier, labels(p)"
     results = g.run(query)
-    counter_mapped = 0
-    # csv file
-    file_name = "gene/mapping.tsv"
-    csv_writer = prepare_csv_files(file_name, file_header)
 
-    file_name_not_mapped = "gene/not_mapping.tsv"
-    csv_writer_not = prepare_csv_files(file_name_not_mapped, ['identifier', 'name'])
-    # generate query
-    add_query_to_cypher_file('gene_omim', 'Gene', 'gene', file_name, is_gene=True,
-                             additional_labels=['phenotype_omim', 'predominantly_phenotypes_omim'])
-
-    counter_different_symbol = 0
-    for node, labels, in results:
-        identifier = node['identifier']
-
-        dict_omim_id_to_node[identifier] = dict(node)
-        symbol = node['symbol'].lower() if 'symbol' in node else ''
-        alternati_symbols = [x.lower() for x in node['alternative_symbols']] if 'alternative_symbols' in node else []
-
-        xrefs = node['xrefs'] if 'xrefs' in node else []
-
-        found_at_least_one_mapping = False
-        for xref in xrefs:
-            if xref.startswith('NCBI_GENE:'):
-                ncbi_id = xref.split(':')[1]
-                if ncbi_id in dict_gene_id_to_gene_node:
-                    gene_symbol = [x.lower() for x in
-                                   dict_gene_id_to_gene_node[ncbi_id]['geneSymbol']] if 'geneSymbol' in \
-                                                                                        dict_gene_id_to_gene_node[
-                                                                                            ncbi_id] else []
-                    if symbol not in gene_symbol and len(set(gene_symbol).intersection(alternati_symbols)) == 0:
-                        # print('different gene symbols')
-                        # print(symbol)
-                        # print(gene_symbol)
-                        # print((identifier, ncbi_id))
-                        counter_different_symbol += 1
-                    if (identifier, ncbi_id) not in dict_of_mapped_tuples:
-                        add_mapped_one_to_csv_file(dict_gene_id_to_gene_node, identifier, ncbi_id, csv_writer,
-                                                   dict_of_mapped_tuples, is_gene=True)
-                        found_at_least_one_mapping = True
-
-        if found_at_least_one_mapping:
-            counter_mapped += 1
+    for gene_id, rela, to_id, to_labels in results:
+        if 'Disease' in to_labels:
+            put_pair_in_dictionary(gene_id, rela, to_id, dict_gene_disease)
         else:
-            set_not_mapped_ids.add(identifier)
-            csv_writer_not.writerow([identifier, node['name']])
+            put_pair_in_dictionary(gene_id, rela, to_id, dict_gene_phenotype)
 
-    # print(dict_of_mapped_tuples)
-    print('number of mapped gene:' + str(counter_mapped))
-    print('number of not mapped gene:' + str(len(set_not_mapped_ids)))
-    print(counter_different_symbol)
 
-    return csv_writer
-    # print(set_not_mapped_ids)
+def prepare_set_to_string(set_of_lists):
+    """
+    transform set to a list and then to a joined string
+    :param set_of_lists: set
+    :return: string
+    """
+    return "|".join(list(set_of_lists))
+
+
+def generate_csv_and_cypher_file(label, dictionary):
+    """
+    generate csv and fill it and also generate cypher query
+    :param label: string
+    :param dictionary: dictionary
+    :return:
+    """
+    csv_writer, header = prepare_csv_files(label)
+    for (gene_id, to_id), properties in dictionary.items():
+        list_information = [gene_id, to_id]
+        # add properties in the right order
+        for key in header[2:]:
+            list_information.append(
+                prepare_set_to_string(properties[key])) if key in properties else list_information.append('')
+        csv_writer.writerow(list_information)
 
 
 def main():
@@ -134,21 +163,21 @@ def main():
     print(datetime.datetime.utcnow())
     print('Gene mapping')
 
-    gene_writer = mapping_genes()
+    load_all_omim_gene_phenotypes()
 
     print('##########################################################################')
 
     print(datetime.datetime.utcnow())
-    print('Load all disease from database')
+    print('Prepare rela to disease as cypher query and csv file')
 
-    load_disease_from_database_and_add_to_dict()
+    generate_csv_and_cypher_file('Disease', dict_gene_disease)
 
     print('##########################################################################')
 
     print(datetime.datetime.utcnow())
-    print('Load all omim phenotypes from database and map')
+    print('Prepare rela to phenotyp as cypher query and csv file')
 
-    load_phenotype_from_omim_and_map(gene_writer)
+    generate_csv_and_cypher_file('Phenotype', dict_gene_phenotype)
 
     print('##########################################################################')
 
