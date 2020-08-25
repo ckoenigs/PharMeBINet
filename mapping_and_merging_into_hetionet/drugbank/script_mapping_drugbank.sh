4779da8e64e951f6671f2f5acf69e189151db127
ckoenigs <dragon493@web.de>
2020-08-25 12:13:45 +0200

    update that the cyphers are also in dictionary

--- mapping_and_merging_into_hetionet/drugbank/script_mapping_drugbank.sh
+++ mapping_and_merging_into_hetionet/drugbank/script_mapping_drugbank.sh
@@ -12,13 +12,13 @@ now=$(date +"%F %T")
 echo "Current time: $now"
 echo 'integrate Drugbank drugs with interaction into Hetionet'
 
-python3 integrate_DrugBank_with_interaction_into_hetionet.py "${license}" $path_to_project > output_integration_file_generation.txt
+python3 integrate_DrugBank_with_interaction_into_hetionet.py "${license}" $path_to_project > compound_interaction/output_integration_file_generation.txt
 
 
 now=$(date +"%F %T")
 echo "Current time: $now"
 
-$path_neo4j/cypher-shell -u neo4j -p test -f cypher.cypher > output_cypher_drug.txt
+$path_neo4j/cypher-shell -u neo4j -p test -f compound_interaction/cypher.cypher > output_cypher_drug.txt
 
 sleep 180
 $path_neo4j/neo4j restart
@@ -28,7 +28,7 @@ now=$(date +"%F %T")
 echo "Current time: $now"
 echo interactions
 
-$path_neo4j/cypher-shell -u neo4j -p test -f compound_interaction/cypher_interaction.cypher > output_interaction_cypher.txt
+$path_neo4j/cypher-shell -u neo4j -p test -f compound_interaction/cypher_interaction.cypher > compound_interaction/output_interaction_cypher.txt
 
 sleep 180
 $path_neo4j/neo4j restart
@@ -48,13 +48,13 @@ now=$(date +"%F %T")
 echo "Current time: $now"
 echo 'integrate Drugbank salts with interaction into Hetionet'
 
-python3 salt_to_compound_mapping_connection_to_drugs.py "$license" $path_to_project > output_integration_file_generation_salt.txt
+python3 salt_to_compound_mapping_connection_to_drugs.py "$license" $path_to_project > salts/output_integration_file_generation_salt.txt
 
 
 now=$(date +"%F %T")
 echo "Current time: $now"
 
-$path_neo4j/cypher-shell -u neo4j -p test -f cypher_salt.cypher > output_salt_cypher.txt
+$path_neo4j/cypher-shell -u neo4j -p test -f salts/cypher_salt.cypher > salts/output_salt_cypher.txt
 
 sleep 180
 $path_neo4j/neo4j restart
@@ -66,7 +66,7 @@ echo merge nodes
 
 chmod 775 merge_nodes_salt.sh
 
-./merge_nodes_salt.sh > output_merge_compound.txt
+./merge_nodes_salt.sh > output/output_merge_compound.txt
 
 
 sleep 180
