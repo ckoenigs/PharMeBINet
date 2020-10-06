f79955dc5f20ab8749dc795d306ae279a1011040
ckoenigs <dragon493@web.de>
2020-10-06 11:50:19 +0200

    add list properties

--- import_into_Neo4j/IID/prepare_human_data_IID.py
+++ import_into_Neo4j/IID/prepare_human_data_IID.py
@@ -37,9 +37,9 @@ def generate_node_and_rela_file_and_query(header_rela):
 
     query=query_start+ 'Match (p1:protein_IID{identifier:line.uniprot1}),(p2:protein_IID{identifier:line.uniprot2}) Create (p1)-[:interacts{'
     for header in header_rela:
-        if header in []:
+        if header in ['uniprot1','uniprot2','symbol1','symbol2']:
             continue
-        elif header in []:
+        elif header in ['targeting_drugs','evidence_type','dbs','methods','pmids']:
             query+= header+':split(line.'+header+',";"), '
         else:
             query += header + ':line.' + header + ', '
