56704db49fb32e3ba2497bf58d4bf36757f0d0b6
ckoenigs <dragon493@web.de>
2021-04-26 14:02:47 +0200

    updated readme and split pGKB script

--- mapping_and_merging_into_hetionet/pharmGKB/script_pharmgkb.sh
+++ mapping_and_merging_into_hetionet/pharmGKB/script_pharmgkb.sh
@@ -39,35 +39,6 @@ echo "Current time: $now"
 
 $path_neo4j/cypher-shell -u neo4j -p test -f output/cypher.cypher 
 
-sleep 180
-$path_neo4j/neo4j restart
 sleep 120
-
-
-now=$(date +"%F %T")
-echo "Current time: $now"
-echo 'integrate gene-variant rela'
-
-python3 integrate_variant_to_gene_connection.py $path_to_project $license > variant_gene/output.txt
-
-
-now=$(date +"%F %T")
-echo "Current time: $now"
-echo 'integrate clinical meta data rela'
-
-python3 integrate_clinical_annotation_metadata.py $path_to_project $license > metadata_edge/output.txt
-
-now=$(date +"%F %T")
-echo "Current time: $now"
-echo 'integrate clinical variant annotation rela'
-
-python3 integrate_different_annotations.py $path_to_project $license > annotation_variant_edge/output.txt
-
-now=$(date +"%F %T")
-echo "Current time: $now"
-
-$path_neo4j/cypher-shell -u neo4j -p test -f output/cypher_edge.cypher 
-
-sleep 180
 $path_neo4j/neo4j restart
 sleep 120
