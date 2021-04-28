18660b7038cd8ceffc34e1c0cb22b49062ca865b
ckoenigs <dragon493@web.de>
2021-04-28 08:31:17 +0200

    prepared delete source nodes

---
+++ mapping_and_merging_into_hetionet/deleteNodes.py
@@ -0,0 +1,176 @@
+import time
+import sys
+from databaseConnection import query, generateConnection
+
+STOP_AFTER = 50000  # so many nodes will be deleted, then will the program exit
+
+STOP_AFTER_RELA = 3000000
+
+# dictionary label to list [is_index (True, false), ... ] ..=  property (index) else ..= constraint name
+dict_label_to_index_constraint_infos = {}
+
+# Note that apperently the database will not be reduced in size but instead grow due to logs (in the order of GB=> there
+# are settings to reduce the number of logs)
+
+# all node source to delete
+TODELETE = {'sider', 'ctd', 'ndfrt', 'aeolus', 'drugbank', 'ncbi', 'efo', 'hpo', 'uniprot', 'multi', 'go',
+            'diseaseontology', 'mondo', 'clinvar', 'omim', 'reactome', 'adrecstarget', 'iid', 'medrt', 'pharmgkb'}
+
+# dictionary label to batch size
+dict_label_to_batch_size = {
+    'CTD_chemical': '125',
+    'DatabaseObject_reactome': '125'
+}
+
+# dictionary where first relationships are delete
+dict_label_to_batch_size_rela = {
+
+    'CTD_disease': '10000',
+    'CTD_gene': '50000'
+}
+
+time1 = time.time()
+# db = Graph("http://localhost:7474/db/data/", auth=("neo4j", "test"))
+db = generateConnection()
+stop = False
+
+# preparation for remove index/constraints
+q = 'CALL db.indexes;'
+results = db.session().read_transaction(query, q)
+
+for index_id, name, state, populationPercent, uniqueness, index_type, entityType, labelsOrTypes, properties, provider in results:
+    label = labelsOrTypes[0]
+    if name.startswith('constraint'):
+        infos = [False, name]
+    else:
+        infos = [True, properties[0]]
+    if label not in dict_label_to_index_constraint_infos:
+        dict_label_to_index_constraint_infos[label] = []
+    dict_label_to_index_constraint_infos[label].append(infos)
+
+query_remove_index = 'Drop Index On :%s(%s)'
+query_remove_constraint_with_constraint_name = 'DROP CONSTRAINT %s'
+
+# start of delete nodes and indices/constraints
+totalDeletes = 0
+
+q = 'call db.labels();'
+results = db.session().read_transaction(query, q)
+
+# all not removed labels
+not_removed = set()
+# all removed labels
+removed_labels = set()
+
+for label in results:
+    time_0 = time.time()
+    label = label[0]
+    # print(label)
+    if label[0].isupper():
+        splitted = label.lower().split('_')
+        if len(splitted) == 1:
+            not_removed.add(label)
+            continue
+        intersection = TODELETE.intersection(splitted)
+        if len(intersection) == 0:
+            print('has a _ but no source?')
+            print(label)
+            not_removed.add(label)
+            continue
+        else:
+            print('multiple _ but not a source?')
+            # continue
+    print(label)
+    removed_labels.add(label)
+
+    # how many nodes are there to delete?
+    q = 'MATCH (n:' + label + ')RETURN count(*)'
+    # result = db.run(q).to_table()
+    result = db.session().read_transaction(query, q)
+    time_1 = time.time()
+    print(result[0][0], "of type", label, ", query took", time_1 - time_0)
+
+    # delete them
+    count = result[0][0]
+    if count > 0:
+        time_4 = time.time()
+        if label in dict_label_to_batch_size:
+            nowDel = 0
+            while nowDel < count:
+                if totalDeletes + nowDel > STOP_AFTER:
+                    stop = True
+                    break
+                time_4 = time.time()
+                q = 'MATCH (n:%s) WITH n LIMIT %s DETACH DELETE n' % (label, dict_label_to_batch_size[label])
+                db.session().write_transaction(query, q)
+                nowDel += int(dict_label_to_batch_size[label])
+                time_5 = time.time()
+            # batch=dict_label_to_batch_size[label]
+            time_2 = time.time()
+            print('CHAPTER Deleted', nowDel, 'Nodes of type', label, "in time", time_2 - time_1)
+            totalDeletes += count
+        elif label in dict_label_to_batch_size_rela:
+            # if label in ['CTD_disease']: # , 'CTD_gene'
+            #     continue
+            q = 'MATCH p=(n:' + label + ')--() RETURN count(p)'
+            # result = db.run(q).to_table()
+            result = db.session().read_transaction(query, q)
+            time_1 = time.time()
+            print(result[0][0], "of rela from ", label, ", query took", time_1 - time_0)
+            # if label == 'CTD_disease':
+            #     continue
+            # delete them
+            count_rela = result[0][0]
+            rela_batch = dict_label_to_batch_size_rela[label]
+            rela_delete = 0
+
+            nowDel = 0
+            while nowDel < count_rela:
+                if nowDel > STOP_AFTER_RELA:
+                    stop = True
+                    break
+                time_4 = time.time()
+                q = 'MATCH (n:%s)-[r]-() WITH r LIMIT %s DETACH DELETE r' % (label, rela_batch)
+                db.session().write_transaction(query, q)
+                nowDel += int(dict_label_to_batch_size_rela[label])
+                time_5 = time.time()
+            # while (rela_delete<count)
+            if not stop:
+                batch = '200'
+                q = "CALL apoc.periodic.iterate('MATCH (n:%s) RETURN n', 'DETACH DELETE n', {batchSize:%s})" % (
+                    label, batch)
+                db.session().write_transaction(query, q)
+                time_5 = time.time()
+                # print('Deleted circa', BATCH_SIZE, 'Nodes of type', i, 'of', nowDel, 'in time', time_5 - time_4)
+                time_2 = time.time()
+                print('CHAPTER Deleted', count, 'Nodes of type', label, "in time", time_2 - time_1)
+                totalDeletes += count
+        else:
+            batch = '200'
+            q = "CALL apoc.periodic.iterate('MATCH (n:%s) RETURN n', 'DETACH DELETE n', {batchSize:%s})" % (
+            label, batch)
+            print(q)
+            db.session().write_transaction(query, q)
+            time_5 = time.time()
+            # print('Deleted circa', BATCH_SIZE, 'Nodes of type', i, 'of', nowDel, 'in time', time_5 - time_4)
+            time_2 = time.time()
+            print('CHAPTER Deleted', count, 'Nodes of type', label, "in time", time_2 - time_1)
+            totalDeletes += count
+    if label in dict_label_to_index_constraint_infos:
+        for [is_index, name_property] in dict_label_to_index_constraint_infos[label]:
+            if is_index:
+                q = query_remove_index % (label, name_property)
+            else:
+                q = query_remove_constraint_with_constraint_name % name_property
+            db.session().write_transaction(query, q)
+    if totalDeletes > STOP_AFTER or stop:
+        break
+
+time2 = time.time()
+print("TotalDeleltes:", totalDeletes)
+
+timex = time.time()
+print(not_removed)
+print('removed labels:', removed_labels)
+
+print("Total time", timex - time1)
---
+++ mapping_and_merging_into_hetionet/delete_script.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#define path to neo4j bin
+path_neo4j=$1
+
+# MATCH (n) WHERE ANY ( x IN labels(n) WHERE x in['drugSider', 'CTDpathway', 'NDF_RT_DISEASE_KIND', 'AeolusOutcome', 'Compound_DrugBank', 'NDF_RT_DRUG_KIND','Gene_Ncbi', 'NDF_RT_PHYSIOLOGIC_EFFECT_KIND', 'NDF_RT_THERAPEUTIC_CATEGORY_KIND', 'Pathway_DrugBank','PharmacologicClass_DrugBank', 'Product_DrugBank', 'Protein_DrugBank', 'Target_DrugBank','Transporter_DrugBank', 'efo', 'seSider', 'CTDchemical', 'AeolusDrug', 'HPOdisease', 'HPOsymptom','Protein_Uniprot', 'Salt_DrugBank', 'CTDgene', 'pathway_multi', 'go', 'diseaseontology', 'disease','Carrier_DrugBank', 'Enzyme_DrugBank', 'Metabolite_DrugBank', 'Mutated_protein_gene_DrugBank', 'CTDdisease','CTDGO', 'NDF_RT_DOSE_FORM_KIND', 'NDF_RT_INGREDIENT_KIND', 'NDF_RT_MECHANISM_OF_ACTION_KIND','NDF_RT_PHARMACOKINETICS_KIND', 'Variant_ClinVar', 'gene_omim', 'phenotype_omim','predominantly_phenotypes_omim', 'trait_BloodGroup_ClinVar', 'trait_Disease_ClinVar','trait_DrugResponse_ClinVar', 'trait_Finding_ClinVar', 'trait_NamedProteinVariant_ClinVar','trait_PhenotypeInstruction_ClinVar', 'trait_set_Disease_ClinVar', 'trait_set_DrugResponse_ClinVar','trait_set_Finding_ClinVar', 'trait_set_PhenotypeInstruction_ClinVar', 'trait_set_TraitChoice_ClinVar','Reaction_DrugBank','Additional_Pharmacologic_Classes_MED_RT','BlackBoxEvent_reactome', 'Affiliation_reactome','CandidateSet_reactome','CatalystActivityReference_reactome','CatalystActivity_reactome','ChemicalDrug_reactome','Chemical_Ingredient_MED_RT','Compartment_reactome','ControlReference_reactome','CrosslinkedResidue_reactome','DBInfo_reactome','DatabaseIdentifier_reactome','DefinedSet_reactome','Depolymerisation_reactome','Disease_Finding_MED_RT','Disease_reactome','Dose_Form_MED_RT','Drug_reactome','EntityFunctionalStatus_reactome','EntitySet_reactome','EntityWithAccessionedSequence_reactome', 'Event_reactome','FailedReaction_reactome','GO_Term_reactome','InstanceEdit_reactome', 'Mechanisms_of_Action_MED_RT', 'Pathway_reactome', 'Pharmacokinetics_MED_RT','Physiologic_Effects_MED_RT','Reaction_reactome','ReferenceDatabase_reactome','Taxon_reactome','Terminology_Extensions_for_Classification_MED_RT','Therapeutic_Categories_MED_RT', 'VA_Product_MED_RT','local_hierarchical_concept_MED_RT', 'other_MED_RT', 'DatabaseObject_reactome','drug_ADReCS_Target','variant_ADReCS_Target','CTDanatomy','ditop2_ADReCS_Target','FDA_Established_Pharmacologic_Classes_MED_RT', 'adr_ADReCS_Target','protein_IID','gene_ADReCS_Target','protein_ADReCS_Target'] ) RETURN count(n)
+number_of_deleted_nodes=10000000
+#number_of_deleted_nodes=50000
+# number form program
+STOP_AFTER=50000
+
+
+# the add of STOP_AFTER-1 make sure that it is round up, because the division is only int and round down
+number_of_runs=$((($number_of_deleted_nodes+$STOP_AFTER-1)/$STOP_AFTER))
+echo $number_of_runs
+
+for ((i=1; i<=$number_of_runs;i++));do
+    now=$(date +"%F %T")
+    echo "Current time: $now"
+    echo $i
+    python3 deleteNodes.py
+    sleep 60
+
+    $path_neo4j/neo4j restart
+
+
+    sleep 120
+
+    if (($i % 50 == 0))
+    then 
+        echo long sleep
+        sleep 600
+    fi
+
+done
+
+# other preprocess step
+# write into each node how many edges go out
+# python3 DBaddDegreeProperty.py
