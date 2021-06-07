3fb8805d656e53b87b1bb319fd1cbd06a58376b0
ckoenigs <dragon493@web.de>
2021-06-07 07:53:01 +0200

    add dbSNP gene mapping and gene-variant rela integration

--- mapping_and_merging_into_hetionet/dbSNP/script_to_get_dbSNPs_information.sh
+++ mapping_and_merging_into_hetionet/dbSNP/script_to_get_dbSNPs_information.sh
@@ -36,6 +36,13 @@ echo map and integrate dbSNP
 
 python3 map_dbSNP_to_nodes.py $path_to_project $license > output_mapping/output.txt
 
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+echo map dbSNP gene
+
+python3 map_dbSNP_gene_to_gene.py $path_to_project  > output_mapping/output_gene.txt
+
 now=$(date +"%F %T")
 echo "Current time: $now"
 echo integrat mapping dbSNP information into Neo4j
@@ -44,6 +51,22 @@ $path_neo4j/cypher-shell -u neo4j -p test -f output_mapping/cypher.cypher > outp
 
 now=$(date +"%F %T")
 echo "Current time: $now"
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+echo map dbSNP gene
+
+python3 integrate_gene_variant_rela.py $path_to_project  > output_mapping/output_gene_variant.txt
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+echo integrat mapping dbSNP information into Neo4j
+
+$path_neo4j/cypher-shell -u neo4j -p test -f output_mapping/cypher_edge.cypher > output_mapping/output_cypher_rela.txt
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+
 
 
 sleep 120
