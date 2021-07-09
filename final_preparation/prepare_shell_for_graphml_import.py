400120cd066add95b43da7ed151774ec5921fdcb
ckoenigs <dragon493@web.de>
2021-07-09 14:52:15 +0200

    maybe preparation for export database and import graphml into another neo4j database

---
+++ final_preparation/prepare_shell_for_graphml_import.py
@@ -0,0 +1,71 @@
+import sys
+import datetime
+
+sys.path.append("../..")
+import create_connection_to_databases
+
+'''
+create a connection with neo4j
+'''
+
+
+def create_connection_with_neo4j():
+    # set up authentication parameters and connection
+    # authenticate("localhost:7474", "neo4j", "test")
+    global g
+    g = create_connection_to_databases.database_connection_neo4j()
+
+
+
+def get_the_constraints_and_write_into_file():
+    """
+    get all constraint and check if the node exists
+    if so write constraint into file
+    :return:
+    """
+    shell_file=open('shell_import_pharmebinet.sh','w', encoding='utf-8')
+    shell_file.write(''' # define import tool
+import_tool=$1
+# define path to pharmebinet
+path_to_pharmebinet=$2\n\n''')
+
+    import_tool='java -jar ../$import_tool.jar -i $path_to_pharmebinet"PharMeBiNet.graphml"  -e bolt://localhost:7687 --username neo4j --password test  --modify-edge-labels false --indices "%s"'
+
+    query='''CALL db.indexes ''' # also possible after 4.2.x: SHOW INDEXES
+    results= g.run(query)
+    list_indices=[]
+    for id, name, state, populationPercent, uniqueness, type, entityType, labelsOrTypes, properties, provider, in results:
+        for label in labelsOrTypes:
+            for property in properties:
+                list_indices.append(label+'.'+property)
+    import_tool= import_tool %(';'.join(list_indices))
+    shell_file.write(import_tool)
+    
+
+
+
+def main():
+    print(datetime.datetime.utcnow())
+
+    print('##########################################################################')
+
+    print(datetime.datetime.utcnow())
+    print('create connection')
+
+    create_connection_with_neo4j()
+
+    print('##########################################################################')
+
+    print(datetime.datetime.utcnow())
+    print('get constraints')
+
+    get_the_constraints_and_write_into_file()
+
+    print('##########################################################################')
+
+    print(datetime.datetime.utcnow())
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
