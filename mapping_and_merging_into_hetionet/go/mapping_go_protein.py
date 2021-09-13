bac1b7dd5672ea3e70a8aa4c27477ae6934dfa1a
ckoenigs <dragon493@web.de>
2021-09-13 13:48:04 +0200

    finished mapping to gene and protein

--- mapping_and_merging_into_hetionet/go/mapping_go_protein.py
+++ mapping_and_merging_into_hetionet/go/mapping_go_protein.py
@@ -32,7 +32,7 @@ def add_entry_to_dictionary(dictionary, key, value):
         dictionary[key] = set()
     dictionary[key].add(value)
 
-def generate_files(path_of_directory, label):
+def generate_files(label, cypher_file):
     """
     generate cypher file and csv file
     :return: csv file
@@ -50,11 +50,12 @@ def generate_files(path_of_directory, label):
     header = ['identifier', 'name']
     csv_not_mapping.writerow(header)
 
-    cypher_file=open('output/cypher.cypher','a', encoding='utf-8')
+
     query = '''Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:%smaster_database_change/mapping_and_merging_into_hetionet/go/%s.tsv" As line FIELDTERMINATOR '\\t' 
         Match (n:protein_go{identifier:line.identifier}), (v:%s{identifier:line.other_id}) Set v.go='yes', v.resource=split(line.resource,"|") Create (v)-[:equal_to_go_%s{how_mapped:line.mapped_with}]->(n);\n'''
     query = query % (path_of_directory, file_name, label, label.lower())
     cypher_file.write(query)
+
 
     return csv_mapping, csv_not_mapping
 '''
@@ -77,6 +78,36 @@ def load_protein_and_add_to_dictionary():
 
     print('number of proteins:', len(dict_uniprot_id_to_resource))
 
+# dictionary gene id to resource
+dict_gene_id_to_resource={}
+
+# dictionary gene symbol to gene ids
+dict_symbol_to_gene_ids={}
+
+# dictionary synonym to gene ids
+dict_synonyms_to_gene_ids={}
+
+def load_gene_and_add_to_dictionary():
+    """
+    Load gene symbol and source information into dictionaries
+    :return:
+    """
+    query = "MATCH (n:Gene) RETURN n"
+    results = g.run(query)
+    for node, in results:
+        identifier = node['identifier']
+
+        dict_gene_id_to_resource[identifier] = node['resource']
+        gene_symbols = node['gene_symbols'] if 'gene_symbols' in node else []
+        for gene_symbol in gene_symbols:
+            add_entry_to_dictionary(dict_symbol_to_gene_ids, gene_symbol.lower(), identifier)
+
+        synonyms=node['synonyms'] if 'synonyms' in node else []
+        for synonym in synonyms:
+            add_entry_to_dictionary(dict_synonyms_to_gene_ids, synonym.lower(), identifier)
+
+    print('number of gene:', len(dict_gene_id_to_resource))
+
 
 
 def resource(resource):
@@ -89,11 +120,17 @@ Load all pharmacologic class of drugbank and map to the pc from my database and 
 '''
 
 
-def load_all_protein_form_go_and_map_and_write_to_file(csv_writer, csv_not_mapped):
+def load_all_protein_form_go_and_map_and_write_to_file():
+    cypher_file = open('output/cypher_protein.cypher', 'w', encoding='utf-8')
+    csv_writer, csv_not_mapped=generate_files('Protein', cypher_file)
+    csv_writer_gene, csv_not_mapped_gene=generate_files('Gene', cypher_file)
+
     query = "MATCH (n:protein_go) RETURN n"
     results = g.run(query)
     counter = 0
     counter_not_mapped= 0
+    count_mapped_symbol=0
+    count_not_mapped_gene=0
     for node, in results:
         counter += 1
         identifier = node['identifier']
@@ -106,8 +143,25 @@ def load_all_protein_form_go_and_map_and_write_to_file(csv_writer, csv_not_mappe
             counter_not_mapped+=1
             csv_not_mapped.writerow([identifier, node['name']])
 
+        symbol= node['symbol'].lower() if 'symbol' in node else None
+        if not symbol:
+            continue
+        if symbol in dict_symbol_to_gene_ids:
+            count_mapped_symbol+=1
+            for gene_id in dict_symbol_to_gene_ids[symbol]:
+                csv_writer_gene.writerow([identifier,gene_id,  resource(dict_gene_id_to_resource[gene_id]),'symbol'])
+        elif symbol in dict_synonyms_to_gene_ids:
+            count_mapped_symbol += 1
+            for gene_id in dict_synonyms_to_gene_ids[symbol]:
+                csv_writer_gene.writerow([identifier, gene_id, resource(dict_gene_id_to_resource[gene_id]), 'symbol_with_synonyms'])
+        else:
+            count_not_mapped_gene+=1
+            csv_not_mapped_gene.writerow([identifier, symbol])
+
     print('all:',counter)
     print('not mapped:',counter_not_mapped)
+    print('mapped gene symbol',count_mapped_symbol)
+    print('not mapped gene symbol',count_not_mapped_gene)
 
 
 def main():
@@ -133,20 +187,19 @@ def main():
 
     load_protein_and_add_to_dictionary()
 
-
     print('##########################################################################')
 
     print(datetime.datetime.utcnow())
-    print('Prepare csv file')
-    
-    csv_writer, csv_not_mapped=generate_files(path_of_directory,'Protein')
+    print('Load all gene from database')
+
+    load_gene_and_add_to_dictionary()
 
     print('##########################################################################')
 
     print(datetime.datetime.utcnow())
     print('Load all protein from go and map')
 
-    load_all_protein_form_go_and_map_and_write_to_file(csv_writer, csv_not_mapped)
+    load_all_protein_form_go_and_map_and_write_to_file()
 
     print('##########################################################################')
 
