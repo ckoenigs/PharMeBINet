fc0e49a51e02c0bc312ec1525ebe9d104fa006be
ckoenigs <dragon493@web.de>
2021-09-16 10:02:56 +0200

    remove whole go from hetionet and integrate the new

--- mapping_and_merging_into_hetionet/go/go_integration.sh
+++ mapping_and_merging_into_hetionet/go/go_integration.sh
@@ -11,7 +11,7 @@ echo integrate connection with ne4j shell
 now=$(date +"%F %T")
 echo "Current time: $now"
 
-$path_neo4j/cypher-shell -u neo4j -p test -f cypher.cypher
+$path_neo4j/cypher-shell -u neo4j -p test -f output/cypher.cypher
 
 sleep 120
 
@@ -19,23 +19,11 @@ $path_neo4j/neo4j restart
 
 sleep 120
 
-echo merge nodes 
-now=$(date +"%F %T")
-echo "Current time: $now"
-chmod 775 merge_nodes.sh
-
-./merge_nodes.sh $path_neo4j > output/output_merge.txt
-
-sleep 120
-
-$path_neo4j/neo4j restart
-
-sleep 120
 echo delete nodes 
 now=$(date +"%F %T")
 echo "Current time: $now"
 
-$path_neo4j/cypher-shell -u neo4j -p test -f cypher_delete.cypher
+$path_neo4j/cypher-shell -u neo4j -p test -f output/cypher_delete.cypher
 sleep 120
 
 $path_neo4j/neo4j restart
