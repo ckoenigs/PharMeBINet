b630903e65609b27326b34b400e437d3daeb1d7f
ckoenigs <dragon493@web.de>
2021-11-10 09:45:27 +0100

    add bioDWH2 into pipeline

--- import_into_Neo4j/PharmGKB/script_pharmGKB.sh
+++ import_into_Neo4j/PharmGKB/script_pharmGKB.sh
@@ -6,14 +6,31 @@ path_neo4j=$1
 # define import tool
 import_tool=$2
 
+# define bioDWH2 tool
+biodwh2='BioDWH2-v0.4.0'
+
+
+echo load latest version of pharmGKB and generat GraphML file
+
+dir=./sources/
+
+# prepare workspace and add pharmGKB to bioDWB2 tool
+if [ ! -d "$dir" ]; 
+then
+    echo generate workspace in directory
+    java -jar $biodwh2.jar -c .
+
+    java -jar $biodwh2.jar --add-data-source . PharmGKB
+
+fi
+
+java -jar BioDWH2-v0.4.0.jar -u .
+
 echo $import_tool
 
 echo integrate PharmGKB into neo4j
 
-#$path_neo4j/cypher-shell -u neo4j -p test -f cypher.cypher 
-
-java -jar ../$import_tool.jar -i PharmGKB.graphml  -e bolt://localhost:7687 --username neo4j --password test --label-prefix PharmGKB_ --indices "PharmGKB_Chemical.id;PharmGKB_ClinicalAnnotation.id;PharmGKB_DrugLabel.id;PharmGKB_Gene.id;PharmGKB_Haplotype.id;PharmGKB_HaplotypeSet.id;PharmGKB_Literature.id;PharmGKB_Pathway.id;PharmGKB_Phenotype.id;PharmGKB_StudyParameters.id;PharmGKB_Variant.id;PharmGKB_VariantAnnotation.id;PharmGKB_GuidelineAnnotation.id" > output/import_tool_output.txt
-#java -jar ../Neo4j-GraphML-Importer-v1.1.5.jar -i merged.graphml  -e bolt://localhost:7687 --username neo4j --password test
+java -jar ../$import_tool.jar -i sources/PharmGKB/intermediate.graphml  -e bolt://localhost:7687 --username neo4j --password test --label-prefix PharmGKB_ --indices "PharmGKB_Chemical.id;PharmGKB_ClinicalAnnotation.id;PharmGKB_DrugLabel.id;PharmGKB_Gene.id;PharmGKB_Haplotype.id;PharmGKB_HaplotypeSet.id;PharmGKB_Literature.id;PharmGKB_Pathway.id;PharmGKB_Phenotype.id;PharmGKB_StudyParameters.id;PharmGKB_Variant.id;PharmGKB_VariantAnnotation.id;PharmGKB_GuidelineAnnotation.id" > output/import_tool_output.txt
 
 
 $path_neo4j/cypher-shell -u neo4j -p test -f cypher_add_label.cypher
