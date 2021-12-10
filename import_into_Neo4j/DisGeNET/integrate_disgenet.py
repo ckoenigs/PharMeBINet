5631732ce10b2a2bfa9a845c7ffe9a9db85b1dff
ckoenigs <dragon493@web.de>
2021-12-10 06:29:06 +0100

    correction that no unnessessary ropeties are in gene-disease rela

--- import_into_Neo4j/DisGeNET/integrate_disgenet.py
+++ import_into_Neo4j/DisGeNET/integrate_disgenet.py
@@ -14,34 +14,36 @@ def cypher_node(cypher_file, filename, label, properties, unique_property):
     :param properties: columns der Tabelle (auf Leerzeichen achten)
     :param unique_property: identifier (e.g. diseaseId)
     """
-    
-    query_start = 'Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:' + os.path.join(home, destination+filename) + '" As line fieldterminator "\\t" '
+
+    query_start = 'Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:' + os.path.join(home,
+                                                                                                  destination + filename) + '" As line fieldterminator "\\t" '
 
     query = query_start + 'Create (p:%s_DisGeNet{' % (label)
     for x in properties:
         if x in ['synonyms', 'code']:  # properties that are lists must be splitted
-            query += x+':split(line.'+x+',"|"), '
+            query += x + ':split(line.' + x + ',"|"), '
         else:
-            query += x+':line.'+ x + ', '
-    #delete last comma
+            query += x + ':line.' + x + ', '
+    # delete last comma
     query = query[:-2] + '});\n'
     cypher_file.write(query)
 
-    query2 = 'Create Constraint On (node:%s_DisGeNet) Assert node.%s Is Unique; \n' %(label, unique_property)
+    query2 = 'Create Constraint On (node:%s_DisGeNet) Assert node.%s Is Unique; \n' % (label, unique_property)
     cypher_file.write(query2)
     # write everything in same file
 
-def cypher_edge(cypher_file, filename, label, properties, id_list):
+def cypher_edge(cypher_file, filename, label, properties, id_list, edge_name):
     """
     :param cypher_file: destination file to write the queries to
     :param filename: name of source file
     :param label: list of connecting nodes, e.g. ['variant', 'disease']
     :param properties: columns der Tabelle (auf Leerzeichen achten)
     :param id_list: the keys that are only for matching (they do not need to be part of the edge-information)
+    :param edge_name: specifies how the connection btw. two nodes is called
     """
 
     query_start = 'Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:' + os.path.join(home, destination+filename) + '" As line fieldterminator "\\t" '
-    query = query_start + f'Match (p1:{label[0]}_DisGeNet{{{id_list[0]}:line.{id_list[0]}}}),(p2:{label[1]}_DisGeNet{{{id_list[1]}:line.{id_list[1]}}}) Create (p1)-[:interacts{{  '
+    query = query_start + f'Match (p1:{label[0]}_DisGeNet{{{id_list[0]}:line.{id_list[0]}}}),(p2:{label[1]}_DisGeNet{{{id_list[1]}:line.{id_list[1]}}}) Create (p1)-[:{edge_name}{{  '
     for header in properties:
         # ignore key labels (wie diseaseId)
         if header in id_list:
@@ -62,15 +64,16 @@ def get_proteinInfo():
         2. Relation protein to gene
     :return: list of unique gene-ID's
     """
-    
-    name1 = 'mapa_geneid_4_uniprot_crossref.tsv.gz'
-    file1 = pd.read_csv(os.path.join(source, name1), compression='gzip', sep="\t").convert_dtypes()
+
+    name1 = 'mapa_geneid_4_uniprot_crossref.tsv'
+    file1 = pd.read_csv(os.path.join(source, name1), sep="\t").convert_dtypes()
     # 2. Relation protein to gene; edge-information (can simply be copied, contains no duplicates)
-    file1.rename(columns={'GENEID':'geneId'}, inplace=True) # need to rename column "GENEID" to "geneId" for later coherence
-    file1.to_csv(destination+'gene-protein.tsv', sep='\t', index=False)
+    file1.rename(columns={'GENEID': 'geneId'},
+                 inplace=True)  # need to rename column "GENEID" to "geneId" for later coherence
+    file1.to_csv(destination + 'gene-protein.tsv', sep='\t', index=False)
     # 1. Uniprot-Protein-ID information
     proteins = pd.DataFrame(file1.UniProtKB.unique(), columns=['UniProtKB'])
-    proteins.to_csv(destination+'protein.tsv', sep = '\t', index=False)
+    proteins.to_csv(destination + 'protein.tsv', sep='\t', index=False)
     # return gene-id in set for later use
     return list(set(file1['geneId']))
 
@@ -97,29 +100,34 @@ def get_geneInfo(gene_ids):
 
     # 1. map column gene_symbol onto new dataframe with the unique IDs
     right = file1[['geneId', 'geneSymbol']].drop_duplicates(subset=['geneSymbol'])
-    unique_gene_ids = unique_gene_ids.merge(right, on=['geneId'], how='left', copy=False, sort = True)
+    unique_gene_ids = unique_gene_ids.merge(right, on=['geneId'], how='left', copy=False, sort=True)
     # save Gene-Table to tsv
-    unique_gene_ids.to_csv(destination+'gene.tsv', sep='\t', index=False, na_rep='')
-    
+    unique_gene_ids.to_csv(destination + 'gene.tsv', sep='\t', index=False, na_rep='')
+
     # Aggregate source info of identical variant-gene edges
     file2 = file2.drop('geneSymbol', axis=1).groupby(['snpId', 'geneId'], as_index=False).agg({'sourceId': set})
     file2['sourceId'] = file2['sourceId'].apply(lambda x: '|'.join(list(x)))
     # 2. save Gene-Variant edge info as table
-    file2.to_csv(destination+'variant-gene.tsv', sep = '\t', index = False, na_rep = '')
+    file2.to_csv(destination + 'variant-gene.tsv', sep='\t', index=False, na_rep='')
 
-    disease_df = file3[['diseaseId', 'diseaseName', 'diseaseType', 'diseaseClass','diseaseSemanticType']].drop_duplicates(keep='first')  # needed for later use
+    disease_df = file3[
+        ['diseaseId', 'diseaseName', 'diseaseType', 'diseaseClass', 'diseaseSemanticType']].drop_duplicates(
+        keep='first')  # needed for later use
+    # drop irrelevant coulumns for gene-disease edge
+    file3 = file3.drop(columns=['geneSymbol', 'diseaseName', 'diseaseType', 'diseaseClass', 'diseaseSemanticType'])
+    file1 = file1.drop(columns=['geneSymbol', 'diseaseName', 'diseaseType', 'diseaseClass', 'diseaseSemanticType'])
     # 3. save other part of curated gene_disease to edge-table, include pmids
     # subset all_gene_disease_associations to only curated entries
     # IDEA: Kombination von geneId&diseaseId aus file1 nehmen und auf file3 filtern
     curated = ['UNIPROT', 'CTD_human', 'ORPHANET', 'CLINGEN', 'GENOMICS_ENGLAND', 'CGI', 'PSYGENET']  # 'GWASCAT'
     file3 = file3[file3.source.isin(curated)]
-    file3 = file3.dropna(subset = ['pmid']) #drop the rows that have NaN-entries in pmid
+    file3 = file3.dropna(subset=['pmid'])  # drop the rows that have NaN-entries in pmid
     file3 = file3.merge(file1[['geneId', 'diseaseId']], on=['geneId', 'diseaseId'], how='inner', copy=False, sort=True)
     # Convert dtypes of YearInitial, YearFinal, pmid to INT
     file3 = file3.convert_dtypes()
-    file3.pmid = file3.pmid.astype(str) #convert this column to string, in order to join later with pipes 
+    file3.pmid = file3.pmid.astype(str)  # convert this column to string, in order to join later with pipes
     # aggregate the pmids for each gene + disease!!!
-    pmidsFrame = file3.groupby(['geneId','diseaseId'], as_index=False).agg({'pmid': set})
+    pmidsFrame = file3.groupby(['geneId', 'diseaseId'], as_index=False).agg({'pmid': set})
     # calculate the number of entries for each list under pmid-column
     pmidsFrame['check_num'] = pmidsFrame.apply(lambda df: len(df['pmid']), axis=1)
     pmidsFrame['pmid'] = pmidsFrame['pmid'].apply(lambda x: '|'.join(list(x)))
@@ -133,7 +141,7 @@ def get_geneInfo(gene_ids):
     # print(test[['NofPmids', 'check_num', 'pmid']])
     # print(file1.iloc[284])
     # save gene-disease information as tsv
-    file1.drop('check_num', axis=1).to_csv(destination+'gene-disease.tsv', sep='\t', index=False, na_rep='')
+    file1.drop('check_num', axis=1).to_csv(destination + 'gene-disease.tsv', sep='\t', index=False, na_rep='')
     return [file1.diseaseId.unique(), file2.snpId.unique(), disease_df]
 
 
@@ -161,27 +169,35 @@ def get_variantInfo(snp_ids):
     unique_snp_ids = unique_snp_ids.merge(right, how='left', on=['snpId'], copy=False, sort=True)
     # change dtype of 'position' column to INT
     unique_snp_ids = unique_snp_ids.convert_dtypes()
-    # 1. save variant (node) table 
-    unique_snp_ids.to_csv(destination+'variant.tsv',sep='\t', index=False, na_rep='')
+    # 1. save variant (node) table
+    unique_snp_ids.to_csv(destination + 'variant.tsv', sep='\t', index=False, na_rep='')
 
     # 2. prepare variant-disease (edge) table
     # disease_info_vartbl = file1.
-    file1 = file1.drop(columns=['chromosome', 'position', 'diseaseName', 'diseaseType', 'diseaseClass', 'diseaseSemanticType'])
-    disease_df = file2[['diseaseId', 'diseaseName', 'diseaseType', 'diseaseClass', 'diseaseSemanticType']].drop_duplicates(keep='first')  # needed for later use
-    file2 = file2.drop(columns=['chromosome', 'position', 'diseaseName', 'diseaseType', 'diseaseClass', 'diseaseSemanticType'])
-    curated = ['GWASCAT', 'CLINVAR', 'GWASDB', 'UNIPROT', 'GWASCAT;GWASDB', 'CLINVAR;UNIPROT', 'CLINVAR;GWASCAT', 'CLINVAR;GWASDB', 'CLINVAR;GWASCAT;GWASDB', 'GWASCAT;GWASDB;UNIPROT', 'GWASCAT;UNIPROT']
+    file1 = file1.drop(
+        columns=['chromosome', 'position', 'diseaseName', 'diseaseType', 'diseaseClass', 'diseaseSemanticType'])
+    disease_df = file2[
+        ['diseaseId', 'diseaseName', 'diseaseType', 'diseaseClass', 'diseaseSemanticType']].drop_duplicates(
+        keep='first')  # needed for later use
+    file2 = file2.drop(
+        columns=['chromosome', 'position', 'diseaseName', 'diseaseType', 'diseaseClass', 'diseaseSemanticType'])
+    curated = ['GWASCAT', 'CLINVAR', 'GWASDB', 'UNIPROT', 'GWASCAT;GWASDB', 'CLINVAR;UNIPROT', 'CLINVAR;GWASCAT',
+               'CLINVAR;GWASDB', 'CLINVAR;GWASCAT;GWASDB', 'GWASCAT;GWASDB;UNIPROT', 'GWASCAT;UNIPROT']
     file2 = file2[file2.source.isin(curated)]
     # drop the rows that have NaN-entries in pmid
     file2 = file2.dropna(subset=['pmid'])
-    file2 = file2.merge(file1[['snpId', 'diseaseId']], on=['snpId', 'diseaseId'], how='inner', copy=False, sort=True) #include column information of other file1
+    file2 = file2.merge(file1[['snpId', 'diseaseId']], on=['snpId', 'diseaseId'], how='inner', copy=False,
+                        sort=True)  # include column information of other file1
     # Convert dtypes of YearInitial, YearFinal, pmid to INT
     file2 = file2.convert_dtypes()
-    file2.pmid = file2.pmid.astype(str) #convert column to string in order to apply join-operation later on
+    file2.pmid = file2.pmid.astype(str)  # convert column to string in order to apply join-operation later on
     # aggregate the pmids for each gene + disease!!!
-    pmidsFrame = file2.groupby(['snpId', 'diseaseId'], as_index=False).agg({'pmid': set}) # take set to not aggregate duplicates
+    pmidsFrame = file2.groupby(['snpId', 'diseaseId'], as_index=False).agg(
+        {'pmid': set})  # take set to not aggregate duplicates
     # calculate the number of entries for each list under pmid-column
     pmidsFrame['check_num'] = pmidsFrame.apply(lambda df: len(df['pmid']), axis=1)
-    pmidsFrame['pmid'] = pmidsFrame['pmid'].apply(lambda x: '|'.join(list(x)))  # convert column of sets to column of lists
+    pmidsFrame['pmid'] = pmidsFrame['pmid'].apply(
+        lambda x: '|'.join(list(x)))  # convert column of sets to column of lists
     # Merge gene-disease table with extra pmid info
     file1 = file1.merge(pmidsFrame, on=['snpId', 'diseaseId'], how='left', copy=False, sort=True)
     # CHECKS
@@ -190,7 +206,7 @@ def get_variantInfo(snp_ids):
     # print(file1[['snpId', 'diseaseId', 'NofPmids', 'check_num', 'pmid']].head(20))
     # test = file1[file1['NofPmids'] != file1['check_num']]
     # print(test[['NofPmids', 'check_num', 'pmid']])
-    file1.drop('check_num', axis=1).to_csv(destination+'variant-disease.tsv', sep='\t', index=False, na_rep='')
+    file1.drop('check_num', axis=1).to_csv(destination + 'variant-disease.tsv', sep='\t', index=False, na_rep='')
     return [file1.diseaseId.unique(), disease_df]
 
 
@@ -205,7 +221,7 @@ def get_diseaseInfo(disease_ids_genetbl, disease_ids_vartbl, disease_df_genetbl,
     # load dataset
     name1 = 'disease_mappings.tsv.gz'
     file1 = pd.read_csv(os.path.join(source, name1), compression='gzip', sep='\t').convert_dtypes()
-    
+
     # combine the unique disease-entries of both datasets
     unique_ids = pd.DataFrame()
     unique_ids['diseaseId'] = list(set(file1['diseaseId']).union(*[set(disease_ids_genetbl), set(disease_ids_vartbl)]))
@@ -222,14 +238,15 @@ def get_diseaseInfo(disease_ids_genetbl, disease_ids_vartbl, disease_df_genetbl,
 
     # group the different source-codes and synonyms of one disease together
     # list for vocab:code on same diseaseId, list for synonyms on same diseaseId
-    file1_grouped = file1.groupby(['diseaseId', 'diseaseName'], as_index=False).agg({'code': set, 'synonyms': set})  # take set to not aggregate duplicates
+    file1_grouped = file1.groupby(['diseaseId', 'diseaseName'], as_index=False).agg(
+        {'code': set, 'synonyms': set})  # take set to not aggregate duplicates
     # convert column of sets to column of lists
     file1_grouped['code'] = file1_grouped['code'].apply(lambda x: '|'.join(list(x)))
     file1_grouped['synonyms'] = file1_grouped['synonyms'].apply(lambda x: '|'.join(list(x)))
 
     # combine the columns of the 2 datasets in the unique_ids table
     unique_ids = pd.merge(unique_ids, file1_grouped, on=['diseaseId', 'diseaseName'], how='left')
-    unique_ids.to_csv(destination+'disease.tsv', sep='\t', index=False, na_rep='')
+    unique_ids.to_csv(destination + 'disease.tsv', sep='\t', index=False, na_rep='')
 
 
 
@@ -315,12 +332,15 @@ def main():
 
     # dictionnary with the ID's that link the two nodes for each .tsv file
     id_dict = {'variant-disease.tsv': ['snpId', 'diseaseId'], 'gene-protein.tsv': ['geneId', 'UniProtKB'], 'gene-disease.tsv': ['geneId', 'diseaseId'], 'variant-gene.tsv': ['snpId', 'geneId']}
+    edge_dict = {'variant-disease.tsv': 'associates', 'gene-protein.tsv': 'interacts', 'gene-disease.tsv': 'interacts', 'variant-gene.tsv': 'associates'}
     for curr_file in output_edges:
-        label = curr_file.split('.', 1)[0].split('-') # split the names into list with two node entries (make sure that the order of id_dict keys and order of the keys in list match)
+        label = curr_file.split('.', 1)[0].split(
+            '-')  # split the names into list with two node entries (make sure that the order of id_dict keys and order of the keys in list match)
         header = list(pd.read_csv(os.path.join(destination, curr_file), nrows=0, sep='\t').columns)
         id_list = id_dict[curr_file]
-        #print(f'file: {curr_file} \nlabel: {label} \nproperties: {header} \nid_list: {id_list}\n')
-        cypher_edge(cypher_file, curr_file, label, header, id_list)
+        edge_name = edge_dict[curr_file]
+        # print(f'file: {curr_file} \nlabel: {label} \nproperties: {header} \nid_list: {id_list}\n')
+        cypher_edge(cypher_file, curr_file, label, header, id_list, edge_name)
 
     print(datetime.datetime.utcnow())
 if __name__ == "__main__":
