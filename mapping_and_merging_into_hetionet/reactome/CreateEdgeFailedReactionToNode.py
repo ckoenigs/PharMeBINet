d9b443ed41544ee13a0933ae6ad7935975ead3a1
ckoenigs <dragon493@web.de>
2022-04-23 09:59:59 +0200

    change to RLE

--- mapping_and_merging_into_hetionet/reactome/CreateEdgeFailedReactionToNode.py
+++ mapping_and_merging_into_hetionet/reactome/CreateEdgeFailedReactionToNode.py
@@ -33,10 +33,9 @@ load in all pathways from hetionet in a dictionary
 
 
 def load_hetionet_failedreaction_hetionet_node_in(csv_file, dict_failedReaction_hetionet_node_hetionet,
-                                                  new_relationship,
-                                                  node_reactome_label, rela_equal_name, node_hetionet_label):
-    query = '''MATCH (p:FailedReaction)-[:equal_to_reactome_failedreaction]-(r:FailedReaction_reactome)-[v:%s]->(n:%s)-[:%s]-(b:%s) RETURN p.identifier, b.identifier, v.order, v.stoichiometry'''
-    query = query % (new_relationship, node_reactome_label, rela_equal_name, node_hetionet_label)
+                                                  new_relationship, node_reactome_label, node_hetionet_label):
+    query = '''MATCH (p:FailedReaction)-[:equal_to_reactome_failedreaction]-(r:FailedReaction_reactome)-[v:%s]->(n:%s)-[]-(b:%s) RETURN p.identifier, b.identifier, v.order, v.stoichiometry'''
+    query = query % (new_relationship, node_reactome_label, node_hetionet_label)
     results = graph_database.run(query)
     # for id1, id2, order, stoichiometry, in results:
     for failedreaction_id, node_id, order, stoichiometry, in results:
@@ -45,7 +44,7 @@ def load_hetionet_failedreaction_hetionet_node_in(csv_file, dict_failedReaction_
             sys.exit("Doppelte failedreaction-Disease Kombination")
         dict_failedReaction_hetionet_node_hetionet[(failedreaction_id, node_id)] = [stoichiometry, order]
         csv_file.writerow([failedreaction_id, node_id, order, stoichiometry])
-    print('number of failedreaction-'+node_reactome_label+' relationships in hetionet:' + str(
+    print('number of failedreaction-' + node_reactome_label + ' relationships in hetionet:' + str(
         len(dict_failedReaction_hetionet_node_hetionet)))
 
 
@@ -60,7 +59,7 @@ def create_cypher_file(directory, file_path, node_label, rela_name):
     cypher_file.write(query)
 
 
-def check_relationships_and_generate_file(new_relationship, node_reactome_label, rela_equal_name, node_hetionet_label,
+def check_relationships_and_generate_file(new_relationship, node_reactome_label, node_hetionet_label,
                                           directory, rela_name):
     print(
         '###########################################################################################################################')
@@ -68,9 +67,9 @@ def check_relationships_and_generate_file(new_relationship, node_reactome_label,
     print(datetime.datetime.now())
     print('Load all relationships from hetionet_failedReaction and hetionet_nodes into a dictionary')
     # file for mapped or not mapped identifier
-    file_name= directory + '/mapped_failedReaction_to_'+node_reactome_label+'_'+rela_name+'.tsv'
+    file_name = directory + '/mapped_failedReaction_to_' + node_reactome_label + '_' + rela_name + '.tsv'
 
-    file_mapped_failedreaction_to_node = open(file_name,'w', encoding="utf-8")
+    file_mapped_failedreaction_to_node = open(file_name, 'w', encoding="utf-8")
     csv_mapped = csv.writer(file_mapped_failedreaction_to_node, delimiter='\t', lineterminator='\n')
     csv_mapped.writerow(['id_hetionet_failedReaction', 'id_hetionet_node', 'order', 'stoichiometry'])
 
@@ -78,7 +77,7 @@ def check_relationships_and_generate_file(new_relationship, node_reactome_label,
 
     load_hetionet_failedreaction_hetionet_node_in(csv_mapped, dict_failedReaction_node, new_relationship,
                                                   node_reactome_label,
-                                                  rela_equal_name, node_hetionet_label)
+                                                  node_hetionet_label)
 
     print(
         '###########################################################################################################################')
@@ -94,7 +93,7 @@ def main():
     global path_of_directory, license
     if len(sys.argv) > 1:
         path_of_directory = sys.argv[1]
-        license= sys.argv[2]
+        license = sys.argv[2]
     else:
         sys.exit('need a path reactome protein')
 
@@ -107,13 +106,11 @@ def main():
     # 0: old relationship;           1: name of node in Reactome;        2: relationship equal to Hetionet-node
     # 3: name of node in Hetionet;   4: name of new relationship
     list_of_combinations = [
-        ['disease', 'Disease_reactome', 'equal_to_reactome_disease', 'Disease',
-         'LEADS_TO_DISEASE_FltdD'],
-        ['compartment', 'GO_CellularComponent_reactome', 'equal_to_reactome_gocellcomp', 'CellularComponent', 'IN_COMPARTMENT_FicCC'],
-        ['normalReaction', 'Reaction_reactome', 'equal_to_reactome_reaction', 'Reaction', 'IS_NORMAL_REACTION_FinrR'],
-        ['normalReaction', 'BlackBoxEvent_reactome', 'equal_to_reactome_blackBoxEvent', 'BlackBoxEvent', 'IS_NORMAL_REACTION_FinrB'],
-        ['precedingEvent', 'Reaction_reactome', 'equal_to_reactome_reaction', 'Reaction', 'PRECEDING_REACTION_FprR'],
-        ['inferredTo', 'FailedReaction_reactome', 'equal_to_reactome_failedreaction', 'FailedReaction', 'HAS_EFFECT_ON_FheoF']
+        ['disease', 'Disease_reactome', 'Disease', 'LEADS_TO_RLEltdD'],
+        ['compartment', 'GO_CellularComponent_reactome', 'CellularComponent', 'IN_COMPARTMENT_RLEicCC'],
+        ['normalReaction', 'ReactionLikeEvent_reactome', 'ReactionLikeEvent', 'IS_NORMAL_REACTION_RLEinrRLE'],
+        ['precedingEvent', 'ReactionLikeEvent_reactome', 'ReactionLikeEvent', 'PRECEDING_REACTION_RLEprRLE'],
+        ['inferredTo', 'ReactionLikeEvent_reactome', 'ReactionLikeEvent', 'HAS_EFFECT_ON_RLEheoRLRLE']
     ]
 
     directory = 'FailedReactionEdges'
@@ -122,11 +119,9 @@ def main():
     for list_element in list_of_combinations:
         new_relationship = list_element[0]
         node_reactome_label = list_element[1]
-        rela_equal_name = list_element[2]
-        node_hetionet_label = list_element[3]
-        rela_name = list_element[4]
-        check_relationships_and_generate_file(new_relationship, node_reactome_label, rela_equal_name,
-                                              node_hetionet_label, directory,
+        node_hetionet_label = list_element[2]
+        rela_name = list_element[3]
+        check_relationships_and_generate_file(new_relationship, node_reactome_label, node_hetionet_label, directory,
                                               rela_name)
     cypher_file.close()
 
