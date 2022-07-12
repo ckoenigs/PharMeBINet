1fb3dc50eedf7024942d2e65405ee4cac4fdd00b
ckoenigs <dragon493@web.de>
2022-07-12 07:33:41 +0200

    a parser for the database hippie

---
+++ import_into_Neo4j/hippie/parse_hippie.py
@@ -0,0 +1,166 @@
+import csv, datetime, sys
+import os.path
+
+sys.path.append("../..")
+import pharmebinetutils
+
+
+def download_and_open_file():
+    global csv_reader
+    print('download file if not exist', datetime.datetime.now())
+    if not os.path.exists('HIPPIE-current.mitab.txt'):
+        url = 'http://cbdm-01.zdv.uni-mainz.de/~mschaefer/hippie/HIPPIE-current.mitab.txt'
+        pharmebinetutils.download_file(url)
+
+    file = open('HIPPIE-current.mitab.txt', 'r', encoding='utf-8')
+    csv_reader = csv.DictReader(file, delimiter='\t')
+
+
+def create_tsv_files():
+    global node_file_name, header_node, csv_node
+    print('start generate tsv', datetime.datetime.now())
+    node_file_name = 'output/node.tsv'
+    node_file = open(node_file_name, 'w', encoding='utf-8')
+    csv_node = csv.writer(node_file, delimiter='\t')
+    header_node = ['identifier', 'alternative_ids', 'aliases', 'taxid', 'gene_name']
+    csv_node.writerow(header_node)
+
+    global edge_file_name, header_edge, csv_edge
+    edge_file_name = 'output/edge.tsv'
+    edge_file = open(edge_file_name, 'w', encoding='utf-8')
+    csv_edge = csv.writer(edge_file, delimiter='\t')
+    header_edge = ['id1', 'id2', 'detection_methods', 'publication_author', 'publication_id', 'interaction_types',
+                   'source_dbs', 'interaction_ids', 'confidence_value', 'presence_in_other_species']
+    csv_edge.writerow(header_edge)
+
+
+def create_cypher_queries():
+    cypher_file = open('cypher.cypher', 'w', encoding='utf-8')
+    query_start = f'Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:{path_of_directory}import_into_Neo4j/hippie/%s" As line FIELDTERMINATOR "\\t" '
+
+    query_node = query_start + ' Create (n:Protein_Hippie{ '
+    for property in header_node:
+        query_node += property + ': line.' + property + ', '
+    query_node = query_node[:-2] + '});\n'
+    query_node = query_node % (node_file_name)
+    cypher_file.write(query_node)
+
+    query = 'Create Constraint On (node:Protein_Hippie) Assert node.identifier Is Unique;\n'
+    cypher_file.write(query)
+
+    query_edge = query_start + ' Match (n:Protein_Hippie{identifier:line.id1}),(m:Protein_Hippie{identifier:line.id2}) Create (n)-[:INTERACTS{ '
+    for property in header_edge[2:]:
+        if property not in ['confidence_value']:
+            query_edge += property + ': split(line.' + property + ', "|"), '
+        else:
+            query_edge += property + ': line.' + property + ', '
+
+    query_edge = query_edge[:-2] + '}]->(m);\n'
+    query_edge = query_edge % (edge_file_name)
+
+    cypher_file.write(query_edge)
+
+
+set_ids = set()
+
+
+def add_check_if_entry_is_empty(value):
+    return value if value != '-' else ''
+
+
+"""
+0:'ID Interactor A': node 1
+1:'ID Interactor B': node 2
+2:'Alt IDs Interactor A': node 1
+3:'Alt IDs Interactor B': node 2
+4:'Aliases Interactor A': node 1
+5:'Aliases Interactor B': node 2
+6:'Interaction Detection Methods': edge
+7:'Publication 1st Author': edge
+8:'Publication Identifiers': edge
+9:'Taxid Interactor A': node 1
+10:'Taxid Interactor B': node 2
+11:'Interaction Types': edge
+12:'Source Databases': edge
+13:'Interaction Identifiers': edge
+14:'Confidence Value': edge
+15:'Presence In Other Species': edge
+16:'Gene Name Interactor A': node 1
+17:'Gene Name Interactor B': node 2
+
+"""
+
+
+def extract_info_from_mitab_file():
+    for line in csv_reader:
+        id1 = line['ID Interactor A']
+        id2 = line['ID Interactor B']
+        if id1 not in set_ids:
+            csv_node.writerow([line['ID Interactor A'], add_check_if_entry_is_empty(line['Alt IDs Interactor A']),
+                               add_check_if_entry_is_empty(line['Aliases Interactor A']),
+                               add_check_if_entry_is_empty(line['Taxid Interactor A']),
+                               add_check_if_entry_is_empty(line['Gene Name Interactor A'])])
+            set_ids.add(id1)
+        if id2 not in set_ids:
+            csv_node.writerow([line['ID Interactor B'], add_check_if_entry_is_empty(line['Alt IDs Interactor B']),
+                               add_check_if_entry_is_empty(line['Aliases Interactor B']),
+                               add_check_if_entry_is_empty(line['Taxid Interactor B']),
+                               add_check_if_entry_is_empty(line['Gene Name Interactor B'])])
+            set_ids.add(id2)
+        csv_edge.writerow([id1, id2, add_check_if_entry_is_empty(line['Interaction Detection Methods']),
+                           add_check_if_entry_is_empty(line['Publication 1st Author']),
+                           add_check_if_entry_is_empty(line['Publication Identifiers']),
+                           add_check_if_entry_is_empty(line['Interaction Types']),
+                           add_check_if_entry_is_empty(line['Source Databases']),
+                           add_check_if_entry_is_empty(line['Interaction Identifiers']),
+                           add_check_if_entry_is_empty(line['Confidence Value']),
+                           add_check_if_entry_is_empty(line['Presence In Other Species'])])
+        # print(dict(line))
+        # print(dict(line).keys())
+        # break
+
+
+print(datetime.datetime.now())
+
+
+def main():
+    global path_of_directory
+    if len(sys.argv) > 1:
+        path_of_directory = sys.argv[1]
+    else:
+        sys.exit('need a path Hippie')
+
+    print('##########################################################################')
+
+    print(datetime.datetime.now())
+    print('download and open data file')
+
+    download_and_open_file()
+
+    print('##########################################################################')
+
+    print(datetime.datetime.now())
+    print('create tsv files')
+
+    create_tsv_files()
+
+    print('##########################################################################')
+
+    print(datetime.datetime.now())
+    print('prepare cypher queries')
+    create_cypher_queries()
+
+    print('##########################################################################')
+
+    print(datetime.datetime.now())
+    print('extract info form mitab file into different tsv files')
+    extract_info_from_mitab_file()
+
+    print('##########################################################################')
+
+    print(datetime.datetime.now())
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
