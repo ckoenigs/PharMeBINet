db8f9218cc9d7a979073f35a0ee003ecec41647d
ckoenigs <dragon493@web.de>
2022-08-05 08:13:50 +0200

    final version rnainter dna to gene mapping

--- mapping_and_merging_into_hetionet/RNAinter/dna_RNAInter.py
+++ mapping_and_merging_into_hetionet/RNAinter/dna_RNAInter.py
@@ -1,6 +1,8 @@
-import csv,sys
+import csv, sys
+import datetime
 sys.path.append("../..")
 import create_connection_to_databases
+import pharmebinetutils
 
 '''
 create a connection with neo4j
@@ -12,8 +14,17 @@ def create_connection_with_neo4j():
     global g
     g = create_connection_to_databases.database_connection_neo4j()
 
-# cypher file
-cypher_file=open("output/cypher.cypher","w",encoding="utf-8")
+
+def resource_add_and_prepare(resource, source):
+    """
+    add to resource a new source and generate a sort join string
+    :param resource: list/set
+    :param source: string
+    :return: string
+    """
+    resource = set(resource)
+    resource.add(source)
+    return '|'.join(sorted(resource, key=lambda s: s.lower()))
 
 
 def dna_RNAInter():
@@ -25,49 +36,62 @@ def dna_RNAInter():
     query = "MATCH (n:dna_RNAInter) RETURN n.Raw_ID "
     result = g.run(query)
     for raw_id, in result:
-        id= raw_id.split(":", 1)
-        if len(id)==2:
+        id = raw_id.split(":", 1)
+        if len(id) == 2:
             if id[0] not in dnaRNAInter:
-                dnaRNAInter[id[0]] ={}
-                dnaRNAInter[id[0]][id[1]]=raw_id
+                dnaRNAInter[id[0]] = {}
+                dnaRNAInter[id[0]][id[1]] = raw_id
             else:
-                dnaRNAInter[id[0]][id[1]]=raw_id
+                dnaRNAInter[id[0]][id[1]] = raw_id
 
     # compare the identifiers from Gene with the RAW_IDs from dna_RNAInter
-    # save the IDs that appear in both of them in a tsv file
-    query = "MATCH (n:Gene) RETURN n.identifier"
+    # save the IDs that appear in both of them in a tsv
+    query = "MATCH (n:Gene) RETURN n.identifier, n.resource"
     result = g.run(query)
 
-    file_name='output/DNAedges.tsv'
+    file_name = 'output/DNAedges.tsv'
     with open(file_name, 'w', newline='') as tsv_file:
         writer = csv.writer(tsv_file, delimiter='\t')
-        line = ["Raw_ID", "identifier"]
+        line = ["identifier", "Raw_ID", "resource"]
         writer.writerow(line)
-        for id, in result:
+        for id, resource, in result:
             if id in dnaRNAInter["NCBI"]:
                 list = []
-                list.append(dnaRNAInter["NCBI"][id]), list.append(id)
+                list.append(id), list.append(dnaRNAInter["NCBI"][id])
+                newresource = pharmebinetutils.resource_add_and_prepare(resource, "RNAInter")
+                list.append(newresource)
                 writer.writerow(list)
     tsv_file.close()
 
-    #
+    # cypher file
+    cypher_file = open("output/cypher.cypher", "w", encoding="utf-8")
     print("######### Start: Cypher #########")
     query_start = f'Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:{path_of_directory}mapping_and_merging_into_hetionet/RNAinter/{file_name}" As line fieldterminator "\t" '
-    query = query_start + f'Match (p1:dna_RNAInter{{Raw_ID:line.Raw_ID}}),(p2:Gene{{identifier:line.identifier}}) Create (p1)-[:associateGeneDnaRNAInter{{  '
-    query = query[:-2]+'}]->(p2);\n'
+    query = query_start + f'Match (p1:dna_RNAInter{{Raw_ID:line.Raw_ID}}),(p2:Gene{{identifier:line.identifier}})  SET p2.rnainter="yes", p2.resource = split(line.resource,"|") Create (p1)-[:associateGeneDnaRNAInter]->(p2);\n  '
     cypher_file.write(query)
-    print("######### End: Cypher #########")
+    cypher_file.close()
+
+
 
 def main():
-
     global path_of_directory
     if len(sys.argv) > 1:
         path_of_directory = sys.argv[1]
     else:
         sys.exit('need a path rnaInter')
 
+    print('###########################################################################')
+    print(datetime.datetime.now(), 'create connections')
     create_connection_with_neo4j()
+
+    print('###########################################################################')
+    print(datetime.datetime.now(), 'mapping')
     dna_RNAInter()
+
+    print('###########################################################################')
+    print(datetime.datetime.now())
+
+
 
 if __name__ == "__main__":
     # execute only if run as a script
