e14dc4a462e00078a45d9256ad42c15201dc4c24
ckoenigs <dragon493@web.de>
2022-10-24 06:03:18 +0200

    finished drugcentral product mapping

--- mapping_and_merging_into_hetionet/drugcentral/mapping_product_drugcentral.py
+++ mapping_and_merging_into_hetionet/drugcentral/mapping_product_drugcentral.py
@@ -1,8 +1,6 @@
-from py2neo import Graph
 import datetime
 import csv
 import sys
-import html
 from collections import defaultdict
 
 sys.path.append("../..")
@@ -20,9 +18,13 @@ def create_connection_with_neo4j():
 
 
 # dictionary
+dict_product = {}
 dict_product_to_name = {}
+dict_product_name = {}
+dict_mapping_parameter = {}
 dict_productId_to_resource = {}
 dict_ndc_pc = defaultdict(set)
+dict_product_mapping = defaultdict(dict)
 
 
 def load_product_in():
@@ -45,6 +47,12 @@ def load_product_in():
 
         dict_productId_to_resource[identifier] = resource
 
+        # name
+        name = name.lower()
+        if name not in dict_product_name:
+            dict_product_name[name] = set()
+        dict_product_name[name].add(identifier)
+
         if ndc_pc != "":
             dict_ndc_pc[ndc_pc].add(identifier)
 
@@ -63,28 +71,30 @@ def load_DC_product_in():
 
     for node, node_id, in results:
         prod_ndc = node["ndc_product_code"]
-        product_name = node["product_name"] if 'product_name' in node else ''
+        product_name = node["product_name"] if 'product_name'  in node else ''
         product_name = product_name.lower()
+
 
         if prod_ndc in dict_ndc_pc:
             products = dict_ndc_pc[prod_ndc]
             for product_id in products:
-                dict_nodes_to_product[node_id] = product_id
                 if node_id not in dict_node_to_methode:
-                    dict_node_to_methode[node_id] = set()
-                dict_node_to_methode[node_id].add('ndc_pc')
+                    dict_product_mapping[node_id][product_id] = set()
+                dict_product_mapping[node_id][product_id].add('ndc_pc')
                 mapped_products.add(node_id)
+
 
         if node_id not in mapped_products:
             csv_not_mapped.writerow([node_id, prod_ndc, product_name])
 
     for node_id in mapped_products:
-        methodes = list(dict_node_to_methode[node_id])
-        product_id = dict_nodes_to_product[node_id]
-        resource = set(dict_productId_to_resource[product_id])
-        resource.add('DrugCentral')
-        resource = '|'.join(resource)
+        for product_id in dict_product_mapping[node_id]:
+            methodes = list(dict_product_mapping[node_id][product_id])
+            resource = set(dict_productId_to_resource[product_id])
+            resource.add('DrugCentral')
+            resource = '|'.join(resource)
         csv_mapped.writerow([node_id, product_id, resource, methodes])
+
 
 def generate_tsv_files():
     """
@@ -98,13 +108,11 @@ def generate_tsv_files():
     csv_not_mapped = csv.writer(file_not_mapped_protein, delimiter='\t', lineterminator='\n')
     # Header setzen
     csv_not_mapped.writerow(['id', 'ndc', 'name'])
-
     # prepare mapped file
     file_name='product/mapped_product.tsv'
     file_mapped_protein = open(file_name, 'w', encoding="utf-8")
     csv_mapped = csv.writer(file_mapped_protein, delimiter='\t', lineterminator='\n')
     csv_mapped.writerow(['node_id', 'id_hetionet', 'resource', 'how_mapped'])
-
     generate_cypher_file(file_name)
 
 
@@ -116,8 +124,8 @@ def generate_cypher_file(file_name):
     :return:
     """
     cypher_file = open('output/cypher.cypher', 'w')
-    query = '''Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:%smapping_and_merging_into_hetionet/drugcentral/%s" As line  FIELDTERMINATOR '\\t'  MATCH (n:DC_Product), (c:Product{identifier:line.id_hetionet}) Where ID(n)= ToInteger(line.node_id)  Set c.drugcentral='yes', c.resource=split(line.resource,'|') Create (c)-[:equal_to_Product_drugcentral{how_mappeds:line.how_mapped}]->(n); \n'''
-    query = query % (path_of_directory, file_name)
+    query = '''Using Periodic Commit 10000 Load CSV  WITH HEADERS From "file:%smapping_and_merging_into_hetionet/drugcentral/%s" As line  FIELDTERMINATOR '\\t'  MATCH (n:DC_Product), (c:Product{identifier:line.id_hetionet}) Where ID(n)= ToInteger(line.node_id)  Set c.drugcentral='yes', c.resource=split(line.resource,'|') Create (c)-[:equal_to_Product_drugcentral{how_mapped:line.how_mapped}]->(n); \n'''
+    query = query % (path_of_directory,file_name)
     cypher_file.write(query)
     cypher_file.close()
 
@@ -127,10 +135,9 @@ def main():
     if len(sys.argv) > 1:
         path_of_directory = sys.argv[1]
     else:
-        sys.exit('need a path drugcentral product protein')
-
-    print(datetime.datetime.now())
-    print('Generate connection with neo4j and mysql')
+        sys.exit('need a path drugcentral product')
+    print(datetime.datetime.utcnow())
+    print('Generate connection with neo4j')
 
     create_connection_with_neo4j()
 
@@ -147,7 +154,6 @@ def main():
 
     print(datetime.datetime.now())
     print("load product in")
-
     load_product_in()
 
     print(
@@ -155,8 +161,8 @@ def main():
 
     print(datetime.datetime.now())
     print("load DC_product_in")
-
     load_DC_product_in()
+
 
     print(
         '###########################################################################################################################')
