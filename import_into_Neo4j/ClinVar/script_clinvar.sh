dc6760881d6106b4999057aae4332965f72b179c
ckoenigs <dragon493@web.de>
2022-11-21 12:36:13 +0100

    the xml.gz file are save on another place

--- import_into_Neo4j/ClinVar/script_clinvar.sh
+++ import_into_Neo4j/ClinVar/script_clinvar.sh
@@ -6,11 +6,14 @@ path_neo4j=$1
 #path to project
 path_to_project=$2
 
+# path to clinvar saved data
+path_to_clinvar_data=$3
+
 now=$(date +"%F %T")
 echo "Current time: $now"
 echo prepare clinvar data
 
-python3 transform_xml_to_nodes_and_edges.py $path_to_project > output/output_generate_integration_file.txt
+python3 transform_xml_to_nodes_and_edges.py $path_to_project $path_to_clinvar_data > output/output_generate_integration_file.txt
 
 now=$(date +"%F %T")
 echo "Current time: $now"
@@ -35,4 +38,3 @@ $path_neo4j/neo4j restart
 
 sleep 120
 
-rm data/*.gz
