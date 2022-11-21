20f17dc2a7c2fda90412a01d18cf4630f34ca166
ckoenigs <dragon493@web.de>
2022-11-21 09:02:48 +0100

    add edge merging of hippie

--- mapping_and_merging_into_hetionet/hippie/script_hippie_mapping_and_merging.sh
+++ mapping_and_merging_into_hetionet/hippie/script_hippie_mapping_and_merging.sh
@@ -12,7 +12,7 @@ now=$(date +"%F %T")
 echo "Current time: $now"
 echo protein
 
-python3 map_protein_hippie.py $path_to_project > output/output_integration_metabolite.txt
+python3 map_protein_hippie.py $path_to_project > output/output_integration.txt
 
 now=$(date +"%F %T")
 echo "Current time: $now"
@@ -24,4 +24,18 @@ sleep 60
 $path_neo4j/neo4j restart
 sleep 120
 
-# relationships!
+now=$(date +"%F %T")
+echo "Current time: $now"
+echo protein-protein interaction
+
+python3 prepare_edges_merging.py $path_to_project > output/output_integration_edge.txt
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+echo integration of hmdb mapping and nodes into hetionet
+
+$path_neo4j/cypher-shell -u neo4j -p test -f output/cypher_edge.cypher
+
+sleep 60
+$path_neo4j/neo4j restart
+sleep 120
