e4cf7b6dcda98363580a53ae77690893d2022c0f
ckoenigs <dragon493@web.de>
2023-04-05 06:14:41 +0200

    miRBase integration with bioDWH2

---
+++ import_into_Neo4j/miRBase/integrate_mirbase.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#define path to neo4j bin
+path_neo4j=$1
+
+# define import tool
+import_tool=$2
+
+# define bioDWH2 tool
+biodwh2=$3
+
+#password
+password=$4
+
+
+echo load latest version of mirbase and generat GraphML file
+
+dir=./sources/
+
+# prepare workspace and add TTD to bioDWB2 tool
+if [ ! -d "$dir" ]; 
+then
+    echo generate workspace in directory
+    java -jar ../$biodwh2.jar -c .
+
+    java -jar ../$biodwh2.jar --add-data-source . miRBase
+
+fi
+
+java -jar ../$biodwh2.jar -u .
+
+echo $import_tool
+
+
+echo integrate mirbase into neo4j
+
+java -jar ../$import_tool.jar -i sources/miRBase/intermediate.graphml  -e bolt://localhost:7687 --username neo4j --password $password --label-prefix miRBase_ > output/import_tool_output.txt
+
+sleep 30
+
+$path_neo4j/neo4j restart
+
+
+sleep 30
+
+
+
+
