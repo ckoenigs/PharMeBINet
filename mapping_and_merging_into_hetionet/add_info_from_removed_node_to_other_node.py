3d3dbf9636c030d61da31efaf1cbd29777733621
ckoenigs <dragon493@web.de>
2023-04-18 05:58:39 +0200

    change to different Neo4j libary and format

--- mapping_and_merging_into_hetionet/add_info_from_removed_node_to_other_node.py
+++ mapping_and_merging_into_hetionet/add_info_from_removed_node_to_other_node.py
@@ -7,6 +7,7 @@ Created on Fri Jan 26 13:31:43 2018
 
 import sys
 import datetime
+
 sys.path.append("../..")
 sys.path.append("..")
 import create_connection_to_databases
@@ -14,32 +15,33 @@ import create_connection_to_databases
 # dictionary with the differences sources as and the name as property in pharmebinet
 dict_resources = {
     'AEOLUS': 'aeolus',
-    'ClinVar':'clinvar',
+    'ClinVar': 'clinvar',
     'CTD': 'ctd',
-    'dbSNP':'dbsnp',
+    'dbSNP': 'dbsnp',
     'Disease Ontology': 'diseaseOntology',
     'DrugBank': 'drugbank',
-    'GO':'go',
+    'GO': 'go',
     'Hetionet': 'hetionet',
     'hetionet': 'hetionet',
-    'HPO':'hpo',
-    'IID':'iid',
+    'HPO': 'hpo',
+    'IID': 'iid',
     'MonDO': 'mondo',
-    'NCBI':'ncbi',
+    'NCBI': 'ncbi',
     'NDF-RT': 'ndf_rt',
-    'OMIM':'omim',
+    'OMIM': 'omim',
     # for pathway it is missing
-    'PharmGKB':'pharmgkb',
-    'Reactome':'reactome',
+    'PharmGKB': 'pharmgkb',
+    'Reactome': 'reactome',
     'SIDER': 'sider',
-    'UniProt':'uniprot'
+    'UniProt': 'uniprot'
 }
 
 
 # connect with the neo4j database
 def database_connection():
-    global g
-    g = create_connection_to_databases.database_connection_neo4j()
+    global g, driver
+    driver = create_connection_to_databases.database_connection_neo4j_driver()
+    g = driver.session()
 
 
 # dictionary with label as key and value is the constraint property
@@ -51,16 +53,27 @@ Get for all label the unique property, after a : is the label and after a . is t
 
 
 def generate_dictionary_for_labels():
-    query = '''CALL db.constraints'''
+    query = 'SHOW INDEXES'
+    # query = '''CALL db.constraints'''
     results = g.run(query)
-    for name, constraint_string, details, in results:
-        # print(constraint_string)
-        label = constraint_string.split(':')[1].split(' )')[0]
-        unique_property = constraint_string.split('.')[1].split(' ')[0].rsplit(')', 1)[0]
-        dict_label_to_unique_prop[label] = unique_property
+    for record in results:
+        # [name, constraint_string, details] = record.values()
+        # # print(constraint_string)
+        # label = constraint_string.split(':')[1].split(' )')[0]
+        # unique_property = constraint_string.split('.')[1].split(' ')[0].rsplit(')', 1)[0]
+        # dict_label_to_unique_prop[label] = unique_property
+        [id, name, state, populationPercent, type, entityType, labelsOrTypes, properties, indexProvider,
+         owningConstraint] = record.values()
+        if labelsOrTypes:
+            if len(labelsOrTypes) > 1:
+                sys.exit('ohno, multiple labels')
+            if len(properties) > 1:
+                sys.exit('ohno multiple properties')
+            dict_label_to_unique_prop[labelsOrTypes[0]] = properties[0]
     # print(dict_label_to_unique_prop)
 
-cypher_file=open('cypher_merge.cypher','w',encoding='utf-8')
+
+cypher_file = open('cypher_merge.cypher', 'w', encoding='utf-8')
 
 '''
 add the resource to merged node
@@ -71,47 +84,47 @@ def merge_resource_to_node(delete_node, label, merged_node):
     # get all resources of a node
 
     query = '''Match (s:%s) Where s.identifier in ["%s"] Return s'''
-    query = query % (label, delete_node+'","'+merged_node)
+    query = query % (label, delete_node + '","' + merged_node)
     results = g.run(query)
     dict_delete_node = {}
-    dict_merge_node={}
-    for node, in results:
-        identifier= node["identifier"]
-        if identifier==delete_node:
+    dict_merge_node = {}
+    for record in results:
+        node = record.data()['s']
+        identifier = node["identifier"]
+        if identifier == delete_node:
             dict_delete_node = dict(node)
         else:
-            dict_merge_node= dict(node)
+            dict_merge_node = dict(node)
     resources_list = dict_delete_node['resource'] if 'resource' in dict_delete_node else []
     url_ctd = dict_delete_node['ctd_url'] if 'ctd_url' in dict_delete_node else ''
 
-    combined_resource=set(resources_list).union(dict_merge_node['resource']) if 'resource' in dict_merge_node else set()
+    combined_resource = set(resources_list).union(
+        dict_merge_node['resource']) if 'resource' in dict_merge_node else set()
 
     print('delete node:' + delete_node)
     print('merged node:' + merged_node)
 
-    query_start= '''Match (s:%s{identifier:"%s"}) Set s.resource=["%s"], '''
-    query_start =query_start %(label, merged_node, '","'.join(combined_resource))
+    query_start = '''Match (s:%s{identifier:"%s"}) With s Set s.resource=["%s"], '''
+    query_start = query_start % (label, merged_node, '","'.join(combined_resource))
     # make all this source to a yes in the merged node
 
     for source in resources_list:
         add_query = ''' s.%s="yes",''' % (dict_resources[source])
         query_start += add_query
 
-
     # if it comes from CTD also add the ctd url
     if url_ctd != '' and 'url_ctd' not in dict_merge_node:
-
         add_query = ''' s.url_ctd="%s", ''' % (url_ctd)
         query_start += add_query
 
-    if 'merged_identifier' in dict_merge_node :
-        merge_ids=set(dict_merge_node['merged_identifier'])
+    if 'merged_identifier' in dict_merge_node:
+        merge_ids = set(dict_merge_node['merged_identifier'])
         merge_ids.add(delete_node)
     else:
-        merge_ids=set([delete_node])
+        merge_ids = set([delete_node])
 
-    add_part='s.merged_identifier=["%s"]' %('","'.join(merge_ids))
-    query_start= query_start+ add_part +' ;\n'
+    add_part = 's.merged_identifier=["%s"]' % ('","'.join(merge_ids))
+    query_start = query_start + add_part + ';\n '
     cypher_file.write(query_start)
 
 
@@ -127,13 +140,13 @@ def get_rela_info_and_add_cypher_queries(identifier, label, merged_node_id):
     # print(query)
     results = g.run(query)
 
-    set_of_rela_label_tuple=set()
-    for rela_type,  node_labels,  in results:
-
-        node_label=node_labels[0]
-        if not (rela_type,node_label) in set_of_rela_label_tuple:
-            set_of_rela_label_tuple.add((rela_type,node_label) )
-            query='''Match (c1:%s{identifier:"%s"})-[r:%s]->(a:%s), (c2:%s{identifier:"%s"}) Where ID(a)<>ID(c2) Merge (c2)-[h:%s]->(a) On Create Set h=r ;\n '''
+    set_of_rela_label_tuple = set()
+    for record in results:
+        [rela_type, node_labels] = record.values()
+        node_label = node_labels[0]
+        if not (rela_type, node_label) in set_of_rela_label_tuple:
+            set_of_rela_label_tuple.add((rela_type, node_label))
+            query = '''Match (c1:%s{identifier:"%s"})-[r:%s]->(a:%s), (c2:%s{identifier:"%s"}) Where ID(a)<>ID(c2) Merge (c2)-[h:%s]->(a) On Create Set h=r ;\n '''
             query = query % (label, identifier, rela_type, node_label, label, merged_node_id, rela_type)
             cypher_file.write(query)
 
@@ -143,19 +156,17 @@ def get_rela_info_and_add_cypher_queries(identifier, label, merged_node_id):
     query = query % (label, identifier)
     # print(query)
     results = g.run(query)
-    for rela_type,  node_labels, in results:
+    for record in results:
+        [rela_type, node_labels] = record.values()
 
-        node_label=node_labels[0]
-        if not (rela_type,node_label) in set_of_rela_label_tuple:
-            set_of_rela_label_tuple.add((rela_type,node_label) )
-            query='''Match (c1:%s{identifier:"%s"})<-[r:%s]-(a:%s), (c2:%s{identifier:"%s"}) Where ID(a)<>ID(c2) Merge (c2)<-[h:%s]-(a) On Create Set h=r ;\n '''
+        node_label = node_labels[0]
+        if not (rela_type, node_label) in set_of_rela_label_tuple:
+            set_of_rela_label_tuple.add((rela_type, node_label))
+            query = '''Match (c1:%s{identifier:"%s"})<-[r:%s]-(a:%s), (c2:%s{identifier:"%s"}) Where ID(a)<>ID(c2) Merge (c2)<-[h:%s]-(a) On Create Set h=r ;\n '''
             query = query % (label, identifier, rela_type, node_label, label, merged_node_id, rela_type)
             cypher_file.write(query)
 
     print('number of rela from other to node:' + str(len(list_other_to_node)))
-
-
-
 
 
 '''
@@ -167,7 +178,6 @@ def delete_merged_node(identifier, label):
     query = ''' Match (n:%s{identifier:"%s"}) Detach Delete n;\n'''
     query = query % (label, identifier)
     cypher_file.write(query)
-
 
 
 #
@@ -221,6 +231,8 @@ def main():
 
     # delete_merged_node('DB13390', 'Compound')
     delete_merged_node(old_id, label)
+
+    driver.close()
 
     print('##########################################################################')
 
