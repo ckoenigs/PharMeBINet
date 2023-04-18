c4be4d6f4fa47ac8a5cbca8b956500f95811f3bf
ckoenigs <dragon493@web.de>
2023-04-18 06:18:32 +0200

    integration RNA and mapping gene

---
+++ mapping_and_merging_into_hetionet/refseq/integrate_RNA_refseq.py
@@ -0,0 +1,185 @@
+import datetime
+import csv
+import sys
+from collections import defaultdict
+
+sys.path.append("../..")
+import create_connection_to_databases
+import pharmebinetutils
+
+sys.path.append("..")
+from change_xref_source_name_to_a_specifice_form import go_through_xrefs_and_change_if_needed_source_name
+
+'''
+create connection to neo4j 
+'''
+
+
+def create_connection_with_neo4j_mysql():
+    # create connection with neo4j
+    global g, driver
+    driver = create_connection_to_databases.database_connection_neo4j_driver()
+    g = driver.session()
+
+
+def get_node_properties_and_prepare_query(label, additional_label, id_property_name):
+    """
+    Prapare query for node with an additonal label.
+    :param label: string
+    :param additional_label: string additional label in RNA
+    :param id_property_name: string
+    :return:
+    """
+    query = '''MATCH (p:%s) WITH DISTINCT keys(p) AS keys UNWIND keys AS keyslisting WITH DISTINCT keyslisting AS allfields RETURN allfields as l;'''
+    query = query % (label)
+    result = g.run(query)
+
+    list_of_properties = []
+    for property in result:
+        property = property.data()['l']
+        if property not in [id_property_name, 'description']:
+            list_of_properties.append(property + ':m.' + property)
+        else:
+            if property == id_property_name:
+                list_of_properties.append('identifier:m.' + property)
+            else:
+                list_of_properties.append('name:m.' + property)
+
+    query = f'Match (m:{label}{{id:line.id}}) Create (n:RNA :{additional_label} {{ {", ".join(list_of_properties)}, resource:["RefSeq"], source:"RefSeq", license:"CC0 with attribution", refseq:"yes", url:""+line.id}}) Create (n)-[:equals_rna_refseq]->(m)'
+    return query
+
+
+# cypher file
+cypher_file = open('output/cypher.cypher', 'w', encoding='utf-8')
+cypher_file.write(pharmebinetutils.prepare_index_query('RNA', 'identifier', 'mirna'))
+
+
+def generate_tsv_and_query(label, additional_label, id_property_name):
+    """
+
+    :param label:
+    :param additional_label:
+    :param id_property_name:
+    :return:
+    """
+    file_name = f'output/{additional_label}.tsv'
+    file = open(file_name, 'w', encoding='utf-8')
+    csv_writer = csv.writer(file, delimiter='\t')
+    csv_writer.writerow(['id'])
+    query = get_node_properties_and_prepare_query(label, additional_label, id_property_name)
+    query = pharmebinetutils.get_query_import(path_of_directory,
+                                              f'mapping_and_merging_into_hetionet/refseq/{file_name}',
+                                              query)
+    cypher_file.write(query)
+    return csv_writer, file
+
+
+def load_in_all_pre_miRNA(query, label, additional_label, property_name):
+    """
+    Prepare first the TSV file and the cypher query. The fill the tsv file.
+    :param query:
+    :param label:
+    :param additional_label:
+    :param property_name:
+    :return:
+    """
+    csv_writer, file = generate_tsv_and_query(label, additional_label, property_name)
+
+    results = g.run(query)
+
+    counter = 0
+    for record in results:
+        identifier = record.data()['n.id']
+        csv_writer.writerow([identifier])
+        counter += 1
+    file.close()
+
+    print('number of ', additional_label, counter)
+
+
+def prepare_different_nodes():
+    """
+    prepare the different queries and tsv files
+    :return:
+    """
+    list_of_labels = ["refSeq_PrimaryTranscript", "refSeq_lncRNA", "refSeq_mRNA", "refSeq_miRNA",
+                      # "refSeq_Exon", it is not only RNA but also a lot of different types
+                      "refSeq_Transcript", "refSeq_snoRNA", "refSeq_rRNA", "refSeq_RNasePRNA", "refSeq_RNaseMRPRNA",
+                      "refSeq_scRNA", "refSeq_snRNA", "refSeq_YRNA", "refSeq_antisenseRNA", "refSeq_tRNA",
+                      "refSeq_vaultRNA", "refSeq_telomeraseRNA", "refSeq_ncRNA"
+                      ]
+    for label in list_of_labels:
+        query =f'MATCH (n:{label})  RETURN  n.id'
+
+        load_in_all_pre_miRNA(query, label, label.split('_')[1], 'id')
+    cypher_file.close()
+
+
+def prepare_edge():
+    """
+    perpare rela tsv and cypher file and query
+    :return:
+    """
+    file_name = 'output/edge_rna.tsv'
+    file = open(file_name, 'w', encoding='utf-8')
+    csv_writer = csv.writer(file, delimiter='\t')
+    csv_writer.writerow(['pre_id', 'mirna_id', 'from', 'to'])
+    query = '''MATCH (n:miRBase_miRNA)-[rela]-(m:miRBase_pre_miRNA) RETURN  n.id, m.id,rela.from, rela.to  '''
+    results = g.run(query)
+
+    counter = 0
+    for record in results:
+        [mi_id, pre_id, from_info, to_info] = record.values()
+        csv_writer.writerow([pre_id, mi_id, from_info, to_info])
+        counter += 1
+
+    print('number of edges', counter)
+
+    with open('output/cypher_edge.cypher', 'w', encoding='utf-8') as cypher_file_edge:
+        query = 'MATCH (n:miRNA{identifier:line.mirna_id}),(m:pre_miRNA{identifier:line.pre_id}) Create (m)-[:CLEAVES_TO_RctR{from:line.from, to:line.to, source:"miRBase", resource:["miRBase"], license:"CC0 with attribution", url:"https://www.mirbase.org/cgi-bin/mirna_entry.pl?acc="+line.pre_id, mirbase:"yes"}]->(n)'
+        query = pharmebinetutils.get_query_import(path_of_directory,
+                                                  f'mapping_and_merging_into_hetionet/miRBase/{file_name}',
+                                                  query)
+        cypher_file_edge.write(query)
+
+
+def main():
+    global path_of_directory
+    if len(sys.argv) > 1:
+        print(sys.argv)
+        path_of_directory = sys.argv[1]
+    else:
+        sys.exit('need a path')
+
+    print(datetime.datetime.now())
+    print('Generate connection with neo4j and mysql')
+
+    create_connection_with_neo4j_mysql()
+
+    print(
+        '###########################################################################################################################')
+
+    print(datetime.datetime.now())
+    print('Prepare the different ')
+
+    prepare_different_nodes()
+
+    print(
+        '###########################################################################################################################')
+
+    print(datetime.datetime.now())
+    print('prepare edge information')
+
+    # prepare_edge()
+
+    driver.close()
+
+    print(
+        '###########################################################################################################################')
+
+    print(datetime.datetime.now())
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
---
+++ mapping_and_merging_into_hetionet/refseq/map_gene_refseq.py
@@ -0,0 +1,146 @@
+import datetime
+import csv, sys
+
+sys.path.append("../..")
+import create_connection_to_databases
+import pharmebinetutils
+
+'''
+create connection to neo4j and mysql
+'''
+
+
+def create_connection_with_neo4j():
+    # create connection with neo4j
+    # authenticate("localhost:7474", "neo4j", "test")
+    global g, driver
+    driver = create_connection_to_databases.database_connection_neo4j_driver()
+    g = driver.session()
+
+
+# dictionary with pharmebinet genes with identifier as key and value node as dictionary
+dict_genes_pharmebinet = {}
+
+dict_hgnc_to_gene_id = {}
+
+
+def load_pharmebinet_genes_in():
+    '''
+    load in all genes from pharmebinet in a dictionary
+    '''
+    query = '''MATCH (n:Gene) RETURN n'''
+    results = g.run(query)
+
+    for record in results:
+        node = record.data()['n']
+        identifier = node['identifier']
+        dict_genes_pharmebinet[identifier] = dict(node)
+        xrefs = node['xrefs'] if 'xrefs' in node else []
+        for xref in xrefs:
+            if xref.startswith('HGNC:'):
+                hgnc_id = int(xref.rsplit(':', 1)[1])
+                if not hgnc_id in dict_hgnc_to_gene_id:
+                    dict_hgnc_to_gene_id[hgnc_id] = set()
+                dict_hgnc_to_gene_id[hgnc_id].add(identifier)
+
+    print('number of gene nodes in pharmebinet:' + str(len(dict_genes_pharmebinet)))
+
+
+def generate_files():
+    '''
+    Generate cypher and tsv for generating the new nodes and the relationships
+    '''
+    global writer
+    file_name = 'gene/mapping_gene.tsv'
+    csvfile = open(file_name, 'w', encoding='utf-8')
+    writer = csv.writer(csvfile, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(['id', 'id_refseq'])
+
+    # generate cypher file
+    cypher_file = open('output/cypher.cypher', 'a', encoding='utf-8')
+    query = ''' Match (c:Gene{ identifier:line.id}), (n:refSeq_Gene{ id:line.id_refseq})  Create (c)-[:equal_to_gene_refseq]->(n) Set c.refseq="yes", c.resource=c.resource+"RefSeq"'''
+    query = pharmebinetutils.get_query_import(path_of_directory,
+                                              f'mapping_and_merging_into_hetionet/refseq/{file_name}',
+                                              query)
+    cypher_file.write(query)
+    cypher_file.close()
+
+
+def load_and_map_RefSeq_genes():
+    """
+    Load all human genes and try to map with entrez id and name. All mapped are written into a TSV file.
+    :return:
+    """
+    query = '''MATCH (n:refSeq_Gene) RETURN n.id, n.name, n.xrefs'''
+    results = g.run(query)
+
+    counter = 0
+    mapped = 0
+    for record in results:
+        counter += 1
+        [gene_id, name, xrefs] = record.values()
+        if xrefs:
+            for xref in xrefs:
+                if xref.startswith('GeneID'):
+                    id = xref.split(':')[1]
+                    if id in dict_genes_pharmebinet:
+                        writer.writerow([id, gene_id])
+                        mapped += 1
+                        continue
+
+    print('number of existing genes', counter)
+    print('number of mapped genes', mapped)
+
+
+# path to directory
+path_of_directory = ''
+
+
+def main():
+    global path_of_directory
+    if len(sys.argv) > 1:
+        print(sys.argv)
+        path_of_directory = sys.argv[1]
+    else:
+        sys.exit('need a path')
+
+    print(datetime.datetime.now())
+    print('Generate connection with neo4j')
+
+    create_connection_with_neo4j()
+
+    print(
+        '###########################################################################################################################')
+
+    print(datetime.datetime.now())
+    print('Map generate tsv and cypher file ')
+
+    generate_files()
+
+    print(
+        '###########################################################################################################################')
+
+    print(datetime.datetime.now())
+    print('Load all genes from pharmebinet into a dictionary')
+
+    load_pharmebinet_genes_in()
+
+    print(
+        '###########################################################################################################################')
+
+    print(datetime.datetime.now())
+    print('Load all ctd genes from neo4j into a dictionary')
+
+    load_and_map_RefSeq_genes()
+
+    driver.close()
+
+    print(
+        '###########################################################################################################################')
+
+    print(datetime.datetime.now())
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
