66064150bdf256fddf6eb0ca0df8f9985b2fd959
ckoenigs <dragon493@web.de>
2023-04-19 08:46:14 +0200

    refseq merge and mapping script

---
+++ mapping_and_merging_into_hetionet/refseq/script_refseq.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#define path to neo4j bin
+path_neo4j=$1
+
+#path to project
+path_to_project=$2
+
+#password
+password=$3
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+echo add RNA
+
+python3 integrate_RNA_refseq.py $path_to_project > output/RNA_map.txt
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+echo map genes
+
+python3 map_gene_refseq.py $path_to_project > output/protein_map.txt
+
+echo integrate connection with neo4j shell
+now=$(date +"%F %T")
+echo "Current time: $now"
+
+$path_neo4j/cypher-shell -u neo4j -p $password -f output/cypher.cypher
+
+sleep 30
+
+$path_neo4j/neo4j restart
+
+
+sleep 60
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+echo create rna-gene edge
+
+python3 perpare_gene_rna_edges.py $path_to_project > output/rna_edge.txt
+
+echo integrate connection with neo4j shell
+now=$(date +"%F %T")
+echo "Current time: $now"
+
+$path_neo4j/cypher-shell -u neo4j -p $password -f output/cypher_edge.cypher
+
+sleep 30
+
+$path_neo4j/neo4j restart
+
+
+sleep 60
