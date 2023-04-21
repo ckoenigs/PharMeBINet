23944f64e5c9bd0a7715f68f390654f6fa830d30
ckoenigs <dragon493@web.de>
2023-04-21 05:57:18 +0200

    update version of uberon script

--- import_into_Neo4j/Uberon/script_uberon.sh
+++ import_into_Neo4j/Uberon/script_uberon.sh
@@ -3,22 +3,35 @@
 #define path to neo4j bin
 path_neo4j=$1
 
-#download uberon
-wget -O data/ext.obo "http://purl.obolibrary.org/obo/uberon/ext.obo"
+#path to project
+path_to_project=$2
+
+#password
+password=$3
+
+#download do
+wget  -O data/ext.obo "http://purl.obolibrary.org/obo/uberon/ext.obo"
 
 
-python3 ../EFO/transform_obo_to_tsv_and_cypher_file.py data/ext.obo Uberon uberon_extend $path_to_project > output_generate_integration_file.txt
+python3 ../EFO/transform_obo_to_tsv_and_cypher_file.py data/ext.obo Uberon uberon_extend $path_to_project > output/output_generate_integration_file.txt
 
 now=$(date +"%F %T")
 echo "Current time: $now"
 
-echo integrate uberon into neo4j
+echo integrate do into neo4j
 
-$path_neo4j/cypher-shell -u neo4j -p test -f cypher.cypher > output_cypher_integration.txt 2>&1
+$path_neo4j/cypher-shell -u neo4j -p $password -f cypher.cypher
 
-sleep 60
+now=$(date +"%F %T")
+echo "Current time: $now"
+
+echo integrate do into neo4j
+
+$path_neo4j/cypher-shell -u neo4j -p $password -f cypher_edge.cypher
+
+sleep 20
 
 $path_neo4j/neo4j restart
 
 
-sleep 120
+sleep 30
