eb1cb543c4ce2a26f11aab9cbbe59eef815e3ce9
ckoenigs <dragon493@web.de>
2023-05-04 06:55:59 +0200

    finished mapping to refseq

--- mapping_and_merging_into_hetionet/RNAinter/rna_RNAInter.py
+++ mapping_and_merging_into_hetionet/RNAinter/rna_RNAInter.py
@@ -35,18 +35,23 @@ def rna_RNAInter():
     dict_ensembl_to_identifiers = {}
     dict_name_to_identifiers = {}
     dict_gene_to_identifier = {}
+    dict_product_to_identifiers = {}
 
-    query = "MATCH (n:RNA) RETURN n.identifier, n.name,n.xrefs, n.resource, n.gene"
+    query = "MATCH (n:RNA) RETURN n.identifier, n.name,n.xrefs, n.resource, n.gene, n.product, labels(n)"
     result = g.run(query)
 
     for record in result:
-        [identifier, name, xref, resource, gene] = record.values()
+        [identifier, name, xref, resource, gene, product, labels] = record.values()
 
         RNA[identifier] = resource
 
+        is_miRNA = False if not 'miRNA' in labels else True
+
         if name is not None:
             pharmebinetutils.add_entry_to_dict_to_set(dict_name_to_identifiers, name.lower(), identifier)
-            
+        if product is not None:
+            pharmebinetutils.add_entry_to_dict_to_set(dict_product_to_identifiers, product, identifier)
+
         dict_gene_to_identifier[gene] = identifier
         if xref is not None:
             for x in xref:
@@ -54,9 +59,22 @@ def rna_RNAInter():
                 pubid = x.split(':', 1)[1]
 
                 if x.startswith('miRBase'):
+                    # avoid mirbase xrefs where the id is pr-RNA (MI****)
+                    if is_miRNA and not pubid.startswith('MIMAT'):
+                        continue
                     pharmebinetutils.add_entry_to_dict_to_set(dict_mirBase_to_identifiers, pubid, identifier)
                 elif x.startswith('Ensembl'):
                     pharmebinetutils.add_entry_to_dict_to_set(dict_ensembl_to_identifiers, pubid, identifier)
+
+    dict_ncbi_gene_id_to_identifier = {}
+    dict_product_to_identifiers = {}
+
+    query = "MATCH (m:Gene)--(n:RNA) RETURN n.identifier, m.identifier"
+    result = g.run(query)
+
+    for record in result:
+        [identifier, gene_id] = record.values()
+        pharmebinetutils.add_entry_to_dict_to_set(dict_ncbi_gene_id_to_identifier, gene_id, identifier)
 
     # save the identifier and the Raw_ID in a tsv file
     file_name = 'output/RNA_mapping.tsv'
@@ -68,35 +86,51 @@ def rna_RNAInter():
         query = "MATCH (n:rna_RNAInter) RETURN n.Raw_ID, n.Interactor"
         result = g.run(query)
 
-        counter=0
-        counter_mapped=0
+        counter = 0
+        counter_mapped = 0
         for record in result:
             [raw_id, inter] = record.values()
-            counter+=1
+            counter += 1
             source = ''
             if raw_id is not None:
-                rid = raw_id.split(":", 1)
-                if len(rid) == 2 and len(rid[1]) > 2:
-                    rid = rid[1]
+                rid_split = raw_id.split(":", 1)
+                if len(rid_split) == 2 and len(rid_split[1]) > 2:
+                    rid = rid_split[1]
                 else:
                     rid = raw_id
-                source = rid[0]
+                source = rid_split[0]
+
+            # manual check that the gene symbol and the ensemble id are not fitting and no ensemble gene mapping method
+            # exist!
+            if rid == 'ENSG00000088298':
+                continue
+
             if source == 'Ensembl' and rid in dict_ensembl_to_identifiers:
-                counter_mapped+=1
+                counter_mapped += 1
                 write_infos_into_file(writer, raw_id, dict_ensembl_to_identifiers[rid], 'ensembl')
             elif source == 'miRBase' and rid in dict_mirBase_to_identifiers:
-                counter_mapped+=1
+                counter_mapped += 1
                 write_infos_into_file(writer, raw_id, dict_mirBase_to_identifiers[rid], 'mirbase')
+            elif source == "NCBI" and rid in dict_ncbi_gene_id_to_identifier:
+                counter_mapped += 1
+                write_infos_into_file(writer, raw_id, dict_ncbi_gene_id_to_identifier[rid], 'ncbi_gene')
+
+            elif source == 'miRBase' and inter in dict_product_to_identifiers:
+                counter_mapped += 1
+                write_infos_into_file(writer, raw_id, dict_product_to_identifiers[inter], 'inter_product')
             elif raw_id in dict_gene_to_identifier:
-                counter_mapped+=1
+                counter_mapped += 1
                 write_infos_into_file(writer, raw_id, [dict_gene_to_identifier[raw_id]], 'raw_id_gene')
+            elif inter in dict_gene_to_identifier:
+                counter_mapped += 1
+                write_infos_into_file(writer, raw_id, [dict_gene_to_identifier[inter]], 'inter_gene')
             elif raw_id in dict_name_to_identifiers:
-                counter_mapped+=1
+                counter_mapped += 1
                 write_infos_into_file(writer, raw_id, dict_name_to_identifiers[rid], 'name')
 
     tsv_file.close()
-    print('number of nodes',counter)
-    print('number of mapped nodes',counter_mapped)
+    print('number of nodes', counter)
+    print('number of mapped nodes', counter_mapped)
 
     print("######### Start: Cypher #########")
     query = f'Match (p1:rna_RNAInter{{Raw_ID:line.Raw_ID}}),(p2:RNA{{identifier:line.identifier}}) SET p2.resource = split(line.resource,"|"), p2.RNAInter = "yes" Create (p1)-[:associateRNA{{how_mapped:line.how_mapped  }}]->(p2)'
