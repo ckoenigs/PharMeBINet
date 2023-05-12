c8a101a8956d3e949bcba51fd2900f117f320336
ckoenigs <dragon493@web.de>
2023-05-12 05:44:51 +0200

    add hgnc integration script with biodwh2

---
+++ import_into_Neo4j/hgnc/integrate_hgnc.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#define path to neo4j bin
+path_neo4j=$1
+
+# define import tool
+import_tool=$2
+
+# define bioDWH2 tool
+biodwh2=$3
+
+#password
+password=$4
+
+
+echo load latest version of hgnc and generat GraphML file
+
+dir=./sources/
+
+# prepare workspace and add TTD to bioDWB2 tool
+if [ ! -d "$dir" ]; 
+then
+    echo generate workspace in directory
+    java -jar ../$biodwh2.jar -c .
+
+    java -jar ../$biodwh2.jar --add-data-source . HGNC
+
+fi
+
+java -jar ../$biodwh2.jar -u .
+
+echo $import_tool
+
+
+echo integrate hgnc into neo4j
+
+java -jar ../$import_tool.jar -i sources/HGNC/intermediate.graphml  -e bolt://localhost:7687 --username neo4j --password $password --label-prefix hgnc_ --indices "hgnc_Gene.hgnc_id;hgnc_miRNA.mirbase_accession;hgnc_Protein.uniprot_id" > output/import_tool_output.txt
+
+echo finished integration
+
+sleep 30
+
+$path_neo4j/neo4j restart
+
+
+sleep 30
