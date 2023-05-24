501dced20b8862aac133227a53ae49bcccf2b6c9
ckoenigs <dragon493@web.de>
2023-05-24 05:41:36 +0200

    considere frequency information and integrate them

--- mapping_and_merging_into_hetionet/adrecs/merge_edge_integration.py
+++ mapping_and_merging_into_hetionet/adrecs/merge_edge_integration.py
@@ -18,7 +18,7 @@ def create_connection_with_neo4j():
     graph_database = driver.session()
 
 
-def load_chemical_SE_edge_info(csv_file, ):
+def load_chemical_SE_edge_info(csv_file ):
     """
     Load all pairs and relationship information from chemical over ADReCS to SE and write information into file
     :param csv_file: csv writer
@@ -26,19 +26,30 @@ def load_chemical_SE_edge_info(csv_file, ):
     """
 
     dict_pair_to_edge = {}
-    query = '''MATCH (p:Chemical)-[]-(r:ADReCS_Drug)-[v]-(n:ADReCS_ADR)-[]-(b:SideEffect) RETURN p.identifier, b.identifier, v, r.id'''
+    query = '''MATCH (p:Chemical)-[]-(r:ADReCS_Drug)-[v]-(n:ADReCS_ADR)-[]-(b:SideEffect) Where not v.frequency_faers is null RETURN p.identifier, b.identifier, v, r.id'''
     print(query)
     results = graph_database.run(query)
     for record in results:
         [chemical_id, node_id, edge, adrecs_drug_id] = record.values()
-        if (chemical_id, node_id) in dict_pair_to_edge:
-            print(chemical_id, node_id)
+        edge = dict(edge)
+        if (chemical_id, node_id) in dict_pair_to_edge :
+            dict_pair_to_edge[(chemical_id, node_id)][0].append(edge)
             continue
-        dict_pair_to_edge[(chemical_id, node_id)] = dict(edge)
-        csv_file.writerow([chemical_id, node_id, adrecs_drug_id])
+        dict_pair_to_edge[(chemical_id, node_id)] = [[edge],adrecs_drug_id]
+
+
     print('number of drug-SE relationships in adrecs:' + str(
         len(dict_pair_to_edge)))
 
+    for (chemical_id, node_id), list_of_edge_and_adr_id in dict_pair_to_edge.items():
+        adrecs_drug_id=list_of_edge_and_adr_id[1]
+        frequencies=set()
+        grades=set()
+        for edge in list_of_edge_and_adr_id[0]:
+            grade= edge['severity_grade_faers']
+            grades.add(grade)
+            frequencies.add(edge['frequency_faers']+' for ' + grade)
+        csv_file.writerow([chemical_id, node_id, adrecs_drug_id, '|'.join(frequencies),'|'.join(grades)])
 
 '''
 generate relationships between drug-se that have edges in adrecs 
@@ -46,7 +57,7 @@ generate relationships between drug-se that have edges in adrecs
 
 
 def create_cypher_file(file_path, rela):
-    query = ''' MATCH (d:Chemical{identifier:line.chemical_id}),(c:SideEffect{identifier:line.se_id}) Merge (d)-[l: %s]->(c) On Create Set l.resource= ['ADReCS'], l.adrecs= "yes", l.license="CC BY-NC-SA 4.0", l.url="http://bioinf.xmu.edu.cn/ADReCS/drugSummary.jsp?drug_id="+line.adr_id, l.source="ADReCS" On Match Set l.adrecs="yes", l.resource="ADReCS"+l.resource'''
+    query = ''' MATCH (d:Chemical{identifier:line.chemical_id}),(c:SideEffect{identifier:line.se_id}) Merge (d)-[l: %s]->(c) On Create Set l.resource= ['ADReCS'], l.fears_frequencies=split(line.fears_frequencies,"|"), l.fears_severity_grades=split(line.fears_severity_grades, "|"), l.adrecs= "yes", l.license="CC BY-NC-SA 4.0", l.url="http://bioinf.xmu.edu.cn/ADReCS/drugSummary.jsp?drug_id="+line.adr_id, l.source="ADReCS" On Match Set l.adrecs="yes", l.resource="ADReCS"+l.resource, l.fears_frequencies=split(line.fears_frequencies,"|"), l.fears_severity_grades=split(line.fears_severity_grades, "|")'''
     query = query % (rela)
     query = pharmebinetutils.get_query_import(path_of_directory,
                                               f'mapping_and_merging_into_hetionet/adrecs/{file_path}', query)
@@ -78,7 +89,7 @@ def main():
 
     file = open(file_name, 'w', encoding="utf-8")
     csv_edge = csv.writer(file, delimiter='\t', lineterminator='\n')
-    csv_edge.writerow(['chemical_id', 'se_id', 'adr_id'])
+    csv_edge.writerow(['chemical_id', 'se_id', 'adr_id', 'fears_frequencies', 'fears_severity_grades'])
 
     load_chemical_SE_edge_info(csv_edge)
 
