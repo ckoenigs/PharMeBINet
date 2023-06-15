4818ed3e44f5a4e21409636b972dc02e0a8de14c
ckoenigs <dragon493@web.de>
2023-06-15 06:33:44 +0200

    create gene-rna edge and generate combined rna-protein edges

---
+++ mapping_and_merging_into_hetionet/refseq/create_rna_protein_edge.py
@@ -0,0 +1,110 @@
+import datetime
+import csv, sys
+import general_function_refseq
+
+sys.path.append("../..")
+import create_connection_to_databases
+import pharmebinetutils
+
+'''
+create connection to neo4j and mysql
+'''
+
+
+def create_connection_with_neo4j():
+    # create connection with neo4j
+    # authenticate("localhost:7474", "neo4j", "test")
+    global g, driver
+    driver = create_connection_to_databases.database_connection_neo4j_driver()
+    g = driver.session()
+
+
+def prepare_edge(tuple_label_and_refseq_label):
+    """
+    perpare rela tsv and cypher file and query
+    :return:
+    """
+    file_name = f'output/edge_rna_{tuple_label_and_refseq_label[0]}.tsv'
+    file = open(file_name, 'w', encoding='utf-8')
+    csv_writer = csv.writer(file, delimiter='\t')
+    csv_writer.writerow(['identifier', 'pre_id', 'start', 'end', 'strand', 'source', 'phase', 'url'])
+    #
+    query = f'''MATCH (n:{tuple_label_and_refseq_label[0]})--(:{tuple_label_and_refseq_label[1]})-[rela]-(m:refSeq_RNA) RETURN  n.identifier,  m.id , rela  '''
+    print(query)
+    results = g.run(query)
+
+    counter = 0
+    dict_tuple_to_relas = {}
+    for record in results:
+        [identifier, pre_id, rela] = record.values()
+        rela = dict(rela)
+        if not (identifier, pre_id) in dict_tuple_to_relas:
+            dict_tuple_to_relas[(identifier, pre_id)] = []
+        dict_tuple_to_relas[(identifier, pre_id)].append(rela)
+        counter += 1
+
+    for (identifier, pre_id), list_of_relas in dict_tuple_to_relas.items():
+        dict_rela = {}
+        for rela in list_of_relas:
+            for key, values in rela.items():
+                if key not in dict_rela:
+                    if key in ['start', 'end', 'phase']:
+                        dict_rela[key] = set([str(values)])
+                    else:
+                        dict_rela[key] = values
+                elif key in ['start', 'end', 'phase']:
+                    dict_rela[key].add(str(values))
+                elif dict_rela[key] != values:
+                    print(key, dict_rela[key], values)
+        csv_writer.writerow(
+            [identifier, pre_id, '|'.join(dict_rela['start']), '|'.join(dict_rela['end']), dict_rela['strand'], dict_rela['source'],
+             '|'.join( dict_rela['phase']), general_function_refseq.prepare_url_id(pre_id)])
+    file.close()
+
+    print('number of edges', counter)
+
+    with open('output/cypher_edge.cypher', 'a', encoding='utf-8') as cypher_file_edge:
+        query = f'MATCH (n:{tuple_label_and_refseq_label[0]}{{identifier:line.identifier}}),(m:RNA{{identifier:line.pre_id}}) Create (m)-[:{tuple_label_and_refseq_label[2]}{{starts:split(line.start,"|"), ends:split(line.end,"|"),  phases:split(line.phase,"|"), source:"RefSeq from "+line.source, strand:line.strand, resource:["RefSeq"], license:"https://www.ncbi.nlm.nih.gov/home/about/policies/", url:"https://identifiers.org/refseq:"+line.url,  refseq:"yes"}}]->(n)'
+        query = pharmebinetutils.get_query_import(path_of_directory,
+                                                  f'mapping_and_merging_into_hetionet/refseq/{file_name}',
+                                                  query)
+        cypher_file_edge.write(query)
+
+
+# path to directory
+path_of_directory = ''
+
+
+def main():
+    global path_of_directory
+    if len(sys.argv) > 1:
+        print(sys.argv)
+        path_of_directory = sys.argv[1]
+    else:
+        sys.exit('need a path')
+
+    print(datetime.datetime.now())
+    print('Generate connection with neo4j')
+
+    create_connection_with_neo4j()
+
+    print(
+        '###########################################################################################################################')
+
+    print(datetime.datetime.now())
+    print('Map generate tsv and cypher file ')
+
+    for tuple_label_and_refseq_label in [("Protein", "refSeq_CDS", "TRANSLATES_TO_RttP")]:
+        prepare_edge(tuple_label_and_refseq_label)
+
+    driver.close()
+
+    print(
+        '###########################################################################################################################')
+
+    print(datetime.datetime.now())
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
---
+++ mapping_and_merging_into_hetionet/refseq/map_cds_to_protein_refseq.py
@@ -0,0 +1,161 @@
+import datetime
+import csv, sys
+
+sys.path.append("../..")
+import create_connection_to_databases
+import pharmebinetutils
+
+'''
+create connection to neo4j and mysql
+'''
+
+
+def create_connection_with_neo4j():
+    # create connection with neo4j
+    global g, driver
+    driver = create_connection_to_databases.database_connection_neo4j_driver()
+    g = driver.session()
+
+
+# dictionary with pharmebinet proteins with identifier as key and value node as dictionary
+dict_protein_pharmebinet_to_resource = {}
+# dictionary refseq id to protein ids
+dict_refseq_to_protein_ids = {}
+# dictionary gene symbol to protein ids
+dict_gene_symbol_to_protein_ids = {}
+
+
+def load_pharmebinet_node():
+    '''
+    load in all protein from pharmebinet in a dictionary
+    '''
+    query = '''MATCH (n:Protein) RETURN n'''
+    results = g.run(query)
+
+    for record in results:
+        node = record.data()['n']
+        identifier = node['identifier']
+        dict_protein_pharmebinet_to_resource[identifier] = dict(node)
+        xrefs = node['xrefs'] if 'xrefs' in node else []
+        for xref in xrefs:
+            if xref.startswith('RefSeq:'):
+                refseq_id = xref.rsplit(':', 1)[1]
+                pharmebinetutils.add_entry_to_dict_to_set(dict_refseq_to_protein_ids, refseq_id, identifier)
+
+    # get the identifier of the protein using the connection between 'Gene' and 'Protein'
+    query = "MATCH a=(m:Gene)--(b:Protein) RETURN m.identifier, m.gene_symbol,b.identifier"
+    result = g.run(query)
+    for record in result:
+        [gene, gene_symbol, protein] = record.values()
+        pharmebinetutils.add_entry_to_dict_to_set(dict_gene_symbol_to_protein_ids, gene_symbol, protein)
+
+    print('number of protein nodes in pharmebinet:' + str(len(dict_protein_pharmebinet_to_resource)))
+
+
+def generate_files():
+    '''
+    Generate cypher and tsv for generating the new nodes and the relationships
+    '''
+    global writer
+    file_name = 'protein/mapping_protein.tsv'
+    csvfile = open(file_name, 'w', encoding='utf-8')
+    writer = csv.writer(csvfile, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(['id', 'id_refseq', 'how_mapped'])
+
+    # generate cypher file
+    cypher_file = open('output/cypher.cypher', 'a', encoding='utf-8')
+    query = ''' Match (c:Protein{ identifier:line.id}), (n:refSeq_CDS{ id:line.id_refseq})  Create (c)-[:equal_to_protein_refseq{how_mapped:line.how_mapped}]->(n) Set c.refseq="yes", c.resource=c.resource+"RefSeq"'''
+    query = pharmebinetutils.get_query_import(path_of_directory,
+                                              f'mapping_and_merging_into_hetionet/refseq/{file_name}',
+                                              query)
+    cypher_file.write(query)
+    cypher_file.close()
+
+
+def load_and_map_RefSeq_cds():
+    """
+    Load all human cds and try to map with
+    :return:
+    """
+    query = '''MATCH (n:refSeq_CDS) RETURN n.id, n.name, n.protein_id, n.gene'''
+    results = g.run(query)
+
+    counter = 0
+    mapped = 0
+    for record in results:
+        counter += 1
+        [cds_id, name, protein_id, gene_symbol] = record.values()
+
+        is_mapped = False
+
+        if gene_symbol in dict_gene_symbol_to_protein_ids:
+            mapped += 1
+            is_mapped = True
+            for pharmebinet_id in dict_gene_symbol_to_protein_ids[gene_symbol]:
+                writer.writerow([pharmebinet_id, cds_id, 'gene_symbol'])
+
+        if is_mapped:
+            continue
+
+        if protein_id in dict_refseq_to_protein_ids:
+            mapped += 1
+            is_mapped = True
+            for pharmebinet_id in dict_refseq_to_protein_ids[protein_id]:
+                writer.writerow([pharmebinet_id, cds_id, 'refseq_id'])
+
+    print('number of existing proteins', counter)
+    print('number of mapped proteins', mapped)
+
+
+# path to directory
+path_of_directory = ''
+
+
+def main():
+    global path_of_directory
+    if len(sys.argv) > 1:
+        print(sys.argv)
+        path_of_directory = sys.argv[1]
+    else:
+        sys.exit('need a path refseq')
+
+    print(datetime.datetime.now())
+    print('Generate connection with neo4j')
+
+    create_connection_with_neo4j()
+
+    print(
+        '###########################################################################################################################')
+
+    print(datetime.datetime.now())
+    print('Map generate tsv and cypher file ')
+
+    generate_files()
+
+    print(
+        '###########################################################################################################################')
+
+    print(datetime.datetime.now())
+    print('Load all proteins from pharmebinet into a dictionary')
+
+    load_pharmebinet_node()
+
+    print(
+        '###########################################################################################################################')
+
+    print(datetime.datetime.now())
+    print('Load all ctd proteins from neo4j into a dictionary')
+
+    load_and_map_RefSeq_cds()
+
+    driver.close()
+
+    print(
+        '###########################################################################################################################')
+
+    print(datetime.datetime.now())
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
