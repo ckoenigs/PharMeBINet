6cd713183387bc80ae12a024ad25fcd69beb7e95
ckoenigs <dragon493@web.de>
2023-07-25 09:46:12 +0200

    change atc merge because of changed atc source

--- mapping_and_merging_into_hetionet/atc/map_and_integrate_atc.py
+++ mapping_and_merging_into_hetionet/atc/map_and_integrate_atc.py
@@ -47,7 +47,7 @@ dict_atc_code_to_compound_ids = {}
 
 def load_compounds_from_database_and_add_to_dict():
     """
-    Load all drugbank with atc codes into a dictionary
+    Load all kegg with atc codes into a dictionary
     :return:
     """
     query = "MATCH (n:Compound) Where n.atc_codes is not NULL RETURN n.identifier, n.atc_codes"
@@ -55,9 +55,7 @@ def load_compounds_from_database_and_add_to_dict():
     for record in results:
         [identifier, atc_codes] = record.values()
         for atc_code in atc_codes:
-            if atc_code not in dict_atc_code_to_compound_ids:
-                dict_atc_code_to_compound_ids[atc_code] = set()
-            dict_atc_code_to_compound_ids[atc_code].add(identifier)
+            pharmebinetutils.add_entry_to_dict_to_set(dict_atc_code_to_compound_ids, atc_code, identifier)
 
 
 cypher_file = open('output/cypher.cypher', 'w', encoding='utf-8')
@@ -89,43 +87,42 @@ def write_files(path_of_directory):
     header_new = ['id']
     csv_new.writerow(header_new)
 
-    query = '''Match (n:atc{identifier:line.id}), (v:Compound{identifier:line.compound_id}) Create (v)-[:equal_to_atc]->(n)'''
-    query = pharmebinetutils.get_query_import(path_of_directory,
-                                              f'mapping_and_merging_into_hetionet/atc/{file_name_mapped}',
-                                              query)
-    cypher_file.write(query)
-
-    query = '''Match (n:atc{identifier:line.id}), (v:PharmacologicClass{identifier:line.pc_id}) Set v.atc_codes=[line.id], v.resource=split(line.resource,"|"), v.drugbank="yes" Create (v)-[:equal_to_atc]->(n)'''
+    query = '''Match (n:atc_kegg{identifier:line.id}), (v:PharmacologicClass{identifier:line.pc_id}) Set v.atc_codes=[line.id], v.resource=split(line.resource,"|"), v.kegg="yes" Create (v)-[:equal_to_atc]->(n)'''
     query = pharmebinetutils.get_query_import(path_of_directory,
                                               f'mapping_and_merging_into_hetionet/atc/{file_name_mapped_pc}',
                                               query)
     cypher_file.write(query)
 
-    query = '''Match (n:atc{identifier:line.id}) Create (v:PharmacologicClass{identifier:line.id, drugbank:'yes', resource:['DrugBank'], source:'ATC from DrugBankT', url:'http://identifiers.org/atc:'+line.id, name:n.name, license:"Creative Common's Attribution-NonCommercial 4.0 International", class_type:["ATC code"], atc_codes:[line.id]}) Create (v)-[:equal_to_atc]->(n)'''
+    query = '''Match (n:atc_kegg{identifier:line.id}) Create (v:PharmacologicClass{identifier:line.id, kegg:'yes', resource:['KEGG'], source:'ATC from KEGG', url:'http://identifiers.org/atc:'+line.id, name:n.name, license:"Use of all or parts of the material requires reference to the WHO Collaborating Centre for Drug Statistics Methodology. Copying and distribution for commercial purposes is not allowed. Changing or manipulating the material is not allowed.", class_type:["ATC code"], atc_codes:[line.id]}) Create (v)-[:equal_to_atc]->(n)'''
     query = pharmebinetutils.get_query_import(path_of_directory,
                                               f'mapping_and_merging_into_hetionet/atc/{file_name_new}',
                                               query)
     cypher_file.write(query)
 
-    list_of_labels = ['Compound', 'PharmacologicClass']
+    list_of_labels = ['PharmacologicClass']
 
     for [label_1, label_2] in [[x, y] for x in list_of_labels for y in list_of_labels]:
-        query = '''MATCH p=(n:%s)--(:atc)-[]->(a:atc)--(b:%s) Merge (n)-[r:BELONGS_TO_%sbt%s]->(b) On Create Set r.source='ATC from DrugBank', r.url="http://identifiers.org/atc:"+a.identifier , r.resource=['DrugBank'], r.drugbank='yes', r.license="Creative Common's Attribution-NonCommercial 4.0 International License";\n'''
+        query = '''MATCH p=(n:%s)--(:atc_kegg)-[]->(a:atc_kegg)--(b:%s) Merge (n)-[r:BELONGS_TO_%sbt%s]->(b) On Create Set r.source='ATC from KEGG', r.url="http://identifiers.org/atc:"+a.identifier , r.resource=['KEGG'], r.kegg='yes', r.license="Use of all or parts of the material requires reference to the WHO Collaborating Centre for Drug Statistics Methodology. Copying and distribution for commercial purposes is not allowed. Changing or manipulating the material is not allowed.";\n'''
         query = query % (
             label_1, label_2, dict_first_letter_to_rela_letter[label_1[0]],
             dict_first_letter_to_rela_letter[label_2[0]])
         cypher_file.write(query)
+
+    query = '''Match (n:PharmacologicClass{identifier:line.id}), (v:Compound{identifier:line.compound_id}) Create (v)-[:BELONGS_TO_CHbtPC{source:'ATC from KEGG', url:"http://identifiers.org/atc:"+n.id , resource:['KEGG'], kegg:'yes', license:"Use of all or parts of the material requires reference to the WHO Collaborating Centre for Drug Statistics Methodology. Copying and distribution for commercial purposes is not allowed. Changing or manipulating the material is not allowed."}]->(n)'''
+    query = pharmebinetutils.get_query_import(path_of_directory,
+                                              f'mapping_and_merging_into_hetionet/atc/{file_name_mapped}',
+                                              query)
+    cypher_file.write(query)
+
     return csv_mapped, csv_new, csv_mapped_pc
 
-
-# dictionary mapped drug id to atc codes
-dict_drug_id_to_atc_codes = {}
 
 # dictionary mapped pc id to atc codes
 dict_pc_id_to_atc_codes = {}
 
 # check that each pair exists only one time
 set_of_rela_pairs = set()
+dict_atc_to_drug_ids = {}
 
 
 def load_all_label_and_map(csv_map_drug, csv_new):
@@ -134,7 +131,7 @@ def load_all_label_and_map(csv_map_drug, csv_new):
     :param csv_map: csv writter
     :return:
     """
-    query = "MATCH (n:atc) RETURN n"
+    query = "MATCH (n:atc_kegg) RETURN n"
     results = g.run(query)
 
     # counter
@@ -145,54 +142,53 @@ def load_all_label_and_map(csv_map_drug, csv_new):
         identifier = node['identifier']
         name = node['name'].lower() if 'name' in node else ''
 
-        found_mapping = False
-        # map to compound
-        if identifier in dict_atc_code_to_compound_ids:
-            counter_mapped_to_compound += 1
-            for compound_id in dict_atc_code_to_compound_ids[identifier]:
-                if compound_id not in dict_drug_id_to_atc_codes:
-                    dict_drug_id_to_atc_codes[compound_id] = set()
-                dict_drug_id_to_atc_codes[compound_id].add(identifier)
-                if (compound_id, identifier) not in set_of_rela_pairs:
-                    csv_map_drug.writerow([compound_id, identifier])
-                    set_of_rela_pairs.add((compound_id, identifier))
-            continue
         if name in dict_name_to_pharmacologic_class_id:
             counter_mapped_to_pc += 1
             for pc_id in dict_name_to_pharmacologic_class_id[name]:
                 if pc_id not in dict_pc_id_to_atc_codes:
                     dict_pc_id_to_atc_codes[pc_id] = set()
                 dict_pc_id_to_atc_codes[pc_id].add(identifier)
-                # resource = dict_pharmacologic_class_id_to_resource[pc_id]
-                # resource.add("DrugBank")
-                # resource='|'.join(sorted(resource))
-                # csv_mapped_pc.writerow([pc_id, identifier, resource])
             continue
         counter_new += 1
         csv_new.writerow([identifier])
+        check_if_id_mapped_to_compound(identifier, csv_map_drug, identifier)
 
-    print('number of mapped to drug:', counter_mapped_to_compound)
     print('number of mapped to pc:', counter_mapped_to_pc)
     print('number of new:', counter_new)
 
 
-def to_avoid_multiple_mapping(csv_mapped_pc, csv_new):
+def check_if_id_mapped_to_compound(atc_id, csv_map_drug, pc_id):
+    """
+    Get a atc id and check if this is in the compounds and if so write into tsv file.
+    :param atc_id:
+    :param csv_map_drug:
+    :param pc_id:
+    :return:
+    """
+    if atc_id in dict_atc_code_to_compound_ids:
+        for drug_id in dict_atc_code_to_compound_ids[atc_id]:
+            csv_map_drug.writerow([drug_id, pc_id])
+
+
+def to_avoid_multiple_mapping(csv_mapped_pc, csv_new, csv_map_drug):
     """
     go through all name mapped
     :return:
     """
     for pc_id, set_of_atcs in dict_pc_id_to_atc_codes.items():
-        resource = dict_pharmacologic_class_id_to_resource[pc_id]
-        resource.add("DrugBank")
-        resource = '|'.join(sorted(resource))
+        resource = pharmebinetutils.resource_add_and_prepare(dict_pharmacologic_class_id_to_resource[pc_id],'KEGG')
         if len(set_of_atcs) == 1:
-            csv_mapped_pc.writerow([pc_id, set_of_atcs.pop(), resource])
+            atc_id = set_of_atcs.pop()
+            csv_mapped_pc.writerow([pc_id, atc_id, resource])
+            check_if_id_mapped_to_compound(atc_id, csv_map_drug, pc_id)
         else:
             longest_one = max(set_of_atcs, key=len)
             csv_mapped_pc.writerow([pc_id, longest_one, resource])
+            check_if_id_mapped_to_compound(longest_one, csv_map_drug, pc_id)
             for atc in set_of_atcs:
                 if atc != longest_one:
                     csv_new.writerow([atc])
+                    check_if_id_mapped_to_compound(atc_id, csv_map_drug, atc)
 
 
 def main():
@@ -229,21 +225,21 @@ def main():
     print(datetime.datetime.now())
     print('Generate files')
 
-    csv_mapper, csv_new, csv_mapped_pc = write_files(path_of_directory)
+    csv_drug_connection, csv_new, csv_mapped_pc = write_files(path_of_directory)
 
     print('##########################################################################')
 
     print(datetime.datetime.now())
     print('Load all label from database')
 
-    load_all_label_and_map(csv_mapper, csv_new)
+    load_all_label_and_map(csv_drug_connection, csv_new)
 
     print('##########################################################################')
 
     print(datetime.datetime.now())
     print('map only to one for pc')
 
-    to_avoid_multiple_mapping(csv_mapped_pc, csv_new)
+    to_avoid_multiple_mapping(csv_mapped_pc, csv_new, csv_drug_connection)
 
     driver.close()
 
