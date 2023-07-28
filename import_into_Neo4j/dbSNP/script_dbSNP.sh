26f7088de82774382d46812dc90aa2f9beb68b94
ckoenigs <dragon493@web.de>
2023-07-28 12:03:11 +0200

    add script but is not used

---
+++ import_into_Neo4j/dbSNP/script_dbSNP.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#define path to neo4j bin
+path_neo4j=$1
+
+#path to project
+path_to_project=$2
+
+#password
+password=$3
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+echo parse dbSNP
+
+
+python3 parse_json_to_tsv_dbsnp.py "/mnt/aba90170-e6a0-4d07-929e-1200a6bfc6e1/databases/dbSNP" $path_to_project  > output/output_generate_integration_file.txt
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+
+echo integrate efo into neo4j
+
+$path_neo4j/cypher-shell -u neo4j -p $password -f cypher.cypher > output/output_cypher_node.txt 2>&1
+
+sleep 60
+
+$path_neo4j/neo4j restart
+
+sleep 120
