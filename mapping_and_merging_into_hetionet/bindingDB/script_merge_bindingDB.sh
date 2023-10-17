5a75d4e095da9fcffa130fe20a48f9cb789bfb31
ckoenigs <dragon493@web.de>
2023-10-17 09:54:15 +0200

    add script

---
+++ mapping_and_merging_into_hetionet/bindingDB/script_merge_bindingDB.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#define path to neo4j bin
+path_neo4j=$1
+
+#path to project
+path_to_project=$2
+
+#password
+password=$3
+
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+echo protein mapping
+python3 map_protein_bindingdb.py $path_to_project > protein/output.txt
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+echo drug mapping
+python3 map_chemical_bindingdb.py $path_to_project > chemical/output.txt
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+echo integrate map drug and outcome
+
+$path_neo4j/cypher-shell -u neo4j -p $password -f output/cypher.cypher
+
+sleep 30
+$path_neo4j/neo4j restart
+sleep 50
+
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+echo complex integration
+python3 complex_node.py $path_to_project > protein/output.txt
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+echo integrate map drug and outcome
+
+$path_neo4j/cypher-shell -u neo4j -p $password -f output/cypher_node.cypher
+
+sleep 30
+$path_neo4j/neo4j restart
+sleep 50
