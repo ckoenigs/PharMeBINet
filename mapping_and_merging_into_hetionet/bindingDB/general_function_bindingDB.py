a381d7af70db678ccb835d2220b3494d12a97c72
ckoenigs <dragon493@web.de>
2023-10-27 10:51:05 +0200

    perpare general query

--- mapping_and_merging_into_hetionet/bindingDB/general_function_bindingDB.py
+++ mapping_and_merging_into_hetionet/bindingDB/general_function_bindingDB.py
@@ -4,7 +4,8 @@ sys.path.append("../..")
 import pharmebinetutils
 
 
-def generate_files(path_of_directory, file_name, source, label_bindingdb, label_pharmebinet, id_property_bindingdbs):
+def generate_files(path_of_directory, file_name, source, label_bindingdb, label_pharmebinet, id_property_bindingdbs,
+                   additoinal_property=''):
     """
     generate cypher file and tsv file
     :return: tsv file
@@ -14,7 +15,7 @@ def generate_files(path_of_directory, file_name, source, label_bindingdb, label_
         os.mkdir(path_of_directory)
 
     file_path = os.path.join(path_of_directory, file_name)
-    header = ['node_id', 'pharmebinet_node_id', 'resource', 'mapping_method']
+    header = ['node_id', 'pharmebinet_node_id', 'resource', 'mapping_method', 'xrefs']
     # 'w+' creates file, 'w' opens file for writing
     mode = 'w' if os.path.exists(file_path) else 'w+'
     file = open(file_path, mode, encoding='utf-8', newline="")
@@ -22,15 +23,15 @@ def generate_files(path_of_directory, file_name, source, label_bindingdb, label_
     csv_mapping.writerow(header)
 
     cypher_file_path = os.path.join(source, 'cypher.cypher')
-    # mapping_and_merging_into_hetionet/DisGeNet/
     prepare_where_string = ''
     for id_property_bindingdb in id_property_bindingdbs:
         prepare_where_string += f'n.{id_property_bindingdb}=line.node_id or '
-    query = f' Match (n:{label_bindingdb}), (v:{label_pharmebinet}{{identifier:line.pharmebinet_node_id}}) Where {prepare_where_string[:-3]} Set v.bindingdb="yes", v.resource=split(line.resource,"|") Create (v)-[:equal_to_bindingDB_{label_pharmebinet.lower()}{{mapped_with:line.mapping_method}}]->(n)'
+    query = f' Match (n:{label_bindingdb}), (v:{label_pharmebinet}{{identifier:line.pharmebinet_node_id}}) Where {prepare_where_string[:-3]} Set v.bindingdb="yes", v.resource=split(line.resource,"|") {additoinal_property} Create (v)-[:equal_to_bindingDB_{label_pharmebinet.lower()}{{mapped_with:line.mapping_method}}]->(n)'
     mode = 'a' if os.path.exists(cypher_file_path) else 'w'
     cypher_file = open(cypher_file_path, mode, encoding='utf-8')
     query = pharmebinetutils.get_query_import(path_of_directory,
                                               file_name,
                                               query)
     cypher_file.write(query)
+    cypher_file.close()
     return csv_mapping
