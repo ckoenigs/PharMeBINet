f6687cd4a76f5e1b4debf56bce0f9dc7fbe2ada9
ckoenigs <dragon493@web.de>
2024-01-10 14:06:54 +0100

    change edge name

--- mapping_and_merging_into_hetionet/drugbank/integrate_product_and_rela_to_compound.py
+++ mapping_and_merging_into_hetionet/drugbank/integrate_product_and_rela_to_compound.py
@@ -55,7 +55,7 @@ def prepare_cypher_query(file_name):
     cypher_file.write(pharmebinetutils.prepare_index_query('Product', 'identifier'))
     cypher_file.close()
     cypher_rela = open('output/cypher_rela.cypher', 'a', encoding='utf-8')
-    query_rela = 'Match (n:Compound)--(:Compound_DrugBank)--(:Product_DrugBank)--(m:Product) Create (n)-[:HAS_ChPR{source:"DrugBank", resource:["DrugBank"], license:"%s", drugbank:"yes"}]->(m);\n'
+    query_rela = 'Match (n:Compound)--(:Compound_DrugBank)--(:Product_DrugBank)--(m:Product) Create (n)-[:HAS_CHhPR{source:"DrugBank", resource:["DrugBank"], license:"%s", drugbank:"yes"}]->(m);\n'
     query_rela = query_rela % (license)
     cypher_rela.write(query_rela)
 
