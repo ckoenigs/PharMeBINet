8fb8883549dc4edae5edac74a62716d58284b8cf
ckoenigs <dragon493@web.de>
2024-01-18 05:13:57 +0100

    put data source path somewhere else

--- import_into_Neo4j/drugcentral/integrate_drugcentral.sh
+++ import_into_Neo4j/drugcentral/integrate_drugcentral.sh
@@ -12,31 +12,34 @@ biodwh2=$3
 #password
 password=$4
 
+#path other data source space
+path_data_source=$5
+
 
 echo load latest version of DrugCentral and generat GraphML file
 
-dir=./sources/
+dir=$path_data_source/drugcentral/sources/
 
 # prepare workspace and add DrugCentral to bioDWB2 tool
 if [ ! -d "$dir" ]; 
 then
     echo generate workspace in directory
-    java -jar ../$biodwh2.jar -c .
+    java -jar ../$biodwh2.jar -c $path_data_source/drugcentral
 
-    java -jar ../$biodwh2.jar --add-data-source . DrugCentral
+    java -jar ../$biodwh2.jar --add-data-source $path_data_source/drugcentral DrugCentral
     # if not all is exported then the config need to be changed
     # dataSourceProperties: "DrugCentral" : { "forceExport" : false, "skipLINCSSignatures" : true, "skipFAERSReports" : true, "skipDrugLabelFullTexts" : true }
 
 fi
 
-java -jar ../$biodwh2.jar -u .
+java -jar ../$biodwh2.jar -u $path_data_source/drugcentral
 
 echo $import_tool
 
 
 echo integrate DurgCentral into neo4j
 
-java -jar ../$import_tool.jar -i sources/DrugCentral/intermediate.graphml  -e bolt://localhost:7687 --username neo4j --password $password --label-prefix DC_ --indices "DC_ATC.id;DC_ATCDDD.id;DC_ActionType.id;DC_ActiveIngredient.id;DC_Approval.id;DC_AttributeType.id;DC_Bioactivity.id;DC_DOTerm.id;DC_DOTermXref.id;DC_DataSource.id;DC_DbVersion.id;DC_DrugClass.id;DC_DrugLabel.id;DC_GOTerm.id;DC_Identifier.id;DC_IdentifierType.id;DC_InnStem.id;DC_OMOPConcept.id;DC_OrangeBookExclusivity.id;DC_OrangeBookPatent.id;DC_OrangeBookProduct.id;DC_PDB.id;DC_ParentDrugMolecule.id;DC_PharmaClass.id;DC_Product.id;DC_Property.id;DC_Reference.id;DC_Structure.id;DC_Synonyms.id;DC_Target.id;DC_TargetComponent.id;DC_TargetKeyword.id;DC_VetOmop.id;DC_VetProd.id" > output/import_tool_output.txt
+java -jar ../$import_tool.jar -i $path_data_source/drugcentral/sources/DrugCentral/intermediate.graphml  -e bolt://localhost:7687 --username neo4j --password $password --label-prefix DC_ --indices "DC_ATC.id;DC_ATCDDD.id;DC_ActionType.id;DC_ActiveIngredient.id;DC_Approval.id;DC_AttributeType.id;DC_Bioactivity.id;DC_DOTerm.id;DC_DOTermXref.id;DC_DataSource.id;DC_DbVersion.id;DC_DrugClass.id;DC_DrugLabel.id;DC_GOTerm.id;DC_Identifier.id;DC_IdentifierType.id;DC_InnStem.id;DC_OMOPConcept.id;DC_OrangeBookExclusivity.id;DC_OrangeBookPatent.id;DC_OrangeBookProduct.id;DC_PDB.id;DC_ParentDrugMolecule.id;DC_PharmaClass.id;DC_Product.id;DC_Property.id;DC_Reference.id;DC_Structure.id;DC_Synonyms.id;DC_Target.id;DC_TargetComponent.id;DC_TargetKeyword.id;DC_VetOmop.id;DC_VetProd.id" > output/import_tool_output.txt
 
 sleep 30
 
