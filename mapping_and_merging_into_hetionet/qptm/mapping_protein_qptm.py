636e470118d7fc4e3adad610eed51531ef88321a
a-groba <ann.groba@gmail.com>
2024-11-28 10:03:37 +0100

    added qptm protein mapping

---
+++ mapping_and_merging_into_hetionet/qptm/mapping_protein_qptm.py
@@ -0,0 +1,154 @@
+import csv
+import datetime
+import os, sys
+
+sys.path.append("../..")
+import create_connection_to_databases
+import pharmebinetutils
+
+# dictionary protein id to resource
+dict_identifier_to_resource = {}
+
+# dictionary protein name to identifier
+dict_protein_name_to_identifier = {}
+
+# dictionary for gene_symbol to protein gene_name
+dict_identifier_to_alternative_ids = {}
+
+
+def load_proteins_from_database_and_add_to_dict():
+    """
+    Load all Proteins from pharmebinet and add them into a dictionary
+    """
+    query = "MATCH (n:Protein) RETURN n.identifier, n.name, n.resource, n.alternative_ids"
+    results = g.run(query)
+
+    for identifier, name, resource, alternative_ids in results:
+        dict_identifier_to_resource[identifier] = resource
+        name = name.lower()
+        pharmebinetutils.add_entry_to_dict_to_set(dict_protein_name_to_identifier, name, identifier)
+        if alternative_ids is not None:
+            dict_identifier_to_alternative_ids[identifier] = alternative_ids
+
+
+def generate_files(path_of_directory):
+    """
+    generate cypher file and tsv file
+    :return: tsv file
+    """
+    # make sure folder exists
+    if not os.path.exists(path_of_directory):
+        os.mkdir(path_of_directory)
+
+    file_name = 'qptm_protein_to_Protein'
+    file_path = os.path.join(path_of_directory, file_name) + '.tsv'
+    header = ['qptm_identifier', 'identifier', 'resource', 'mapping_method']
+    # 'w+' creates file, 'w' opens file for writing
+    mode = 'w' if os.path.exists(file_path) else 'w+'
+    file = open(file_path, mode, encoding='utf-8')
+    csv_mapping = csv.writer(file, delimiter='\t')
+    csv_mapping.writerow(header)
+
+    if not os.path.exists(source):
+        os.mkdir(source)
+
+    cypher_file_path = os.path.join(source, 'cypher.cypher')
+    # mapping_and_merging_into_hetionet/qptm/
+    query = f' Match (n:qPTM_Protein{{uniprot_id:line.qptm_identifier}}), (v:Protein{{identifier:line.identifier}}) Set v.qptm="yes", v.resource=split(line.resource,"|") Create (v)-[:equal_to_qPTM_protein{{mapped_with:line.mapping_method}}]->(n)'
+    mode = 'a' if os.path.exists(cypher_file_path) else 'w'
+    query = pharmebinetutils.get_query_import(path_of_directory, file_name + '.tsv', query)
+    cypher_file = open(cypher_file_path, mode, encoding='utf-8')
+    cypher_file.write(query)
+
+    return csv_mapping
+
+
+def load_all_qptm_proteins_and_finish_the_files(csv_mapping):
+    """
+    Load all variation sort the ids into the right tsv, generate the queries, and add rela to the rela tsv
+    """
+
+    query = "MATCH (n:qPTM_Protein) RETURN n"
+    results = g.run(query)
+    counter_not_mapped = 0
+    counter_all = 0
+    for record in results:
+        node = record.data()['n']
+        counter_all += 1
+        identifier = node.get('uniprot_id', None)
+        gene_name = node.get('gene_name', None)
+
+        # mapping
+        if identifier in dict_identifier_to_resource:
+            csv_mapping.writerow(
+                [identifier, identifier,
+                 pharmebinetutils.resource_add_and_prepare(dict_identifier_to_resource[identifier], "qPTM"),
+                 'uniprot_id'])
+        elif identifier not in dict_identifier_to_alternative_ids:
+            for main_id, alternatives in dict_identifier_to_alternative_ids.items():
+                if identifier in alternatives:
+                    csv_mapping.writerow(
+                        [identifier, main_id,
+                         pharmebinetutils.resource_add_and_prepare(dict_identifier_to_resource[main_id], "qPTM"),
+                         'alternative_id'])
+        else:
+            counter_not_mapped += 1
+            print(identifier)
+
+    print('number of not-mapped proteins:', counter_not_mapped)
+    print('number of all proteins:', counter_all)
+
+
+def create_connection_with_neo4j():
+    """
+    create a connection with neo4j
+    """
+    # set up authentication parameters and connection
+    global g, driver
+    driver = create_connection_to_databases.database_connection_neo4j_driver()
+    # driver = create_connection_to_database_metabolite.database_connection_neo4j_driver()
+    g = driver.session(database='graph')
+
+
+def main():
+    global path_of_directory
+    global source
+    global home
+
+    # path_of_directory = "/Users/ann-cathrin/Documents/Master_4_Semester/Forschungsmodul_Heyer/Projekt_Cassandra/Test"
+
+    if len(sys.argv) > 1:
+        path_of_directory = sys.argv[1]
+    else:
+        sys.exit('need a path')
+
+    home = os.getcwd()
+    source = os.path.join(home, 'output')
+    path_of_directory = os.path.join(home, 'protein/')
+
+    print('##########################################################################')
+    print(datetime.datetime.now())
+    print('connection to db')
+    create_connection_with_neo4j()
+    print('##########################################################################')
+
+    print(datetime.datetime.now())
+    print('Load all Proteins from database')
+    load_proteins_from_database_and_add_to_dict()
+    print('##########################################################################')
+
+    print(datetime.datetime.now())
+    print('Generate cypher and tsv file')
+    csv_mapping = generate_files(path_of_directory)
+
+    print('##########################################################################')
+
+    print(datetime.datetime.now())
+    print('Load all qPTM proteins from database')
+    load_all_qptm_proteins_and_finish_the_files(csv_mapping)
+
+    driver.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ mapping_and_merging_into_hetionet/qptm/script_qptm_mapping_and_integration.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+#define path to neo4j bin
+path_neo4j="/Users/ann-cathrin/Downloads/neo4j-community-5.19.0/bin"
+#path_neo4j=$1
+
+# path to project
+path_to_project="/Users/ann-cathrin/Documents/Master_4_Semester/Forschungsmodul_Heyer/Projekt_Cassandra/PharMeBINet/"
+#path_to_project=$2
+
+#password
+password="test1234"
+#password=$3
+
+
+
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+echo map and integrate proteins
+
+if [ ! -d protein ]; then
+  mkdir protein
+fi
+
+python3 mapping_protein_qptm.py $path_to_project > protein/output_protein.txt
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+echo map and integrate proteins
+
+if [ ! -d protein ]; then
+  mkdir ptm
+fi
+
+#python3 integrate_ptm_qptm.py $path_to_project > ptm/output_ptm.txt
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+echo integrate mappings into neo4j
+
+python ../../execute_cypher_shell.py $path_neo4j $password output/cypher.cypher > output/cypher.txt
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+echo 'Edge qptm ptm-phenotype'
+
+if [ ! -d ptm_phenotype_edge ]; then
+  mkdir ptm_phenotype_edge
+fi
+
+#python3 mapping_ptm_phenotype.py $path_to_project > ptm_phenotype_edge/output.txt
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+echo 'Edge qptm ptm-protein'
+
+if [ ! -d ptm_protein_edge ]; then
+  mkdir ptm_protein_edge
+fi
+
+#python3 mapping_ptm_protein.py $path_to_project > ptm_protein_edge/output.txt
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+echo integrate edges into neo4j
+
+#python ../../execute_cypher_shell.py $path_neo4j $password output/cypher_edge.cypher > output/cypher_edge.txt
+
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+echo restarting neo4j
+
+sleep 10
+# python restart_neo4j.py $path_neo4j > output/neo4j1.txt
+python ../../restart_neo4j.py $path_neo4j > output/neo4.txt
+sleep 10
+
