215712bd0b3d8b6cae3df9416ebc74e4a045bdf6
ckoenigs <dragon493@web.de>
2025-01-21 09:39:28 +0100

    add cell ontology

---
+++ import_into_Neo4j/CO/script_import_co.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#define path to neo4j bin
+path_neo4j=$1
+
+#path to project
+path_to_project=$2
+
+#password
+password=$3
+
+# prepare directories
+if [ ! -d output ]; then
+  mkdir output
+fi
+if [ ! -d data ]; then
+  mkdir data
+fi
+
+
+
+#download co
+wget  -O data/cl.obo "https://purl.obolibrary.org/obo/cl.obo"
+
+
+python3 ../EFO/transform_obo_to_tsv_and_cypher_file.py data/cl.obo CO Cell_type_CO $path_to_project > output/output_generate_integration_file.txt
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+
+echo integrate co into neo4j
+
+python ../../execute_cypher_shell.py $path_neo4j $password cypher.cypher > output/cypher.txt
+
+now=$(date +"%F %T")
+echo "Current time: $now"
+
+echo integrate co into neo4j
+
+python ../../execute_cypher_shell.py $path_neo4j $password cypher_edge.cypher > output/cypher.txt
+
+sleep 20
+
+python ../../restart_neo4j.py $path_neo4j > output/neo4.txt
+
+
+sleep 30
