555149d5fc2758b9c6dc6753de0b508090b6d5d9
ckoenigs <dragon493@web.de>
2025-01-28 07:46:36 +0100

    map fideo food

---
+++ mapping_and_merging_into_hetionet/fideo/script_fideo_merge.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+path_neo4j=$1
+
+#path to project
+path_to_project=$2
+
+#password
+password=$3
+
+
+if [ ! -d output ]; then
+  mkdir output
+  mkdir edges
+fi
+echo add food
+now=$(date +"%F %T")
+echo "Current time: $now"
+
+python3 map_food_fideo.py $path_to_project > output/output_add.txt
+
+
+
+echo integrate connection with neo4j shell
+now=$(date +"%F %T")
+echo "Current time: $now"
+
+python ../../execute_cypher_shell.py $path_neo4j $password output/cypher.cypher > output/cypher.txt
+
+sleep 30
+
+python ../../restart_neo4j.py $path_neo4j > output/neo4.txt
+
+sleep 40
+
+echo add edges
+now=$(date +"%F %T")
+echo "Current time: $now"
+
+
+#python ../../execute_cypher_shell.py $path_neo4j $password output/cypher_edge.cypher > output/cypher2.txt
+#sleep 30
+
+#python ../../restart_neo4j.py $path_neo4j > output/neo4j.txt
+
+#sleep 40
