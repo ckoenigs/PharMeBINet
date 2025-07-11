a8b71436189dc2839af96bae7d6aebb5e6c1ea46
ckoenigs <dragon493@web.de>
2025-07-11 07:20:47 +0200

    prepare cypher fil in script

--- import_into_Neo4j/Hetionet/script_import_hetionet_with_graphml.sh
+++ import_into_Neo4j/Hetionet/script_import_hetionet_with_graphml.sh
@@ -18,14 +18,9 @@ if [ ! -d output ]; then
   mkdir output
 fi
 
-# if reactome is with another version
-# JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/ 
-# path_neo4j_other='/mnt/aba90170-e6a0-4d07-929e-1200a6bfc6e1/databases/neo4j/neo4j-community-4.4.16/bin'
-path_neo4j_other=$path_neo4j
 
 $path_neo4j/restart_neo4j.sh graph stop
 
-#$path_neo4j_other/restart_neo4j.sh reactome restart
 $path_neo4j/restart_neo4j.sh hetionet restart
 
 sleep 30
@@ -36,12 +31,13 @@ now=$(date +"%F %T")
 echo "Current time: $now"
 echo 'extract as graphml'
 
-# $path_neo4j_other/cypher-shell -u neo4j -p $password -f export_reactome.cypher
+# create cypher file with query with variable path
+echo 'CALL apoc.export.graphml.all("'$path_to_project'import_into_Neo4j/Hetionet/data/hetionet.graphml", {batchSize:10000, readLabels: true, storeNodeIds: false, useTypes: true});' > export_hetionet.cypher
+
 python ../../execute_cypher_shell.py $path_neo4j $password export_hetionet.cypher > output/cypher.txt
 
 sleep 30
 
-#$path_neo4j_other/neo4j stop
 $path_neo4j/neo4j stop
 
 
@@ -55,7 +51,7 @@ now=$(date +"%F %T")
 echo "Current time: $now"
 echo 'import with tool'
 
-java -jar ../$import_tool.jar -i $path_to_project"import_into_Neo4j/Hetionet/data/pathwaydata.graphml"  -e bolt://localhost:7687 --username neo4j --password $password --label-suffix $suffix --modify-edge-labels false --indices "Pathway_hetionet.identifier;SideEffect_hetionet.identifier;CellularComponent_hetionet.identifier;Anatomy_hetionet.identifier;Symptom_hetionet.identifier;Disease_hetionet.identifier;BiologicalProcess_hetionet.identifier;Gene_hetionet.identifier;PharmacologicClass_hetionet.identifier;MolecularFunction_hetionet.identifier;Compound_hetionet.identifier;CellularComponent_hetionet.name;BiologicalProcess_hetionet.name;PharmacologicClass_hetionet.name;Disease_hetionet.name;Gene_hetionet.name;SideEffect_hetionet.name;Pathway_hetionet.name;Compound_hetionet.name;MolecularFunction_hetionet.name;Symptom_hetionet.name;Anatomy_hetionet.name" > output/import_tool_output.txt
+java -jar ../$import_tool.jar -i $path_to_project"import_into_Neo4j/Hetionet/data/hetionet.graphml"  -e bolt://localhost:7687 --username neo4j --password $password --label-suffix $suffix --modify-edge-labels false --indices "Pathway_hetionet.identifier;SideEffect_hetionet.identifier;CellularComponent_hetionet.identifier;Anatomy_hetionet.identifier;Symptom_hetionet.identifier;Disease_hetionet.identifier;BiologicalProcess_hetionet.identifier;Gene_hetionet.identifier;PharmacologicClass_hetionet.identifier;MolecularFunction_hetionet.identifier;Compound_hetionet.identifier;CellularComponent_hetionet.name;BiologicalProcess_hetionet.name;PharmacologicClass_hetionet.name;Disease_hetionet.name;Gene_hetionet.name;SideEffect_hetionet.name;Pathway_hetionet.name;Compound_hetionet.name;MolecularFunction_hetionet.name;Symptom_hetionet.name;Anatomy_hetionet.name" > output/import_tool_output.txt
 
 sleep 30
 
