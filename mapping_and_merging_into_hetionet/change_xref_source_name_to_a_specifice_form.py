cd84116293d4c6b2f429de48e9320ce0c161cc4d
ckoenigs <dragon493@web.de>
2025-07-15 06:34:44 +0200

    format file

--- mapping_and_merging_into_hetionet/change_xref_source_name_to_a_specifice_form.py
+++ mapping_and_merging_into_hetionet/change_xref_source_name_to_a_specifice_form.py
@@ -1,23 +1,23 @@
 # dictionary wrong (not fitting) source to real source
 dict_wrong_source_to_source = {
     'MIM': 'OMIM',
-    'RxCUI':'RxNorm_CUI',
-    'MSH':'MESH',
-    'kegg_id':'KEGG Compound',
-    'chemspider_id':'ChemSpider',
-    'drugbank_id':'DrugBank',
-    'foodb_id':'FooDB',
-    'chebi_id':'ChEBI',
-    'pubchem_compound_id':'PubChem Compound',
-    'wikipedia_id':'Wikipedia',
-    'pdb_id':'PDB'
-
+    'RxCUI': 'RxNorm_CUI',
+    'MSH': 'MESH',
+    'kegg_id': 'KEGG Compound',
+    'chemspider_id': 'ChemSpider',
+    'drugbank_id': 'DrugBank',
+    'foodb_id': 'FooDB',
+    'chebi_id': 'CHEBI',
+    'pubchem_compound_id': 'PubChem Compound',
+    'wikipedia_id': 'Wikipedia',
+    'pdb_id': 'PDB',
+    'ChEBI': 'CHEBI',
 
 }
 
+# set of sources where the id depends on the label
+set_source_where_source_depend_on_label = set(['ctd', 'drugbank', 'drugcentral', 'kegg'])
 
-#set of sources where the id depends on the label
-set_source_where_source_depend_on_label=set(['ctd','drugbank','drugcentral', 'kegg'])
 
 def go_through_xrefs_and_change_if_needed_source_name(xrefs, label):
     """
@@ -27,14 +27,14 @@ def go_through_xrefs_and_change_if_needed_source_name(xrefs, label):
     :param label: string
     :return: sorted list of xrefs
     """
-    new_xref=set()
+    new_xref = set()
     for xref in xrefs:
-        source=xref.split(':',1)[0]
+        source = xref.split(':', 1)[0]
         if source in dict_wrong_source_to_source:
-            xref=xref.replace(source,dict_wrong_source_to_source[source])
+            xref = xref.replace(source, dict_wrong_source_to_source[source])
         if source.lower() in set_source_where_source_depend_on_label:
-            xref=xref.replace(source, source+' '+label)
+            xref = xref.replace(source, source + ' ' + label)
         new_xref.add(xref)
-    new_xref=list(new_xref)
+    new_xref = list(new_xref)
     new_xref.sort()
     return new_xref
