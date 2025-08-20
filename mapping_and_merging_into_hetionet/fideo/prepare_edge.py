728f39537bbbe2bf0d16ad5f35d68bf11e35d0da
ckoenigs <dragon493@web.de>
2025-08-20 09:58:40 +0200

    finish fideo edge merging

--- mapping_and_merging_into_hetionet/fideo/prepare_edge.py
+++ mapping_and_merging_into_hetionet/fideo/prepare_edge.py
@@ -49,11 +49,11 @@ def get_all_FIDEO_reference_edge_info():
     Get for food-drug interaction nodes the reference information
     :return:
     """
-    query = f"Match (n:{label_other_node})<-[r:is_about]-(m:{label_other_node}) Return m.id, m.xref, n.id"
-    for reference_id, reference_url, edge_id, in g.run(query):
+    query = f"Match (n:{label_other_node})<-[r:is_about]-(m:{label_other_node}) Return m.id, m.xref, m.name, n.id"
+    for reference_id, reference_url, reference_name, edge_id, in g.run(query):
         if edge_id not in dict_FIDEO_edge_to_reference:
             dict_FIDEO_edge_to_reference[edge_id] = []
-        dict_FIDEO_edge_to_reference[edge_id].append((reference_id,reference_url))
+        dict_FIDEO_edge_to_reference[edge_id].append((reference_id, reference_url, reference_name))
 
 
 def prepare_query(file_name):
@@ -63,37 +63,84 @@ def prepare_query(file_name):
     :return:
     """
     with open('output/cypher_edge.cypher', 'w', encoding='utf8') as f:
-        query = '''Match (s:Chemical{identifier:line.chemical_id }), (m:Food{identifier:line.food_id}) Create (s)<-[:INTERACTS_FiCH{fideo:"yes", license:"", text:line.edge_info , resource:["FIDEO"], url:""+line.edge_id, reference:split(line.reference,'|')}]-(m) '''
+        query = '''Match (s:Chemical{identifier:line.chemical_id }), (m:Food{identifier:line.food_id}) Create (s)<-[:INTERACTS_FiCH{fideo:"yes", license:"CC-BY 4.0", text:line.edge_info , interaction_text:split(line.interaction_text,'|'), resource:["FIDEO"], url:"https://gitub.u-bordeaux.fr/erias/fideo",  hedrine_ids:split(line.hedrine,'|') ,reference:split(line.reference,'|')}]-(m) '''
         query = pharmebinetutils.get_query_import(path_of_directory,
                                                   f'mapping_and_merging_into_hetionet/fideo/{file_name}',
                                                   query)
         f.write(query)
+
+
 # dictionary from reference node to chemical id
-dict_reference_id_to_chemical={}
+dict_reference_id_to_chemical = {}
+
 
 def load_reference_to_chemical():
-    query = f"Match (m:Chemical)--(n:{label_other_node})  Where (n)-[:is_about]-(:{label_other_node}) Return Distinct m.identifier,n.id "
-    for chemical_id, reference_id, in g.run(query):
+    """
+    load all chemical-reference connection with food-interaction information and write into dictionary
+    :return:
+    """
+    query = f"Match (m:Chemical)--(n:{label_other_node})  Where (n)-[:is_about]-(:{label_other_node}) Return Distinct m.identifier,n.id, m.food_interaction "
+    for chemical_id, reference_id, food_interaction_text, in g.run(query):
+        food_interaction_text = food_interaction_text if food_interaction_text else []
         if reference_id in dict_reference_id_to_chemical:
             print('ohno referefnekfdnslfk!!!!!')
-        dict_reference_id_to_chemical[reference_id]=chemical_id
+        dict_reference_id_to_chemical[reference_id] = [chemical_id, food_interaction_text]
+
+
+def get_interaction_text(interaction_texts, food_name, food_id):
+    """
+    try to extract form drugbank food interaction text. Therefore part of the food name need to be in the text. Some
+    foods are excluded because tey are to general and also some words or symbols are exclude. For food product and
+    alcoholic beverage got a manual mapper.
+    :param interaction_texts:
+    :param food_name:
+    :param food_id:
+    :return:
+    """
+    selected_interaction_texts = []
+    if food_id in ['FOODON:03301014', 'FOODON:03309880', 'FOODON:03310207']:
+        return selected_interaction_texts
+    for interaction_text in interaction_texts:
+        interaction_text_lower = interaction_text.lower()
+        position = 0
+        found_part = False
+        for part_food_name in food_name.split(' '):
+            if (part_food_name == 'food' and position != 0) or part_food_name in ['product', 'or', 'beverage',
+                                                                                  '-'] or part_food_name.startswith(
+                '(') or part_food_name[-1] == ')':
+                position += 1
+                continue
+            if part_food_name in interaction_text_lower:
+                found_part = True
+                selected_interaction_texts.append(interaction_text)
+            position += 1
+        if not found_part and (
+                (food_id == 'FOODON:00001002' and interaction_text.startswith('Take on an empty stomach')) or (
+                food_id == 'FOODON:00001579' and 'alcohol' in interaction_text_lower)):
+            selected_interaction_texts.append(interaction_text)
+
+    return selected_interaction_texts
+
 
 def prepare_edge():
     """
     Prepare cypher file with query, the tsv file and load all chemical-food pairs and prepare the infomation and write them into the tsv file
     :return:
     """
-    query = 'Match (i:Chemical)--(:FIDEO_Entry)-[j]-(k:FIDEO_Entry)-[r]-(:FIDEO_Entry)--(h:Food)  Where not j:is_about Return Distinct  i.identifier, i.name, h.identifier, k.id, k.name'
+    query = 'Match (i:Chemical)--(:FIDEO_Entry)-[j]-(k:FIDEO_Entry)-[r]-(:FIDEO_Entry)--(h:Food)  Where not j:is_about Return Distinct  i.identifier, i.name, i.food_interaction, h.identifier, h.name, k.id, k.name'
     file_name = 'edges/food_drug_interaction.tsv'
     prepare_query(file_name)
     set_of_chemical_food_tuples = set()
     with open(file_name, 'w', encoding='utf-8') as f:
         csv_writer = csv.writer(f, delimiter='\t')
-        csv_writer.writerow(['chemical_id', 'food_id', 'edge_id', 'edge_info', 'reference'])
+        csv_writer.writerow(
+            ['chemical_id', 'food_id', 'edge_id', 'edge_info', 'reference', 'interaction_text', 'hedrine'])
         counter_all = 0
         counter_no_reference = 0
-        for chemical_id, chemical_name, food_id, edge_id, edge_info, in g.run(query):
+        for chemical_id, chemical_name, food_interaction, food_id, food_name, edge_id, edge_info, in g.run(query):
             counter_all += 1
+            food_name = food_name.lower()
+            food_interaction = food_interaction if food_interaction else []
             if (chemical_id, food_id) in set_of_chemical_food_tuples and chemical_name.lower() in edge_info.lower():
                 print('multiple edge', chemical_id, food_id)
                 sys.exit('')
@@ -105,39 +152,56 @@ def prepare_edge():
                 print('not an interaction :O', edge_id)
                 sys.exit(');')
             if chemical_name.lower() in edge_info.lower():
+                selected_interaction_texts = get_interaction_text(food_interaction, food_name, food_id)
                 csv_writer.writerow(
-                    [chemical_id, food_id, edge_id, edge_info, "|".join([x[1] for x in dict_FIDEO_edge_to_reference[edge_id]])])
+                    [chemical_id, food_id, edge_id, edge_info,
+                     "|".join(set([x[1] for x in dict_FIDEO_edge_to_reference[edge_id]])),
+                     '|'.join(selected_interaction_texts),
+                     "|".join(set([x[2] for x in dict_FIDEO_edge_to_reference[edge_id] if x[2].startswith('HEDRINE')]))])
                 set_of_chemical_food_tuples.add((chemical_id, food_id))
             else:
                 reference_node_ids = set([x[0] for x in dict_FIDEO_edge_to_reference[edge_id]])
-                set_possible_chemicals =set()
-                found_reference_chemical =False
+                dict_possible_chemicals_to_interaction_texts = {}
+                found_reference_chemical = False
+
                 for reference_id in reference_node_ids:
                     if reference_id in dict_reference_id_to_chemical:
-                        found_reference_chemical=True
-                        if dict_reference_id_to_chemical[reference_id]== chemical_id:
+                        found_reference_chemical = True
+                        if dict_reference_id_to_chemical[reference_id][0] == chemical_id:
+                            selected_interaction_texts = get_interaction_text(
+                                dict_reference_id_to_chemical[reference_id][1], food_name, food_id)
                             csv_writer.writerow(
                                 [chemical_id, food_id, edge_id, edge_info,
-                                 "|".join([x[1] for x in dict_FIDEO_edge_to_reference[edge_id]])])
+                                 "|".join(set([x[1] for x in dict_FIDEO_edge_to_reference[edge_id]])),
+                                 '|'.join(selected_interaction_texts),
+                                 "|".join(set([x[2] for x in dict_FIDEO_edge_to_reference[edge_id] if
+                                           x[2].startswith('HEDRINE')]))])
                             set_of_chemical_food_tuples.add((chemical_id, food_id))
                             break
                         else:
-                            set_possible_chemicals.add(dict_reference_id_to_chemical[reference_id])
-                if len(set_possible_chemicals):
-                    print('change chemical', chemical_id, 'to', set_possible_chemicals)
-                    for possible_chemical in set_possible_chemicals:
-                            csv_writer.writerow(
-                                [possible_chemical, food_id, edge_id, edge_info,
-                                 "|".join([x[1] for x in dict_FIDEO_edge_to_reference[edge_id]])])
-                            set_of_chemical_food_tuples.add((possible_chemical, food_id))
+                            dict_possible_chemicals_to_interaction_texts[
+                                dict_reference_id_to_chemical[reference_id][0]] = \
+                                dict_reference_id_to_chemical[reference_id][1]
+                if len(dict_possible_chemicals_to_interaction_texts):
+                    print('change chemical', chemical_id, 'to', dict_possible_chemicals_to_interaction_texts)
+                    for possible_chemical, interaction_texts in dict_possible_chemicals_to_interaction_texts.items():
+                        selected_interaction_texts = get_interaction_text(interaction_texts, food_name, food_id)
+                        csv_writer.writerow(
+                            [possible_chemical, food_id, edge_id, edge_info,
+                             "|".join(set([x[1] for x in dict_FIDEO_edge_to_reference[edge_id]])),
+                             '|'.join(selected_interaction_texts),
+                             "|".join(
+                                 set([x[2] for x in dict_FIDEO_edge_to_reference[edge_id] if x[2].startswith('HEDRINE')]))])
+                        set_of_chemical_food_tuples.add((possible_chemical, food_id))
                 elif not found_reference_chemical:
                     print('different text but no alternatives', chemical_id, food_id)
+                    selected_interaction_texts = get_interaction_text(food_interaction, food_name, food_id)
                     csv_writer.writerow(
                         [chemical_id, food_id, edge_id, edge_info,
-                         "|".join([x[1] for x in dict_FIDEO_edge_to_reference[edge_id]])])
+                         "|".join(set([x[1] for x in dict_FIDEO_edge_to_reference[edge_id]])),
+                         '|'.join(selected_interaction_texts),
+                         "|".join(set([x[2] for x in dict_FIDEO_edge_to_reference[edge_id] if x[2].startswith('HEDRINE')]))])
                     set_of_chemical_food_tuples.add((chemical_id, food_id))
-
-
 
         print('number of all edge', counter_all)
         print('number of no_reference', counter_no_reference)
